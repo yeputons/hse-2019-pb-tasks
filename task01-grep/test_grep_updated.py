6ae462f4426d50fcd0f1ebb54bb06bbe6cdf5da1
DT6 <dt6cgsg@gmail.com>
2019-11-19 19:25:37 +0300

    Added tests for new parser and temporary removed other tests

--- task01-grep/test_grep_updated.py
+++ task01-grep/test_grep_updated.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+import argparse
 import grep
 
 
-def test_integrate_all_keys_print_files_grep(tmp_path, monkeypatch, capsys):
+def atest_integrate_all_keys_print_files_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
     (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
     monkeypatch.chdir(tmp_path)
@@ -12,7 +13,7 @@ def test_integrate_all_keys_print_files_grep(tmp_path, monkeypatch, capsys):
     assert out == 'b.txt\n'
 
 
-def test_integrate_all_keys_print_not_files_grep(tmp_path, monkeypatch, capsys):
+def atest_integrate_all_keys_print_not_files_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
     (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
     monkeypatch.chdir(tmp_path)
@@ -22,7 +23,7 @@ def test_integrate_all_keys_print_not_files_grep(tmp_path, monkeypatch, capsys):
     assert out == 'a.txt\n'
 
 
-def test_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
+def atest_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
     (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
     monkeypatch.chdir(tmp_path)
@@ -30,3 +31,26 @@ def test_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:3\na.txt:0\n'
+
+
+def test_parse_args_grep():
+    assert grep.parse_args(['-i', 'needle', 'file']) == \
+        argparse.Namespace(count=False, files=['file'], full_match=False, has_lines=False,
+                           ignore=True, inverse=False, needle='needle', no_lines=False,
+                           regex=False)
+    assert grep.parse_args(['-i', '-x', '-v', '-c', '-E', 'n', 'a', 'b']) == \
+        argparse.Namespace(count=True, files=['a', 'b'], full_match=True, has_lines=False,
+                           ignore=True, inverse=True, needle='n', no_lines=False,
+                           regex=True)
+    assert grep.parse_args(['n', '-l']) == \
+        argparse.Namespace(count=False, files=[], full_match=False, has_lines=True,
+                           ignore=False, inverse=False, needle='n', no_lines=False,
+                           regex=False)
+    assert grep.parse_args(['-E', '-L', 'n', 'a']) == \
+        argparse.Namespace(count=False, files=['a'], full_match=False, has_lines=False,
+                           ignore=False, inverse=False, needle='n', no_lines=True,
+                           regex=True)
+    assert grep.parse_args(['-i', '-x', '-v', '-c', '-E', '-l', '-L', 'n', 'a', 'b']) == \
+        argparse.Namespace(count=True, files=['a', 'b'], full_match=True, has_lines=True,
+                           ignore=True, inverse=True, needle='n', no_lines=True,
+                           regex=True)
