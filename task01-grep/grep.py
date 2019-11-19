cd023a3ab1a14ab669cb6c5ed1a256ce21c459b8
Denis Lochmelis <dlochmelis@gmail.com>
2019-11-19 22:26:07 +0300

    added changes to prev version + first commit to git

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,8 +1,70 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, Dict
 import sys
 import re
 import argparse
+
+fl_dict_type = Dict[str, List[str]]
+
+
+def does_line_match(line: str, regex: bool, needle: str) -> bool:
+    if regex:
+        return bool(re.search(needle, line))
+    else:
+        return needle in line
+
+
+def search_needle_in_file_line_dict(file_line_dict: fl_dict_type,
+                                    needle: str, regex: bool) -> fl_dict_type:
+    matching_elements: fl_dict_type = {}
+    for file in file_line_dict:
+        matching_elements[file] = []
+        for line in file_line_dict[file]:
+            if does_line_match(line, regex, needle):
+                matching_elements[file].append(line)
+    return matching_elements
+
+
+def lines_to_numbers(file_line_dict: fl_dict_type) -> fl_dict_type:
+    for file in file_line_dict:
+        file_line_dict[file] = [str(len(file_line_dict[file]))]
+    return file_line_dict
+
+
+# My escaped characters:
+# &&f - filename
+# &&l - line
+def print_format(output_format: str, filename: str, line: str) -> None:
+    output_format = output_format.replace('&&f', filename)
+    output_format = output_format.replace('&&l', line)
+    print(output_format)
+
+
+def print_output_dict(output: fl_dict_type, output_format: str) -> None:
+    is_any_output = False
+    for file in output:
+        for line in output[file]:
+            print_format(output_format, file, line)
+            is_any_output = True
+    if not is_any_output:
+        print()
+
+
+def read_files(files: List['str']) -> fl_dict_type:
+    file_line_dict: fl_dict_type = {}
+    for file in files:
+        file_line_dict[file] = []
+        with open(file, 'r') as input_file:
+            for line in input_file.readlines():
+                file_line_dict[file].append(line.rstrip('\n'))
+    return file_line_dict
+
+
+def read_stdin() -> fl_dict_type:
+    file_line_dict: fl_dict_type = {'': []}
+    for line in sys.stdin.readlines():
+        file_line_dict[''].append(line.rstrip('\n'))
+    return file_line_dict
 
 
 def main(args_str: List[str]):
@@ -10,20 +72,20 @@ def main(args_str: List[str]):
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='output_count', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    file_line_dict = read_files(args.files) if args.files else read_stdin()
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    matching_elements = search_needle_in_file_line_dict(file_line_dict, args.needle, args.regex)
+    if args.output_count:
+        lines_to_numbers(matching_elements)
+
+    output_format = '&&l'
+    if len(args.files) > 1:
+        output_format = '&&f:&&l'
+
+    print_output_dict(matching_elements, output_format)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,33 +1,62 @@
 #!/usr/bin/env python3
+from typing import List
 import io
 import grep
 
 
-def test_integrate_stdin_grep(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', io.StringIO(
-        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    grep.main(['needle?'])
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
+class MyStdinTest:
+    def __init__(self, input_content: str, arguments: List[str], expected_output: str):
+        self.input_content = input_content
+        self.arguments = arguments
+        self.expected_output = expected_output
 
 
-def test_integrate_stdin_regex_grep(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', io.StringIO(
-        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    grep.main(['-E', 'needle?'])
+def integrate_stdin_test_helper(monkeypatch, capsys, input_content: str,
+                                arguments: List[str], expected_output: str):
+    monkeypatch.setattr('sys.stdin', io.StringIO(input_content))
+    grep.main(arguments)
     out, err = capsys.readouterr()
     assert err == ''
-    assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
+    assert out == expected_output
+
+
+def test_integrate_stdin_grep_basic(monkeypatch, capsys):
+    tests = [
+        MyStdinTest('simple test\nabac cabac cabaca\nnext line is empty\n\nend',
+                    ['e'], 'simple test\nnext line is empty\nend\n'),
+        MyStdinTest('no output\nexpected\n\n\nthis time\n', ['no such line'], '\n'),
+        MyStdinTest('', ['empty input'], '\n'),
+        MyStdinTest('same\nsame\nsame\nsame\nline', ['am'], 'same\nsame\nsame\nsame\n')
+    ]
+    for test in tests:
+        integrate_stdin_test_helper(monkeypatch, capsys,
+                                    test.input_content, test.arguments, test.expected_output)
+
+
+def test_integrate_stdin_grep_regex(monkeypatch, capsys):
+    tests = [
+        MyStdinTest('simple test\ntabac cabac cabaca\nnext line is empty\n\nend',
+                    ['-E', '\\b\\w{4}\\b'], 'simple test\nnext line is empty\n'),
+        MyStdinTest('no output\nexpected\n\n\nthis time\n', ['-E', '[A-Z]+'], '\n'),
+        MyStdinTest('', ['-E', '[empty input]+'], '\n'),
+        MyStdinTest('sAme\nsAme\nsAme\nsAme\nline', ['-E', '[A-Z]'], 'sAme\nsAme\nsAme\nsAme\n')
+    ]
+    for test in tests:
+        integrate_stdin_test_helper(monkeypatch, capsys,
+                                    test.input_content, test.arguments, test.expected_output)
 
 
 def test_integrate_stdin_grep_count(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', io.StringIO(
-        'pref needle\nneedle suf\nthe needl\npref needle suf'))
-    grep.main(['-c', 'needle'])
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == '3\n'
+    tests = [
+        MyStdinTest('simple test\ntabac cabac cabaca\nnext line is empty\n\nend',
+                    ['-c', '-E', '\\b\\w{4}\\b'], '2\n'),
+        MyStdinTest('no output\nexpected\n\n\nthis time\n', ['-E', '-c', '[A-Z]+'], '0\n'),
+        MyStdinTest('', ['-E', '-c', '[empty input]+'], '0\n'),
+        MyStdinTest('sAme\nsAme\nsAme\nsAme\nline', ['-c', '-E', '[A-Z]'], '4\n')
+    ]
+    for test in tests:
+        integrate_stdin_test_helper(monkeypatch, capsys,
+                                    test.input_content, test.arguments, test.expected_output)
 
 
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
@@ -57,3 +86,99 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def search_needle_in_file_line_dict_test_helper(
+        input_dict: grep.fl_dict_type, needle: str, regex: bool,
+        expected_dict: grep.fl_dict_type) -> bool:
+    return grep.search_needle_in_file_line_dict(input_dict, needle, regex) == expected_dict
+
+
+def test_unit_search_needle_in_file_line_dict():
+    func = search_needle_in_file_line_dict_test_helper
+
+    assert func(
+        {'file1': ['basic test', 'second line'], 'file2': ['another line']},
+        'line', False,
+        {'file1': ['second line'], 'file2': ['another line']}
+    )
+    assert func(
+        {'emptyfile1': [], 'emptyfile2': [], 'file3': ['does not match']},
+        'needle', False,
+        {'emptyfile1': [], 'emptyfile2': [], 'file3': []}
+    )
+    assert func(
+        {'file1': ['', '', ''], 'file2': []},
+        'so empty', False,
+        {'file1': [], 'file2': []}
+    )
+    assert func(
+        {'file1': ['line 1', 'line 2'], 'file2': ['line 2', 'line 3']},
+        '2', False,
+        {'file1': ['line 2'], 'file2': ['line 2']}
+    )
+    assert func(
+        {'file1': ['basic Test', 'second line'], 'file2': ['anOther line']},
+        '[A-Z]', True,
+        {'file1': ['basic Test'], 'file2': ['anOther line']}
+    )
+    assert func(
+        {'emptyfile1': [], 'emptyfile2': [], 'file3': ['does not match']},
+        '[A-Z]', True,
+        {'emptyfile1': [], 'emptyfile2': [], 'file3': []}
+    )
+    assert func(
+        {'file1': ['line1', 'line 2'], 'file2': ['line 2', 'line3']},
+        '\\b\\w{4}\\b', True,
+        {'file1': ['line 2'], 'file2': ['line 2']}
+    )
+
+
+def test_unit_lines_to_numbers():
+    func = grep.lines_to_numbers
+    assert func(
+        {'file1': ['basic test', 'second line'], 'file2': ['another line']}
+    ) == {'file1': ['2'], 'file2': ['1']}
+
+    assert func(
+        {'emptyfile1': [], 'emptyfile2': [], 'file3': ['not empty']}
+    ) == {'emptyfile1': ['0'], 'emptyfile2': ['0'], 'file3': ['1']}
+
+
+def test_unit_print_output_dict(capsys):
+    func = grep.print_output_dict
+
+    func({'file1': ['basic test', 'second line'], 'file2': ['another line']}, '&&f:&&l')
+    output = capsys.readouterr().out
+    assert output == 'file1:basic test\nfile1:second line\nfile2:another line\n'
+
+    func({'file': ['line1', 'line2']}, '&&l')
+    output = capsys.readouterr().out
+    assert output == 'line1\nline2\n'
+
+    func({'file': []}, '&&l')
+    output = capsys.readouterr().out
+    assert output == '\n'
+
+    func({'file': []}, '&&l')
+    output = capsys.readouterr().out
+    assert output == '\n'
+
+
+def test_unit_read_files(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    result = grep.read_files(['b.txt', 'a.txt'])
+    assert result == {
+        'b.txt': ['the needl', 'pref needle suf'],
+        'a.txt': ['pref needle', 'needle suf']
+    }
+
+
+def test_unit_read_stdin(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('pref needle suf\nohlala\n\n'))
+    result = grep.read_stdin()
+    assert result == {
+        '': ['pref needle suf', 'ohlala', '']
+    }
