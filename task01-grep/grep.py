554608954df1fd8680bcd4a04527c60559798905
gt22 <igorengel@mail.ru>
2019-11-20 18:26:06 +0300

    Initial commit

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,97 @@
 #!/usr/bin/env python3
-from typing import List
-import sys
 import re
-import argparse
+import sys
+from argparse import ArgumentParser, Namespace
+from functools import partial
+from typing import List, Tuple, Iterable, Callable
+
+MatchFunc = Callable[[str], bool]
+SearchResult = Tuple[str, List[str]]
+SearchFunc = Callable[[MatchFunc], List[SearchResult]]
+PrintFunc = Callable[[str, SearchResult], None]
 
 
-def main(args_str: List[str]):
-    parser = argparse.ArgumentParser()
+def print_count(fmt: str, found: SearchResult) -> None:
+    name, lines = found
+    print(fmt.format(fmt, name=name, line=len(lines)))
+
+
+def print_lines(fmt: str, found: SearchResult) -> None:
+    name, lines = found
+    for line in lines:
+        print(fmt.format(name=name, line=line.rstrip('\n')))
+
+
+def match_regex(needle: str, line: str) -> bool:
+    return re.search(needle, line) is not None
+
+
+def match_fulltext(needle: str, line: str) -> bool:
+    return needle in line
+
+
+def search(name: str, inp: Iterable[str], matcher: MatchFunc) -> SearchResult:
+    return name, list(filter(matcher, inp))
+
+
+def search_stdin(matcher: MatchFunc) -> List[SearchResult]:
+    return [search('', sys.stdin, matcher)]
+
+
+def search_files(files: List[str], matcher: MatchFunc) -> List[SearchResult]:
+    search_results = []
+    for file in files:
+        with open(file) as f:
+            search_results.append(search(file, f, matcher))
+    return search_results
+
+
+def get_match_func(args: Namespace) -> MatchFunc:
+    # Пайчарм не понимает partial
+    # noinspection PyTypeChecker
+    return partial(match_regex if args.regex else match_fulltext, args.needle)
+
+
+def get_search_func(args: Namespace) -> SearchFunc:
+    func: SearchFunc
+    if args.files:
+        # Если написать это односточным ифом, то пайчарму хорошо
+        # (или, скорее, он не српавляется, и не даёт ворнига), а mypy плохо
+        # Если написать так, то mypy хорошо, а пайчарму плохо...
+        # noinspection PyTypeChecker
+        func = partial(search_files, args.files)
+    else:
+        func = search_stdin
+    return func
+
+
+def get_format(args: Namespace) -> str:
+    if len(args.files) > 1:
+        return '{name}:{line}'
+    else:
+        return '{line}'
+
+
+def get_print_func(args: Namespace) -> PrintFunc:
+    return print_count if args.count else print_lines
+
+
+def main(args_str: List[str]) -> None:
+    parser = ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    parser.add_argument('-c', dest='count', action='store_true')
+    args: Namespace = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    match_func: MatchFunc = get_match_func(args)
+    search_func: SearchFunc = get_search_func(args)
+    fmt: str = get_format(args)
+    print_func: PrintFunc = get_print_func(args)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    search_results: List[SearchResult] = search_func(match_func)
+    for line in search_results:
+        print_func(fmt, line)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,6 +1,150 @@
 #!/usr/bin/env python3
+from typing import List, Tuple
+from argparse import Namespace
 import io
+import re
+from functools import partial
 import grep
+
+
+# UNIT #
+
+
+# Следующие два в своей работе print_name используют. Что с этим лучше всего делать? Замокать?
+def test_unit_print_count(capsys):
+    grep.print_count('{line}', ('NOT_PRINTED', ['a', 'b', 'c', 'd']))
+    grep.print_count('{name}:{line}', ('PRINTED', []))
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\nPRINTED:0\n'
+
+
+def test_unit_print_lines(capsys):
+    grep.print_lines('{line}', ('NOT_PRINTED', ['a', 'b', 'c', 'd']))
+    grep.print_lines('{name}:{line}', ('PRINTED', ['1', '2']))
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a\nb\nc\nd\nPRINTED:1\nPRINTED:2\n'
+
+
+def test_unit_regexp_mach():
+    tests: List[Tuple[str, str, bool]] = [
+        ('cyhm', 'cyhm', True),
+        ('cyhm', 'cyh', False),
+        ('cyhm?', 'cyhm', True),
+        ('cyhm?', 'cyhm?', True),
+        ('cyhm?', 'cyh', True),
+        ('cyhm+', 'cyhmmmmmm', True),
+        ('cyh', 'cyhm', True)
+    ]
+    for pattern, data, answer in tests:
+        assert grep.match_regex(pattern, data) == answer
+
+
+def test_unit_fulltext():
+    tests: List[Tuple[str, str, bool]] = [
+        ('cyhm', 'cyhm', True),
+        ('cyhm', 'cyh', False),
+        ('cyhm?', 'cyhm', False),
+        ('cyhm?', 'cyhm?', True),
+        ('cyhm?', 'cyh', False),
+        ('cyhm+', 'cyhmmmmmm', False),
+        ('cyh', 'cyhm', True)
+    ]
+    for pattern, data, answer in tests:
+        assert grep.match_fulltext(pattern, data) == answer
+
+
+def test_unit_search():
+    test_str = 'Can you hear me?\nAbsolutely\n'
+
+    assert grep.search('test', io.StringIO(test_str), lambda x: 'me' in x) \
+        == ('test', ['Can you hear me?\n'])
+    assert grep.search('test', io.StringIO(test_str), lambda x: re.search('m?e', x) is not None) \
+        == ('test', ['Can you hear me?\n', 'Absolutely\n'])
+
+
+def test_unit_search_stdin(monkeypatch):
+    test_str = 'Can you hear me?\nAbsolutely\n'
+    monkeypatch.setattr('sys.stdin', io.StringIO(test_str))
+
+    assert grep.search_stdin(lambda x: 'me' in x) \
+        == [('', ['Can you hear me?\n'])]
+
+    monkeypatch.setattr('sys.stdin', io.StringIO(test_str))
+
+    assert grep.search_stdin(lambda x: re.search('m?e', x) is not None) \
+        == [('', ['Can you hear me?\n', 'Absolutely\n'])]
+
+
+def test_unit_search_files(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('Can you hear me?\nAbsolutely\n')
+    monkeypatch.chdir(tmp_path)
+
+    assert grep.search_files(['a.txt'], lambda x: 'me' in x) \
+        == [('a.txt', ['Can you hear me?\n'])]
+    assert grep.search_files(['a.txt'], lambda x: re.search('m?e', x) is not None) \
+        == [('a.txt', ['Can you hear me?\n', 'Absolutely\n'])]
+
+
+def test_get_match_func():
+    needle = 'm?e'
+    args_fulltext = Namespace(regex=False, needle=needle)
+    # Whitebox testing: Оно всегда будет partial.
+
+    matcher_fulltext: partial = grep.get_match_func(args_fulltext)  # type: ignore
+    # pylint: disable=no-member
+    assert matcher_fulltext.func is grep.match_fulltext
+    # pylint: disable=no-member
+    assert matcher_fulltext.args == (needle,)
+    # pylint: disable=no-member
+    assert matcher_fulltext.keywords == {}
+
+    args_regex = Namespace(regex=True, needle='m?e')
+    matcher_regex: partial = grep.get_match_func(args_regex)  # type: ignore
+    # pylint: disable=no-member
+    assert matcher_regex.func is grep.match_regex
+    # pylint: disable=no-member
+    assert matcher_regex.args == (needle,)
+    # pylint: disable=no-member
+    assert matcher_regex.keywords == {}
+
+
+def test_get_search_func():
+    args_stdin = Namespace(files=[])
+    search_stdin = grep.get_search_func(args_stdin)
+    # pylint: disable=comparison-with-callable
+    assert search_stdin == grep.search_stdin
+
+    args_files = Namespace(files=['a', 'b'])
+    search_files: partial = grep.get_search_func(args_files)  # type: ignore
+    # pylint: disable=comparison-with-callable
+    assert search_files.func == grep.search_files
+    # pylint: disable=no-member
+    assert search_files.args == (['a', 'b'],)
+    # pylint: disable=no-member
+    assert search_files.keywords == {}
+
+
+def test_get_format():
+    assert grep.get_format(Namespace(files=[])) == '{line}'
+    assert grep.get_format(Namespace(files=['a'])) == '{line}'
+    assert grep.get_format(Namespace(files=['a', 'b'])) == '{name}:{line}'
+
+
+# INTEGRATION #
+
+
+def test_integrate_multifile_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['needle', 'a.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle suf\na.txt:pref needle suf\n'
+
+
+# GIVEN #
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
