b7566ec3dd54d3b0b416110bfb29f9f21093046b
Kirill Karnaukhov <bitikit@gmail.com>
2019-11-21 14:03:42 +0300

    solved

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,117 @@
 #!/usr/bin/env python3
-from typing import List
+"""
+This script simulates GREP and can work with two flags: -E, -c.
+You can get more information if you run it with the flag '--help'
+"""
+from typing import List, Optional
 import sys
 import re
 import argparse
+import os
+
+
+def exclude_nonexistent_files(file_names: List[str]) -> List:
+    """
+    Gets a list of file names and exclude such files that don't exist
+    :param file_names: names of files
+    :return: list of the existing files
+    """
+    new_file_names = []
+    for file_name in file_names:
+        if os.path.isfile(file_name):
+            new_file_names.append(file_name)
+        else:
+            print('No such file: ' + file_name, file=sys.stderr)
+    return new_file_names
+
+
+def read_all_lines(file_names: List[str]) -> List[List[str]]:
+    """
+    Gets a list of file names and return a list of all lines from them (in lists)
+    :param file_names: names of files
+    :return: a list of all lines
+    """
+    all_lines = []
+    for file_name in file_names:
+        with open(file_name, 'r') as input_file:
+            all_lines.append(input_file.readlines())
+    return all_lines
+
+
+def strip_lines(lines: List[str]) -> List[str]:
+    """
+    Strips \n symbol
+    :param lines: the lines to be stripped
+    :return: list of new lines
+    """
+    return [line.rstrip('\n') for line in lines]
+
+
+def filter_matched_lines(lines: List[str], pattern: str, regex_mode: bool) -> List[str]:
+    """
+    Searches for lines that match pattern
+    :param lines: lines to be checked
+    :param pattern: pattern to be matched
+    :param regex_mode: indicates whether it's a regex expression
+    """
+    got_lines = []
+    searcher = re.compile(pattern)
+    for line in lines:
+        matched = searcher.search(line) if regex_mode else pattern in line
+        if matched:
+            got_lines.append(line)
+    return got_lines
+
+
+def print_matched_lines(lines: List[str], stream_name: Optional[str],
+                        counting_mode_on: bool, output_format: str) -> None:
+    """
+    Prints all the matched lines with the format
+    :param lines: lines to be printed
+    :param stream_name: name of the stream we've read from
+    :param counting_mode_on: indicates whether entries should be counted
+    :param output_format: format to be used in printing
+    """
+    opt_name = stream_name + ':' if stream_name else ''
+    if counting_mode_on:
+        outputs = [output_format.format(name=opt_name, counting=len(lines), text='')]
+    else:
+        outputs = [output_format.format(name=opt_name, counting='', text=line) for line in lines]
+    for output in outputs:
+        print(output)
+
+
+def parse_arguments(args_str: List[str]) -> argparse.Namespace:
+    """
+    Gets arguments from the command line and parses them
+    :param args_str: arguments from the command line
+    :return: parsed arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pattern', type=str)
+    parser.add_argument('files', nargs='*')
+    parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='counting', action='store_true')
+    return parser.parse_args(args_str)
 
 
 def main(args_str: List[str]):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    """
+    :param args_str: arguments of the command line
+    """
+    args = parse_arguments(args_str)
+    if args.files:
+        stream_names = exclude_nonexistent_files(args.files)
+        all_lines = read_all_lines(stream_names)
+    else:
+        all_lines = [sys.stdin.readlines()]
+    if len(args.files) <= 1:
+        stream_names = [None]
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    for lines, stream_name in zip(all_lines, stream_names):
+        lines = strip_lines(lines)
+        matched_lines = filter_matched_lines(lines, args.pattern, args.regex)
+        print_matched_lines(matched_lines, stream_name, args.counting, '{name}{counting}{text}')
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,119 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_the_same_files_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'b.txt', 'a.txt', 'b.txt', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\nb.txt:1\nb.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_empty_out(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'first.txt').write_text("what's\nwrong\nwith\nyou?\n")
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'wro*ng?s+', 'first.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+
+
+def test_integrate_files_no_such_file_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['needle', 'b.txt', 'c.txt', 'a.txt', 'd.txt'])
+    out, err = capsys.readouterr()
+    assert err == 'No such file: c.txt\nNo such file: d.txt\n'
+    assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
+def test_unit_exclude_nonexistent_files(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('something')
+    (tmp_path / 'b.txt').write_text('something#2')
+    monkeypatch.chdir(tmp_path)
+    grep.exclude_nonexistent_files(['a.txt', 'not.txt', 'b.txt', 'b.txt', 'd.txt'])
+    out, err = capsys.readouterr()
+    assert err == 'No such file: not.txt\nNo such file: d.txt\n'
+    assert out == ''
+
+
+def test_unit_read_all_lines(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('something\nhello\nhow are you?\n')
+    (tmp_path / 'b.txt').write_text('interesting\nwhat is going on?\n\n')
+    monkeypatch.chdir(tmp_path)
+    read = grep.read_all_lines(['a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert read == [['something\n', 'hello\n', 'how are you?\n'],
+                    ['interesting\n', 'what is going on?\n', '\n']]
+    assert err == ''
+    assert out == ''
+
+
+def test_unit_strip_lines(capsys):
+    lines = ['hello, Anya!\n', '\nHow are you?\n', 'I hope\n you like this hw!\n\n']
+    lines = grep.strip_lines(lines)
+    out, err = capsys.readouterr()
+    assert lines == ['hello, Anya!', '\nHow are you?', 'I hope\n you like this hw!']
+    assert out == ''
+    assert err == ''
+
+
+def test_unit_filter_matched_lines(capsys):
+    lines = ['hello yohoo!', 'how are you?', 'do you want?', 'do u want?']
+    lines = grep.filter_matched_lines(lines, 'you', False)
+    out, err = capsys.readouterr()
+    assert lines == ['how are you?', 'do you want?']
+    assert out == ''
+    assert err == ''
+
+
+def test_unit_filter_matched_lines_regex(capsys):
+    lines = ['hello yohoo!', 'how are you?', 'do you want?', 'do u want?']
+    lines = grep.filter_matched_lines(lines, 'you?', True)
+    out, err = capsys.readouterr()
+    assert lines == ['hello yohoo!', 'how are you?', 'do you want?']
+    assert out == ''
+    assert err == ''
+
+
+def test_unit_print_matched_lines_fir(capsys):
+    lines = ['hello!', 'how are you?']
+    grep.print_matched_lines(lines, 'Hey.txt', False, '{name}{counting}{text}')
+    out, err = capsys.readouterr()
+    assert out == 'Hey.txt:hello!\nHey.txt:how are you?\n'
+    assert err == ''
+
+
+def test_unit_print_matched_lines_sec(capsys):
+    lines = ['hello!', 'how are you?']
+    grep.print_matched_lines(lines, None, True, '{name}{counting}{text}')
+    out, err = capsys.readouterr()
+    assert out == '2\n'
+    assert err == ''
+
+
+def test_unit_get_arguments_first(capsys):
+    args = grep.parse_arguments(['needle', 'a.txt', 'b.txt', 'c.txt', '-E', '-c'])
+    out, err = capsys.readouterr()
+    assert args.pattern == 'needle'
+    assert args.regex
+    assert args.counting
+    assert args.files == ['a.txt', 'b.txt', 'c.txt']
+    assert out == ''
+    assert err == ''
+
+
+def test_unit_get_arguments_second(capsys):
+    args = grep.parse_arguments(['nope'])
+    out, err = capsys.readouterr()
+    assert args.pattern == 'nope'
+    assert not args.regex
+    assert not args.counting
+    assert not args.files
+    assert out == ''
+    assert err == ''
