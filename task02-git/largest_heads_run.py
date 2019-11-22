97ebc228f8a35b760611e85c5e3125dbabfa4a46
Лопатин Михаил <mihan.oi@mail.ru>
2019-11-22 23:29:25 +0500

    the nested loop is placed in a separate function

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 import random
 
+
+def get_max_run(flips: List[int]) -> int:
+    cur_run = 0
+    max_run = 0
+    for flip in flips
+        if flip:
+            cur_run += 1
+            if cur_run > max_run:
+                max_run += 1
+        else:
+            cur_run = 0
+    return max_run
+
+
 ITERS = 1000
 FLIPS = 100
 
@@ -9,19 +23,9 @@ def main():
     random.seed(123456)
     s = 0
     total = 0
-    flips = [random.choice([0, 1]) for _ in range(FLIPS)]
     for _ in range(ITERS):
         total += 1
-        cur_run = 0
-        max_run = 0
-        for flip in flips
-            if flip:
-                cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-            else:
-                cur_run = 0
-        s += max_run
+        s += get_max_run(random.choice([0, 1]) for _ in range(FLIPS))
     print(s, total, s / total)
 
 
