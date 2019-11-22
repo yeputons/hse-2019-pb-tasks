b1b45e5ccbd306f71b8a5a54ee988935c7f44bad
Artem <resign0@yandex.ru>
2019-11-22 21:22:48 +0300

    5 change s and total places

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -24,9 +24,8 @@ def main():
     s = 0
     total = 0
     for _ in range(ITERS):
+        s += get_max_run([random.choice([0, 1]) for _ in range(FLIPS)])
         total += 1
-        max_run = get_max_run([random.choice([0, 1]) for _ in range(FLIPS)])
-        s += max_run
     print(s, total, s / total)
 
 
