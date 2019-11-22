c349bda71a99cda121ea8b6d83dc4b60c9fe5f85
Peter Surkov <petyo333@gmail.com>
2019-11-22 21:03:46 +0300

    Add tests for new flags

---
+++ task01-grep/test_grep_updated.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from argparse import Namespace
+import grep
+
+
+def test_unit_find_in_str():
+    assert grep.find_in_str('str', 'preFStR', ['ignore case'])
+    assert grep.find_in_str('str', 'STRsUf', ['ignore case'])
+    assert grep.find_in_str('str', 'STR', ['ignore case', 'full match'])
+    assert not grep.find_in_str('str', 'stRd', ['ignore case', 'full match'])
+    assert not grep.find_in_str('str', 'ctr', ['ignore case'])
+    assert not grep.find_in_str('str', 'sTk', ['ignore case'])
+
+    assert grep.find_in_str('str', 'STR', ['regex', 'ignore case', 'full_match'])
+    assert grep.find_in_str('a?', 'A', ['regex', 'ignore case', 'full match'])
+    assert grep.find_in_str('aB.*ef', 'AbCdeF', ['regex', 'ignore case'])
+    assert not grep.find_in_str('aBa+', 'abcBAA', ['regex', 'ignore case'])
+    assert not grep.find_in_str('aBa+', 'aaba+', ['regex', 'ignore case', 'full match'])
+
+
+def test_unit_get_matching_args():
+    assert grep.get_matching_args(Namespace(regex=False, ignore_case=True, full_match=False)) == ['ignore case']
+    assert grep.get_matching_args(Namespace(regex=False, ignore_case=False, full_match=True)) == ['full match']
+
+
+def test_integrate_all_keys_print_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt\n'
+
+
+def test_integrate_all_keys_print_not_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-Livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\n'
+
+
+def test_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-civx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:3\na.txt:0\n'
