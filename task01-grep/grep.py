5aa5fbb71a88cb84f4d3787680044182a650aeb4
Jane <grindevald666@gmail.com>
2019-11-22 18:29:31 +0300

    Commiting grep.py

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,55 @@
 #!/usr/bin/env python3
 from typing import List
+from typing import IO
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
+def parse_args(args_str: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='counting', action='store_true')
     args = parser.parse_args(args_str)
+    return args
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
+
+def making_list(needle: str, stream: IO[str]) -> List[str]:
+    lines: List[str] = []
+    for line in stream:
         line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+        if re.search(needle, line):
+            lines.append(line)
+    return lines
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+
+def search_in_file(files: List[str], needle: str, counting: bool) -> None:
+    for file in files:
+        with open(file, 'r') as in_file:
+            lines = making_list(needle, in_file)
+        lines = [str(len(lines))] if counting else lines
+        if len(files) > 1:
+            lines = [f'{file}:{line}' for line in lines]
+        for line in lines:
+            print(line)
+
+
+def search_in_stdin(needle: str, counting: bool) -> None:
+    lines = making_list(needle, sys.stdin)
+    lines = [str(len(lines))] if counting else lines
+    for line in lines:
+        print(line)
+
+
+def main(args_str: List[str]) -> None:
+    args = parse_args(args_str)
+    needle = args.needle if args.regex else re.escape(args.needle)
+    if args.files:
+        search_in_file(args.files, needle, args.counting)
+    else:
+        search_in_stdin(needle, args.counting)
 
 
 if __name__ == '__main__':
