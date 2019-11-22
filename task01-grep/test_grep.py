662e08deac97251a7cb45d6c8625d23d90bb2de2
YoRHaUnitA0 <39829376+YoRHaUnitA0@users.noreply.github.com>
2019-11-22 21:54:46 +0300

    Bad, good and ugly quotes got fixed.

--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -67,8 +67,8 @@ def test_parse_count(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    files = ["a.txt", "b.txt"]
-    parse_count("needle", files)
+    files = ['a.txt', 'b.txt']
+    parse_count('needle', files)
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'a.txt:2\nb.txt:1\n'
@@ -78,8 +78,8 @@ def test_parse_files(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    files = ["a.txt", "b.txt"]
-    parse_files("needle", files)
+    files = ['a.txt', 'b.txt']
+    parse_files('needle', files)
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'a.txt:pref needle\na.txt:needle suf\nb.txt:pref needle suf\n'
@@ -88,7 +88,7 @@ def test_parse_files(tmp_path, monkeypatch, capsys):
 def test_parse_std(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    parse_std("needle")
+    parse_std('needle')
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
@@ -98,7 +98,7 @@ def test_parse_e_flag(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
     files = []
-    parse_e_flag("needle", files)
+    parse_e_flag('needle', files)
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
