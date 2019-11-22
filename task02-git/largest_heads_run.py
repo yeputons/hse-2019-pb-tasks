95b516c272f93c630274186d4536fe15db5a9005
andreyshspb <andrey.shein.spb@gmail.com>
2019-11-22 19:04:16 +0300

    get_max_run function was simplified by adding max function

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -13,10 +13,9 @@ def get_max_run(flips: List[int]) -> int:
     for flip in flips:
         if flip:
             cur_run += 1
-            if cur_run > max_run:
-                max_run += 1
         else:
             cur_run = 0
+        max_run = max(max_run, cur_run)
     return max_run
 
 
