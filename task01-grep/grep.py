3c4346abcf762ea22ef540f339a8f551a73ef56c
Никита Стоцкий <siqize228@gmail.com>
2019-11-23 02:28:01 +0300

    sent corrections

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,77 @@
 #!/usr/bin/env python3
 from typing import List
+from pathlib import Path
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+def check_line(line: str, pattern: str, is_regex: bool):
+    if is_regex:
+        if re.search(pattern, line):
+            return True
+        else:
+            return False
+    else:
+        if pattern in line:
+            return True
+        else:
+            return False
 
-    # STUB BEGINS
+
+def print_lines(lines: List[str], filename: str, print_name: bool, print_number: bool):
+    for x in range(len(lines)):
+        if not print_number:
+            if print_name:
+                print(f'{filename}:{lines[x]}')
+            else:
+                print(lines[x])
+        else:
+            if print_name:
+                print(f'{filename}:{len(lines)}')
+                break
+            else:
+                print(f'{len(lines)}')
+                break
+
+
+def input_from_files(files: List[str], pattern: str, print_number: bool, is_regex: bool):
+    lines = []
+    print_name = bool(len(files) > 1)
+    for filename in files:
+        if Path(filename).is_file():
+            with open(filename, 'r') as in_file:
+                for line in in_file.readlines():
+                    line = line.rstrip('\n')
+                    if check_line(line, pattern, is_regex):
+                        lines.append(f'{line}')
+                print_lines(lines, filename, print_name, print_number)
+                lines.clear()
+
+
+def input_from_stdin(pattern: str, print_number: bool, is_regex: bool):
+    lines = []
     for line in sys.stdin.readlines():
         line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+        if check_line(line, pattern, is_regex):
+            lines.append(line)
+    if print_number:
+        print(f'{len(lines)}')
+    else:
+        print(*lines, sep="\n")
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+
+def main(args_str: List[str]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pattern', type=str)
+    parser.add_argument('files', nargs='*')
+    parser.add_argument('-c', dest='print_number', action='store_true')
+    parser.add_argument('-E', dest='is_regex', action='store_true')
+    args = parser.parse_args(args_str)
+    if len(args.files) == 0:
+        input_from_stdin(args.pattern, args.print_number, args.is_regex)
+    else:
+        input_from_files(args.files, args.pattern, args.print_number, args.is_regex)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,183 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test2_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle?', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:2\na.txt:2\n'
+
+
+def test_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test2_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['needle', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\na.txt:needle suf\nb.txt:pref needle suf\n'
+
+
+def test3_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:1\n'
+
+
+def test_unit_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt']
+    grep.input_from_files(files, 'needle', True, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test2_unit_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt']
+    grep.input_from_files(files, 'needle', False, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle\nneedle suf\n'
+
+
+def test3_unit_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt']
+    grep.input_from_files(files, 'needle', True, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test4_unit_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    grep.input_from_files(files, 'needle', True, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:1\n'
+
+
+def test5_unit_input_from_files_without_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    grep.input_from_files(files, 'needle', False, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\na.txt:needle suf\nb.txt:pref needle suf\n'
+
+
+def test_unit_input_from_files_with_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt']
+    grep.input_from_files(files, 'needle?', False, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
+
+
+def test2_unit_input_from_files_with_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt']
+    grep.input_from_files(files, 'needle?', True, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
+def test3_unit_input_from_files_with_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    (tmp_path / 'b.txt').write_text('pref needl\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    grep.input_from_files(files, 'needle?', True, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:4\nb.txt:1\n'
+
+
+def test4_unit_input_from_files_with_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\n')
+    (tmp_path / 'b.txt').write_text('pref needl\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    grep.input_from_files(files, 'needle?', False, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:the needl\nb.txt:pref needl\n'
+
+
+def test5unit_input_from_files_with_e(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the need\n')
+    (tmp_path / 'b.txt').write_text('pref need\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    grep.input_from_files(files, 'needle?', False, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+
+
+def test_unit_input_from_stdin_without_e(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nneedle suf\nthe needl\npref needle suf'))
+    grep.input_from_stdin('needle', True, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test2_unit_input_from_stdin_without_e(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nneedle suf\nthe needl\npref needle suf'))
+    grep.input_from_stdin('needle', False, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle\nneedle suf\npref needle suf\n'
+
+
+def test_unit_input_from_stdin_with_e(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    grep.input_from_stdin('needle?', False, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
+
+
+def test2_unit_input_from_stdin_with_e(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    grep.input_from_stdin('needle?', True, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
