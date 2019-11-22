d8e07dac1aa5a4bfebe04de67bf95cdf7d847589
YoRHaUnitA0 <39829376+YoRHaUnitA0@users.noreply.github.com>
2019-11-22 19:49:05 +0300

    Loaded own grep.py
    TODO:
    - moar tests

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -5,25 +5,87 @@ import re
 import argparse
 
 
-def main(args_str: List[str]):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+def parse_count(args):
 
-    # STUB BEGINS
+    if len(args.files) == 0:
+        counter = 0
+        for line in sys.stdin.readlines():
+            line = line.rstrip('\n')
+            if args.needle in line:
+                counter += 1
+        print(counter)
+    else:
+        for f in args.files:
+            counter = 0
+            with open(f, 'r') as in_file:
+                for line in in_file.readlines():
+                    line = line.rstrip('\n')
+                    if args.needle in line:
+                        counter += 1
+            print(f"{f}:{counter}")
+
+
+def parse_files(args):
+    for f in args.files:
+        with open(f, 'r') as in_file:
+            for line in in_file.readlines():
+                line = line.rstrip('\n')
+                if args.needle in line:
+                    if len(args.files) > 1:
+                        print(f"{f}:{line}")
+                    else:
+                        print(line)
+
+
+def parse_std(needle):
     for line in sys.stdin.readlines():
         line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+        if needle in line:
+            print(line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
+
+def parse_e_flag(args):
+
+    if len(args.files) == 0:
+        for line in sys.stdin.readlines():
             line = line.rstrip('\n')
             if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+                print(line)
+    else:
+        for f in args.files:
+            with open(f, 'r') as in_file:
+                for line in in_file.readlines():
+                    line = line.rstrip('\n')
+                    if re.search(args.needle, line):
+                        if len(args.files) > 1:
+                            print(f"{f}:{line}")
+                        else:
+                            print(line)
+
+
+def my_grep(args):
+
+    if args.count:
+        parse_count(args)
+    else:
+        if args.regex:
+            parse_e_flag(args)
+        else:
+            if len(args.files) == 0:
+                parse_std(args.needle)
+            else:
+                parse_files(args)
+
+
+def main(args_str: List[str]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
+    parser.add_argument('needle', type=str)
+    parser.add_argument('files', nargs='*')
+    args = parser.parse_args(args_str)
+
+    my_grep(args)
 
 
 if __name__ == '__main__':
