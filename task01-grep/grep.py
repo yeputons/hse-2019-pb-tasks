b229f9aae05a2c8a898a7bfcea895734d34b13e8
Mikhaylova Anna <annamikhaylova6263@gmail.com>
2019-11-22 22:42:23 +0300

    hw version 2.0

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,55 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, Pattern
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
+def parse(args_str: List[str]):
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    parser.add_argument('-c', dest='count_format', action='store_true')
+    return parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+def read_from_file(filename: str):
+    with open(filename, 'r') as file:
+        return [line for line in file.readlines()]
+
+
+def find_matching_lines(lines: List[str], pattern: str, regex: bool):
+    def match(line: str, regular_expression: Pattern):
+        return re.search(regular_expression, line)
+    if not regex:
+        pattern = re.escape(pattern)
+    regular_expression = re.compile(pattern)
+    matched = [line for line in lines if match(line, regular_expression)]
+    return matched
+
+
+def print_matched(matched: List[str], need_filenames: bool = False, filename: str = None):
+    for line in matched:
+        if need_filenames:
+            print(filename, end=':')
+        print(line)
+
+
+def main(args_str: List[str]):
+    args = parse(args_str)
+
+    if args.files:
+        for filename in args.files:
+            input_lines = [line.strip('\n') for line in read_from_file(filename)]
+            matched = find_matching_lines(input_lines, args.needle, args.regex)
+            formatted = [str(len(matched))] if args.count_format else [line for line in matched]
+            print_matched(formatted, len(args.files) > 1, filename)
+    else:
+        input_lines = [line.strip('\n') for line in sys.stdin.readlines()]
+        matched = find_matching_lines(input_lines, args.needle, args.regex)
+        formatted = [str(len(matched))] if args.count_format else [line for line in matched]
+        print_matched(formatted)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,186 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_grep_read_from_file1(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    out = grep.read_from_file('a.txt')
+    assert out == ['pref needle\n', 'needle suf\n']
+
+
+def test_grep_read_from_file2(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    monkeypatch.chdir(tmp_path)
+    out = grep.read_from_file('a.txt')
+    assert out == ['pref needle?\n', 'needle? suf\n', 'the needl\n', 'pref needle? suf']
+
+
+def test_grep_parse1():
+    in_parse = ['-c', '-E', 'needle', 'b.txt', 'a.txt']
+    out = grep.parse(in_parse)
+    assert out.needle == 'needle'
+    assert out.files == ['b.txt', 'a.txt']
+    assert out.count_format
+    assert out.regex
+
+
+def test_grep_parse2():
+    in_parse = ['-c', 'needle', 'b.txt', 'a.txt']
+    out = grep.parse(in_parse)
+    assert out.needle == 'needle'
+    assert out.files == ['b.txt', 'a.txt']
+    assert out.count_format
+    assert not out.regex
+
+
+def test_grep_parse3():
+    in_parse = ['-E', 'needle', 'b.txt', 'a.txt']
+    out = grep.parse(in_parse)
+    assert out.needle == 'needle'
+    assert out.files == ['b.txt', 'a.txt']
+    assert not out.count_format
+    assert out.regex
+
+
+def test_grep_parse4():
+    in_parse = ['-E', 'needle']
+    out = grep.parse(in_parse)
+    assert out.needle == 'needle'
+    assert out.files == []
+    assert not out.count_format
+    assert out.regex
+
+
+def test_grep_parse5():
+    in_parse = ['needle']
+    out = grep.parse(in_parse)
+    assert out.needle == 'needle'
+    assert out.files == []
+    assert not out.count_format
+    assert not out.regex
+
+
+def test_grep_find_substr_count_one_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'aba', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test_grep_find_regex_count_one_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'b.txt').write_text('ababaca\nnabca')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'ab.*a', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test_grep_find_substr_one_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['aba', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'aba\nabaca\nraba\n'
+
+
+def test_grep_find_regex_one_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'ab.+a', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'abaca\nfabca nabc\n'
+
+
+def test_grep_find_substr_count1(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    (tmp_path / 'b.txt').write_text('ababaca\nnabca')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'aba', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:3\nb.txt:1\n'
+
+
+def test_grep_find_substr_count2(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    (tmp_path / 'b.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'aba', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:3\nb.txt:0\n'
+
+
+def test_grep_find_regex_count1(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    (tmp_path / 'b.txt').write_text('ababaca\nnabca')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'ab.*a', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:4\nb.txt:2\n'
+
+
+def test_grep_find_regex_count2(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    (tmp_path / 'b.txt').write_text('ababaca\nnabca')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'ab.+a', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:2\n'
+
+
+def test_grep_find_substr1(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    (tmp_path / 'b.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['aba', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:aba\na.txt:abaca\na.txt:raba\n'
+
+
+def test_grep_find_regex1(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('aba\nabaca\nfabca nabc\nraba')
+    (tmp_path / 'b.txt').write_text('ababaca\nnabca\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'ab.+a', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:abaca\na.txt:fabca nabc\nb.txt:ababaca\nb.txt:nabca\n'
+
+
+def test_grep_print_matched1(capsys):
+    grep.print_matched(['helpme', 'sos'], False, 'a.txt')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'helpme\nsos\n'
+
+
+def test_grep_print_matched2(capsys):
+    grep.print_matched(['helpme', 'sos'], True, 'a.txt')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:helpme\na.txt:sos\n'
+
+
+def test_grep_print_matched3(capsys):
+    grep.print_matched([2], False, 'a.txt')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test_grep_print_matched4(capsys):
+    grep.print_matched([2], True, 'a.txt')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\n'
