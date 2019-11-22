4e60b73ad02fb9c9834340e41025422bec41f8b7
YoRHaUnitA0 <39829376+YoRHaUnitA0@users.noreply.github.com>
2019-11-22 21:46:22 +0300

    Initial commit for tests.
    Might be fixed.

--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import io
-import grep
+from .grep import main
+from .grep import parse_count
+from .grep import parse_e_flag
+from .grep import parse_files
+from .grep import parse_std
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    grep.main(['needle?'])
+    main(['needle?'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
@@ -15,7 +19,7 @@ def test_integrate_stdin_grep(monkeypatch, capsys):
 def test_integrate_stdin_regex_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    grep.main(['-E', 'needle?'])
+    main(['-E', 'needle?'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
@@ -24,7 +28,7 @@ def test_integrate_stdin_regex_grep(monkeypatch, capsys):
 def test_integrate_stdin_grep_count(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle\nneedle suf\nthe needl\npref needle suf'))
-    grep.main(['-c', 'needle'])
+    main(['-c', 'needle'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == '3\n'
@@ -33,7 +37,7 @@ def test_integrate_stdin_grep_count(monkeypatch, capsys):
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    grep.main(['needle', 'a.txt'])
+    main(['needle', 'a.txt'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle suf\n'
@@ -43,7 +47,7 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    grep.main(['needle', 'b.txt', 'a.txt'])
+    main(['needle', 'b.txt', 'a.txt'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
@@ -53,7 +57,48 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    grep.main(['-c', 'needle', 'b.txt', 'a.txt'])
+    main(['-c', 'needle', 'b.txt', 'a.txt'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_parse_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    files = ["a.txt", "b.txt"]
+    parse_count("needle", files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:1\n'
+
+
+def test_parse_files(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    files = ["a.txt", "b.txt"]
+    parse_files("needle", files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\na.txt:needle suf\nb.txt:pref needle suf\n'
+
+
+def test_parse_std(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    parse_std("needle")
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
+
+
+def test_parse_e_flag(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    files = []
+    parse_e_flag("needle", files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
