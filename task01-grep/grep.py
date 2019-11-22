9085bc037778bb3f89f2dc7c6a401b17d85754ce
Курносова Полина <you@example.com>
2019-11-22 23:39:46 +0300

    first try on grep

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,76 @@
 #!/usr/bin/env python3
-from typing import List
+
+from typing import List, Callable
 import sys
 import re
 import argparse
 
 
+def names_to_strings(filenames: List[str]) -> List[List[str]]:
+    res = []
+    for name in filenames:
+        with open(name, 'r') as file:
+            res.append([string.rstrip('\n') for string in file.readlines()])
+    return res
+
+
+def stdin_to_strings() -> List[List[str]]:
+    return [[string.rstrip('\n') for string in sys.stdin.readlines()]]
+
+
+def match_string(string: str, pattern: str) -> bool:
+    return pattern in string
+
+
+def match_regex(string: str, pattern: str) -> bool:
+    return bool(re.search(pattern, string))
+
+
+def print_lines(strings: List[str], filename: str, is_alone: bool) -> None:
+    for string in strings:
+        if is_alone:
+            print(string)
+        else:
+            print(f'{filename}:{string}')
+
+
+def print_count(strings: List[str], filename: str, is_alone: bool) -> None:
+    if is_alone:
+        print(len(strings))
+    else:
+        print(f'{filename}:{len(strings)}')
+
+
+def find_matched(file: List[str], pattern: str, match_f: Callable[[str, str], bool]) -> List[str]:
+    return list(filter(lambda string: match_f(string, pattern), file))
+
+
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
+    parser.add_argument('pattern', type=str)
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='print_count', action='store_true')
+    parser.add_argument('files', nargs='*')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    filenames = args.files
+    files = names_to_strings(filenames)
+    if not filenames:
+        files = stdin_to_strings()
+        filenames = ['sys.stdin']
+    match_f = match_string
+    if args.regex:
+        match_f = match_regex
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    matched_strings = [find_matched(
+        file, args.pattern, match_f) for file in files]
+
+    print_function = print_lines
+    if args.print_count:
+        print_function = print_count
+
+    for matches, name in zip(matched_strings, filenames):
+        print_function(matches, name, len(filenames) == 1)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,6 +1,81 @@
 #!/usr/bin/env python3
+
 import io
 import grep
+
+
+def test_names_to_strings(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('ouou\noiio')
+    (tmp_path / 'b.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    tested_function = grep.names_to_strings(['a.txt', 'b.txt'])
+    _, err = capsys.readouterr()
+    assert err == ''
+    assert tested_function == [['ouou', 'oiio'], []]
+
+
+def test_stdin_files_to_strings(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'ouou\noiio'))
+    tested_function = grep.stdin_to_strings()
+    _, err = capsys.readouterr()
+    assert err == ''
+    assert tested_function == [['ouou', 'oiio']]
+
+
+def test_match_string():
+    assert grep.match_string('iwanttodie', 'die')
+    assert grep.match_string('yuyewuoda', 'wu')
+    assert grep.match_string('', '')
+    assert not grep.match_string('8780000', '788')
+    assert not grep.match_string('8', '788')
+    assert not grep.match_string('too', 'tootoo')
+
+
+def test_match_regex():
+    assert grep.match_regex('io', '')
+    assert grep.match_regex('io', '[a-q][b-z]')
+    assert grep.match_regex('8', '[0-9]')
+    assert not grep.match_regex('trytofind', '[0-9]')
+    assert not grep.match_regex('800', '[a-u]')
+
+
+def test_print_lines(capsys):
+    grep.print_lines(['l1', 'l2'], 'iamtheonlyone', True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'l1\nl2\n'
+
+    grep.print_lines(['line1', 'line2'], 'iamoneoffiles', False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'iamoneoffiles:line1\niamoneoffiles:line2\n'
+
+
+def test_print_count(capsys):
+    grep.print_count(['7', '8'], 'alone', True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+    grep.print_count(['7', '8'], 'notalone', False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'notalone:2\n'
+
+
+def test_find_matched(capsys):
+    matched_strings = grep.find_matched(['oi', 'io', '90'],
+                                        'o', grep.match_string)
+    _, err = capsys.readouterr()
+    assert err == ''
+    assert matched_strings == ['oi', 'io']
+
+    matched_strings = grep.find_matched(['7', '0', 'aoao'],
+                                        '[0-9]', grep.match_regex)
+    _, err = capsys.readouterr()
+    assert err == ''
+    assert matched_strings == ['7', '0']
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
