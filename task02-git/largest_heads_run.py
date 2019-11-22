767788cf64947ebb2cb98daf96028c30aba0e036
Dmitry Koldunovich <koldundrad@mail.ru>
2019-11-22 22:08:36 +0300

    S in outer cycle

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -19,9 +19,9 @@ def main():
                 cur_run += 1
                 if cur_run > max_run:
                     max_run += 1
-                    s += 1
-            else:
-                cur_run = 0
+                else:
+                    cur_run = 0
+                s += 1
     print(s, total, s / total)
 
 
