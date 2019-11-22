45ca894f270b7e37db0e3e1bd3fb7a35b98943d4
Янин Василий <vasilii_yanin@mail.ru>
2019-11-23 02:09:54 +0300

    fuction 'get_max_run' optimized

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 from typing import List
 import random
- 
+
+
 def get_max_run(flips: List[int]) -> int:
-        cur_run = 0
-        max_run = 0
-        for flip in flips:
-            if flip:
-                cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-            else:
-                cur_run = 0
-        return max_run
- 
+    cur_run = 0
+    max_run = 0
+    for flip in flips:
+        if flip:
+            cur_run += 1
+        else:
+            cur_run = 0
+        max_run = max(max_run, cur_run)
+    return max_run
+
+
 ITERS = 1000
 FLIPS = 100
- 
- 
+
+
 def main():
     random.seed(123456)
     s = 0
@@ -26,7 +27,7 @@ def main():
         total += 1
         s += get_max_run(random.choice([0, 1]) for _ in range(FLIPS))
     print(s, total, s / total)
- 
- 
+
+
 if __name__ == '__main__':
     main()
