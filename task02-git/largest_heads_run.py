5ba9efa4a1b44c6d49c749fb170f802bc2989009
Artem <resign0@yandex.ru>
2019-11-22 21:18:17 +0300

    3 put second for in function

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
+from typing import List
 import random
+
+def get_max_run(flips: List[int]) -> int:
+    cur_run = 0
+    max_run = 0
+    for flip in flips:
+        if flip:
+            cur_run += 1
+            if cur_run > max_run:
+                max_run += 1
+        else:
+            cur_run = 0
+    return max_run
 
 ITERS = 1000
 FLIPS = 100
-
 
 def main():
     random.seed(123456)
@@ -11,16 +23,7 @@ def main():
     total = 0
     for _ in range(ITERS):
         total += 1
-        cur_run = 0
-        max_run = 0
-        flips = [random.choice([0, 1]) for _ in range(FLIPS)]
-        for flip in flips:
-            if flip:
-                cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-            else:
-                cur_run = 0
+        max_run = get_max_run([random.choice([0, 1]) for _ in range(FLIPS)])
         s += max_run
     print(s, total, s / total)
 
