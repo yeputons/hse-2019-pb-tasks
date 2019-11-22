ee11c443e968ec1861c7b76f44d52afc8c4591d1
Jane <grindevald666@gmail.com>
2019-11-22 18:30:12 +0300

    Commiting test_grep.py

--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,74 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_making_list_files(tmp_path, monkeypatch):
+    needle = 'needle'
+    p = tmp_path / 'a.txt'
+    p.write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    with p.open() as f:
+        lines = grep.making_list(needle, f)
+        assert lines == ['pref needle suf']
+
+
+def test_making_list_in_stdin():
+    output = io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    needle = 'needle'
+    lines = grep.making_list(needle, output)
+    assert lines == ['pref needle?', 'needle? suf', 'pref needle? suf']
+
+
+def test_search_in_file_one_file(tmp_path, monkeypatch, capsys):
+    needle = 'needle'
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt']
+    counting = 0
+    grep.search_in_file(files, needle, counting)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle suf\n'
+    counting = 1
+    grep.search_in_file(files, needle, counting)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_search_in_file_many_files(tmp_path, monkeypatch, capsys):
+    needle = 'needle'
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    counting = 0
+    grep.search_in_file(files, needle, counting)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\na.txt:needle suf\nb.txt:pref needle suf\n'
+    counting = 1
+    grep.search_in_file(files, needle, counting)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:1\n'
+
+
+def test_search_in_stdin(monkeypatch, capsys):
+    needle = 'needle'
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    counting = 0
+    grep.search_in_stdin(needle, counting)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    counting = 1
+    grep.search_in_stdin(needle, counting)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
