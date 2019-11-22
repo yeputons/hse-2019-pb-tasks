21592adbc537a5a68c20feda620e06474aa4b867
chizhova marija aleksandrovna <mariachizhova17@gmail.com>
2019-11-22 16:51:50 +0300

    Unit tests implemented.

--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,67 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_count_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_stdin_grep_count_regex(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nneedle suf\nthe needl\npref needle suf'))
+    grep.main(['-c', '-E', 'needle'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test_integrate_files_grep_count_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_print_result(tmp_path, monkeypatch, capsys):
+    monkeypatch.chdir(tmp_path)
+    grep.print_result(['b.txt:1', 'a.txt:2'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_strip_lines(capsys):
+    lines = ['London\n', '\nis\n', 'the capital\n of Great Britain.\n\n']
+    lines = grep.strip_lines(lines)
+    out, err = capsys.readouterr()
+    assert lines == ['London', '\nis', 'the capital\n of Great Britain.']
+    assert out == ''
+    assert err == ''
+
+
+def test_filter_lines():
+    lines = ['aba', 'bcd', 'banana']
+    assert grep.filter_lines(False, 'a', lines) == ['aba', 'banana']
+    assert grep.filter_lines(False, 'b', lines) == ['aba', 'bcd', 'banana']
+    assert grep.filter_lines(True, 'a*b', lines) == ['aba', 'bcd', 'banana']
+    assert grep.filter_lines(False, 'a*b', lines) == []
+
+
+def test_grep_lines():
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', False, False) == ['file.txt:abc']
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', False, True) == ['file.txt:1']
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', True, True) == ['file.txt:1']
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', True, False) == ['file.txt:abc']
+
+
+def test_format_lines():
+    assert grep.format_lines('abc.txt', 'Hello world!') == 'abc.txt:Hello world!'
