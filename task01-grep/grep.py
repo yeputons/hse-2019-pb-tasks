2a66738e6fe52900b80cb41721fd171cc7a02e0c
kuzyaross <nkozubaeff@gmail.com>
2019-11-22 21:35:20 +0300

    fixed solutions v3.0

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,30 +1,1 @@
-#!/usr/bin/env python3
-from typing import List
-import sys
-import re
-import argparse
-
-
-def main(args_str: List[str]):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
-
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
-
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+#!/usr/bin/env python3from typing import Listimport sysimport reimport argparsefrom pathlib import Pathdef exists_in_line(pattern: str, line: str, is_regex: bool) -> bool:    if is_regex:        return bool(re.search(pattern, line))    else:        return pattern in linedef search_in_lines(lines_to_search_from: List[str], is_regex: bool,                    pattern: str) -> List[str]:    found_lines: List[str] = []    for line in lines_to_search_from:        line = line.rstrip('\n')        if exists_in_line(pattern, line, is_regex):            found_lines.append(line)    return found_linesdef print_line(line: str, prefix: str):    if len(prefix) > 0:        print(prefix, end=':')    print(line)def print_results(prefix: str, is_count: bool, found_lines: List[str]):    if is_count:        print_line(str(len(found_lines)), prefix)    else:        for line in found_lines:            print_line(line, prefix)def find_pattern(files: List[str], is_count: bool, is_regex: bool,                 pattern: str):    prefix = ''    found_lines: List[str]    if len(files) > 0:        for file_name in files:            p = Path.cwd()            file = p.joinpath(file_name)            if file.exists() and file.is_file():                with file.open() as in_file:                    found_lines = search_in_lines(in_file.readlines(),                                                  is_regex, pattern)                    if len(files) > 1:                        prefix = file_name                    print_results(prefix, is_count, found_lines)            else:                print(f'Can not open a file:{file_name}. Error has occurred')    else:        found_lines = search_in_lines(sys.stdin.readlines(), is_regex, pattern)        print_results(prefix, is_count, found_lines)def main(args_str: List[str]):    parser = argparse.ArgumentParser(description='searches for "needle" and prints it')    parser.add_argument('needle', type=str, help='regular expression or str to search')    parser.add_argument('files', nargs='*', help='files to search from')    parser.add_argument('-E', dest='regex', action='store_true', help='search regular expression')    parser.add_argument('-c', action='store_true', help='count')    args = parser.parse_args(args_str)    find_pattern(args.files, args.c, args.regex, args.needle)if __name__ == '__main__':    main(sys.argv[1:])
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from typing import List
 import io
 import grep
 
@@ -57,3 +58,126 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('ahhe\nhhhline\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'h+?', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'ahhe\nhhhline\n'
+
+
+def test_integrate_files_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('Higher school of\nkekonomics\n')
+    (tmp_path / 'b.txt').write_text('Love\nthis one\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'o+', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:Higher school of\na.txt:kekonomics\nb.txt:Love\nb.txt:this one\n'
+
+
+def test_integrate_files_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('Some prefix 00\n')
+    (tmp_path / 'b.txt').write_text('Another\nneedle\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'o+', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:1\n'
+
+
+def test_exists_in_line_empty():
+    assert not grep.exists_in_line('some text', '', False)
+
+
+def test_exists_in_line_regex():
+    assert grep.exists_in_line('.o+', 'file.o', True)
+    assert not grep.exists_in_line('.exe+', 'a.bin', True)
+
+
+def test_search_in_lines_func():
+    lines: List[str] = ['Line 1', 'Line 2', 'Line 3']
+    assert grep.search_in_lines(lines, False, '2') == ['Line 2']
+
+
+def test_search_in_lines_regex():
+    lines: List[str] = ['Line 1', 'Line 2', 'Line 3']
+    assert grep.search_in_lines(lines, True, 'i+n') == ['Line 1', 'Line 2', 'Line 3']
+
+
+def test_search_in_lines_empty():
+    lines: List[str] = []
+    assert grep.search_in_lines(lines, False, 'pattern') == []
+
+
+def test_print_line_func_prefix_empty(capsys):
+    grep.print_line('0', '')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '0\n'
+
+
+def test_print_line_func(capsys):
+    grep.print_line('Some line', 'a.txt')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:Some line\n'
+
+
+def test_print_result_function(capsys):
+    lines: List[str] = ['Line 1']
+    grep.print_results('a.txt', False, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:Line 1\n'
+
+
+def test_print_result_function_empty(capsys):
+    lines: List[str] = []
+    grep.print_results('', True, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '0\n'
+
+
+def test_print_result_function_count(capsys):
+    lines: List[str] = ['line 1', 'line 2']
+    grep.print_results('', True, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test_print_result_function_count_len(capsys):
+    lines: List[str] = ['line 1', 'line 2']
+    grep.print_results('a.txt', True, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\n'
+
+
+def test_file_search_no_file(capsys):
+    grep.find_pattern(['a.txt'], False, True, 'h+?')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'Can not open a file:a.txt. Error has occurred\n'
+
+
+def test_files_search(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'b.txt').write_text('Some stuff\n')
+    monkeypatch.chdir(tmp_path)
+    grep.find_pattern(['b.txt'], False, False, 'stuff')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'Some stuff\n'
+
+
+def test_console_search_empty(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(''))
+    out, err = capsys.readouterr()
+    grep.find_pattern([], False, False, 'str')
+    assert err == ''
+    assert out == ''
