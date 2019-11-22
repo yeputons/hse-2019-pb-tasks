8c9878f1d889eda39dc532273d994cdef375ede6
nani.desu.ka <55149626+nani-desu-ka@users.noreply.github.com>
2019-11-22 16:01:15 +0300

    Add files via upload

---
+++ grep_test.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import io
+import grep
+
+
+def test_integrate_stdin_grep(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    grep.main(['needle?'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
+
+
+def test_integrate_stdin_regex_grep(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    grep.main(['-E', 'needle?'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
+
+
+def test_integrate_stdin_grep_count(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nneedle suf\nthe needl\npref needle suf'))
+    grep.main(['-c', 'needle'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle suf\n'
+
+
+def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['needle', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
+def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle\n'
+
+
+def test_integrate_files_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\n')
+    (tmp_path / 'b.txt').write_text('the needle\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\nb.txt:the needle\n'
+
+
+def test_integrate_file_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\n'
+
+
+def test_integrate_files_grep_regex_failed(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\n')
+    (tmp_path / 'b.txt').write_text('the needl\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\n'
