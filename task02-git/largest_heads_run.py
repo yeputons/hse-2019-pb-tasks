d37de53c1325dd48b5de3d8df85f544fd4772c70
KKD <kirill.kreslavski@gmail.com>
2019-11-22 19:58:09 +0300

    Variables s and total are mow being updated in another order in loop

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -24,8 +24,8 @@ def main():
     s = 0
     total = 0
     for _ in range(ITERS):
+        s += get_max_run(random.choice([0, 1]) for _ in range(FLIPS))
         total += 1
-        s += get_max_run([random.choice([0, 1]) for _ in range(FLIPS)])
     print(s, total, s / total)
 
 
