8748587800ec24af87741c2dbf23bbb6e50d2b18
Vladislav Nosivskoy <vladnosiv@gmail.com>
2019-11-22 18:01:11 +0300

    add simple max solution

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -9,16 +9,13 @@ FLIPS = 100
 def get_max_run(flips: List[int]) -> int:
     cur_run = 0
     max_run = 0
-    s = 0
     for flip in flips:
         if flip:
             cur_run += 1
-            if cur_run > max_run:
-                max_run += 1
-                s += 1
         else:
             cur_run = 0
-    return s
+        max_run = max(max_run, cur_run)
+    return max_run
 
 
 def main():
