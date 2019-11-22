5cfbb6ec4d0ddbf8605c9bad570f28fc4e471819
Peter Surkov <petyo333@gmail.com>
2019-11-22 19:37:42 +0300

    task01 - first part

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,8 +1,66 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import Tuple, List, TextIO
 import sys
 import re
 import argparse
+
+
+def find_in_str(pattern: str, s: str, matching_args: List[str]) -> bool:
+    if 'regex' in matching_args:
+        return bool(re.search(pattern, s))
+    else:
+        return pattern in s
+
+
+def get_matching_args(args: argparse.Namespace) -> List[str]:
+    matching_args = []
+    if args.regex:
+        matching_args.append('regex')
+    return matching_args
+
+
+def find_in_file(
+        file: TextIO,
+        pattern: str,
+        matching_args: List[str]
+        ) -> List[str]:
+
+    lines = map(lambda line: line.rstrip('\n'), file)
+    return [line for line in lines if find_in_str(pattern, line, matching_args)]
+
+
+def find_in_files_or_stdin(  # find in files or in stdin if files empty
+        files: List[str],
+        pattern: str,
+        matching_args: List[str]) -> List[Tuple[str, List[str]]]:
+
+    matches = []
+
+    if files:
+        for filename in files:
+            with open(filename, 'r') as file:
+                matches.append((filename, find_in_file(file, pattern, matching_args)))
+    else:
+        matches = [('stdin', find_in_file(sys.stdin, pattern, matching_args))]
+
+    return matches
+
+
+def to_count(matches: List[Tuple[str, List[str]]]) -> List[Tuple[str, int]]:
+    return [(filename, len(lines)) for filename, lines in matches]
+
+
+def print_matches(all_matches: List[Tuple[str, List[str]]], print_count: bool) -> None:
+    output_format = '{1}' if len(all_matches) == 1 else '{0}:{1}'
+
+    if print_count:
+        all_matches_cnt = to_count(all_matches)
+        for filename, cnt in all_matches_cnt:
+            print(output_format.format(filename, cnt))
+    else:
+        for filename, matches in all_matches:
+            for match in matches:
+                print(output_format.format(filename, match))
 
 
 def main(args_str: List[str]):
@@ -10,20 +68,11 @@ def main(args_str: List[str]):
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    matches = find_in_files_or_stdin(args.files, args.needle, get_matching_args(args))
+    print_matches(matches, args.count)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,6 +1,81 @@
 #!/usr/bin/env python3
 import io
+from argparse import Namespace
 import grep
+
+
+def test_unit_find_in_str():
+    assert grep.find_in_str('str', 'prefstr', [])
+    assert grep.find_in_str('str', 'strsuf', [])
+    assert grep.find_in_str('str', 'str', [])
+    assert not grep.find_in_str('str', 'std', [])
+    assert not grep.find_in_str('str', 'Str', [])
+    assert not grep.find_in_str('str', 'sTr', [])
+
+    assert grep.find_in_str('str', 'str', ['regex'])
+    assert grep.find_in_str('a?', 'a', ['regex'])
+    assert grep.find_in_str('ab.*ef', 'abcdef', ['regex'])
+    assert not grep.find_in_str('aba+', 'abcbaa', ['regex'])
+
+
+def test_unit_get_matching_args():
+    assert grep.get_matching_args(Namespace(regex=False, not_regex=True)) == []
+    assert grep.get_matching_args(Namespace(regex=True, not_regex=False)) == ['regex']
+
+
+def test_unit_find_in_file(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('sssstr\nstr\ntrs\nstrr')
+    (tmp_path / 'b.txt').write_text('sssstr\nstr\ntrs\nstrr\ntr\nstd')
+    monkeypatch.chdir(tmp_path)
+
+    with open('a.txt', 'r') as file:
+        assert grep.find_in_file(
+            file,
+            'str',
+            []) == ['sssstr', 'str', 'strr']
+
+    with open('b.txt', 'r') as file:
+        assert grep.find_in_file(
+            file,
+            's+tr?',
+            ['regex']) == ['sssstr', 'str', 'strr', 'std']
+
+
+def test_unit_find_in_files_or_stdin(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('sssstr\nstr\ntr')
+    (tmp_path / 'b.txt').write_text('st\nssssttttrrr\nsstrr')
+    monkeypatch.chdir(tmp_path)
+    assert grep.find_in_files_or_stdin(
+        ['a.txt', 'b.txt'],
+        'str',
+        []) == [('a.txt', ['sssstr', 'str']), ('b.txt', ['sstrr'])]
+
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'st\nssttr\nsstrr'))
+    assert grep.find_in_files_or_stdin(
+        None,
+        'str',
+        []) == [('stdin', ['sstrr'])]
+
+
+def test_unit_to_count():
+    assert grep.to_count([
+        ('a', ['1', '2', '3']),
+        ('b', []),
+        ('c', ['1'])]) == [('a', 3), ('b', 0), ('c', 1)]
+
+
+def test_unit_print_matches(capsys):
+    all_matches = [('a.txt', ['abc', 'def']), ('b.txt', [])]
+    grep.print_matches(all_matches, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:abc\na.txt:def\n'
+
+    grep.print_matches(all_matches, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:0\n'
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
@@ -40,13 +115,13 @@ def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
 
 
 def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
-    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
-    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    (tmp_path / 'a.txt').write_text('prefneedle\nneedlesuf\n')
+    (tmp_path / 'b.txt').write_text('theneedl\nprefneedlesuf')
     monkeypatch.chdir(tmp_path)
-    grep.main(['needle', 'b.txt', 'a.txt'])
+    grep.main(['needle', 'b.txt', 'a.txt', 'b.txt'])
     out, err = capsys.readouterr()
     assert err == ''
-    assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+    assert out == 'b.txt:prefneedlesuf\na.txt:prefneedle\na.txt:needlesuf\nb.txt:prefneedlesuf\n'
 
 
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
