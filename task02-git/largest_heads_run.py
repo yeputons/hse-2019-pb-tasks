ae273840b8f041deafccee3947ea5aae80813fee
Sasha Vorobeva <avorobdm@gmail.com>
2019-11-23 11:40:59 +0300

    Changes of the order of variables 'total' and 's'

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -2,13 +2,10 @@
 from typing import List
 import random
 
-ITERS = 1000
-FLIPS = 100
 
-
-def get_max_run(flips: List[str]) -> int:
-    max_run = 0
+def get_max_run(flips: List[int]) -> int:
     cur_run = 0
+    max_run = 0
     for flip in flips:
         if flip:
             cur_run += 1
@@ -18,14 +15,17 @@ def get_max_run(flips: List[str]) -> int:
     return max_run
 
 
+ITERS = 1000
+FLIPS = 100
+
+
 def main():
     random.seed(123456)
     s = 0
     total = 0
     for _ in range(ITERS):
+        s += get_max_run(random.choice([0, 1]) for _ in range(FLIPS))
         total += 1
-        flips = [random.choice([0, 1]) for _ in range(FLIPS)]
-        s += get_max_run(flips)
     print(s, total, s / total)
 
 
