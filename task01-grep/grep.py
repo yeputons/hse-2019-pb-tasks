8d90f360d64ee992733a93cca3055d4a1b204c23
Nikita Usoltsev <usoltsev-nv@yandex.ru>
2019-11-23 18:07:30 +0300

    done

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,65 @@
 #!/usr/bin/env python3
-from typing import List
-import sys
-import re
 import argparse
+import re
+import sys
+from typing import List
 
 
-def main(args_str: List[str]):
+def parse_args(args_str: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('string', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
     args = parser.parse_args(args_str)
+    return args
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+def print_file(print_filenames, flag_c, prefix, lines: List[str]) -> None:
+    if not print_filenames:
+        prefix = ''
+    if flag_c:
+        print(f'{prefix}{len(lines)}')
+    else:
+        for lin in lines:
+            print(f'{prefix}{lin}')
+
+
+def print_stdio(flag_c, lines: List[str]) -> None:
+    if flag_c:
+        print(len(lines))
+    else:
+        for lin in lines:
+            print(lin)
+
+
+def search_append(string, line, lines: List[str]) -> None:
+    line = line.rstrip('\n')
+    if re.search(string, line):
+        lines.append(line)
+
+
+def search_right_string(string: str, place_of_search, lines: List[str]) -> None:
+    for line in place_of_search:
+        search_append(string, line, lines)
+
+
+def main(args_str: List[str]) -> None:
+    args = parse_args(args_str)
+    string = args.string if args.regex else args.string
+    lines: List[str] = []
+
+    if args.files:
+        print_filenames: bool = len(args.files) > 1
+        for file in args.files:
+            with open(file, 'r') as in_file:
+                search_right_string(string, in_file, lines)
+                print_file(print_filenames, args.count, file + ':', lines)
+                lines = []
+
+    else:
+        search_right_string(string, sys.stdin, lines)
+        print_stdio(args.count, lines)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import io
+from typing import List
 import grep
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
-        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+        'pref needle?\nneedle? suf\npref needle? suf\nthank you OHneedHELP'))
     grep.main(['needle?'])
     out, err = capsys.readouterr()
     assert err == ''
@@ -57,3 +58,66 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_search_append():
+    lines: List[str] = ['victor', 'love and piece']
+    grep.search_append('win', 'wins', lines)
+    assert lines.pop() == 'wins'
+    grep.search_append('win', 'wns', lines)
+    assert lines.pop() == 'love and piece'
+    grep.search_append('v*y', 'victory', lines)
+    assert lines.pop() == 'victory'
+    grep.search_append('v*y', 'victoru', lines)
+    assert lines.pop() == 'victor'
+
+
+def test_print_file(capsys):  # DODODODOODODODODODODO
+    lines: List[str] = ['thank you', 'a lot', 'thanks']
+    file_name = 'hello, Olga:'
+
+    grep.print_file(1, 1, file_name, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'hello, Olga:3\n'
+
+    grep.print_file(1, 0, file_name, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'hello, Olga:thank you\nhello, Olga:a lot\nhello, Olga:thanks\n'
+
+    grep.print_file(0, 1, file_name, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+    grep.print_file(0, 0, file_name, lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'thank you\na lot\nthanks\n'
+
+
+def test_print_stdio(capsys):
+    lines: List[str] = ['dead', 'line', 'deadline']
+    grep.print_stdio(1, lines)
+    out, err = capsys.readouterr()
+    assert out == '3\n'
+    assert err == ''
+    grep.print_stdio(0, lines)
+    out, err = capsys.readouterr()
+    assert out == 'dead\nline\ndeadline\n'
+    assert err == ''
+
+
+def test_search_right_string():
+    lines = ['ahaha', 'win', 'victory', 'ha']
+    res = []
+    for line in lines:
+        grep.search_append('ha', line, res)
+    assert res == ['ahaha', 'ha']
+
+    lines = ['ahaha', 'win', 'victory', 'ha']
+    res = []
+    for line in lines:
+        grep.search_append('no', line, res)
+    assert res == []
