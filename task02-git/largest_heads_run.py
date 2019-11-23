fcbf0e436e31e2b0430b0508e5c79ca3823b6331
Jane <grindevald666@gmail.com>
2019-11-23 05:44:02 +0300

    Generate all flips in advance

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -13,8 +13,8 @@ def main():
         total += 1
         cur_run = 0
         max_run = 0
-        for _ in range(FLIPS):
-            flip = random.choice([0, 1])
+        flips = [random.choice([0, 1]) for _ in range(FLIPS)]
+        for flip in flips:
             if flip:
                 cur_run += 1
             else:
