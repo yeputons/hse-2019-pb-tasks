099e284dd33f33e0b38c3ac00feeb3e7b78ae19f
Алина Ан <an_alina@bk.ru>
2019-11-23 16:57:27 +0300

    made a new function

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 import random
+from typing import List
+
 
 ITERS = 1000
 FLIPS = 100
 
+def get_max_run(flips: List[int]) -> int:
+    cur_run = 0
+    max_run = 0
+    for flip in flips:
+        if flip:
+            cur_run += 1
+            if cur_run > max_run:
+                max_run += 1
+        else:
+            cur_run = 0
+    return max_run
 
 def main():
     random.seed(123456)
     s = 0
     total = 0
     for _ in range(ITERS):
-        flips = [random.choice([0, 1]) for _ in range(FLIPS)]
         total += 1
-        cur_run = 0
-        max_run = 0
-        for flip in flips:
-            if flip:
-                cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-            else:
-                cur_run = 0
-        s += max_run
+        s += get_max_run(random.choice([0, 1]) for _ in range(FLIPS))
     print(s, total, s / total)
 
 
