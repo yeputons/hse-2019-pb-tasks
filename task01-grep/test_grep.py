4e7fa6163f24e7a409ca52a6a1bdca2a297ec89e
vavilov mark <vavilovmark@gmail.com>
2019-11-23 15:42:23 +0300

    Style adjustments

--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -3,25 +3,29 @@ import io
 import grep
 
 def test_search():
-    assert grep.search('test?', 'tes', True) == True
-    assert grep.search('test?', 'tst', True) == False
-    assert grep.search('tes?', 'test', False) == False
-    assert grep.search('tes?', 'tes?to', False) == True
+    assert grep.search('test?', 'tes', True) is True
+    assert grep.search('test?', 'tst', True) is False
+    assert grep.search('tes?', 'test', False) is False
+    assert grep.search('tes?', 'tes?to', False) is True
 
-def helper(f, check, capsys):
+def helper(check, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == check
 
 def test_to_do(capsys):
-    helper(grep.to_do([], True, ''), '0\n', capsys)
-    helper(grep.to_do([1, 2, 3, 4, 5], True, ''), '5\n', capsys)
-        
+    grep.to_do([], True, '')
+    helper('0\n', capsys)
+    grep.to_do([1, 2, 3, 4, 5], True, '')
+    helper('5\n', capsys)
 
-def test_find_in_file(tmp_path, monkeypatch, capsys):
-    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+
+def test_find_in_file(tmp_path, capsys):
+    (tmp_path / 'a.txt').write_text(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf')
     with open(tmp_path / 'a.txt', 'r') as file:
-        helper(grep.find_in_file(file, 'needle?', True, False), 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n', capsys)
+        grep.find_in_file(file, 'needle?', True, False)
+        helper('pref needle?\nneedle? suf\nthe needl\npref needle? suf\n', capsys)
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
