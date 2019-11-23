a520b38cadcd97c668ffab3eba356a1ff4892863
LenSunko01 <sunko.elena2001@gmail.com>
2019-11-23 10:40:08 +0300

    First iteration

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python3
+import io
 from typing import List
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
-    parser = argparse.ArgumentParser()
+def print_in_format(file: io.StringIO, second: str, length: int):
+    if length > 1:
+        print(file.name, ':', second, sep='')
+    else:
+        print(second)
+
+
+def search_print_count(needle: str, files: List[io.StringIO], regex: bool, count: bool):
+    for file in files:
+        number = 0
+        for line in file.readlines():
+            line = line.strip('\n')
+            if (regex and re.search(needle, line)) or (not regex and needle in line):
+                if count:
+                    number += 1
+                else:
+                    print_in_format(file, line, len(files))
+        if count:
+            print_in_format(file, str(number), len(files))
+
+
+def parser_initialisation() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description='Print lines/count number of enters in text')
     parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
+    parser.add_argument('files', nargs='*', type=argparse.FileType('r'), default=[sys.stdin])
     parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    parser.add_argument('-c', dest='c', action='store_true')
+    return parser
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+def extract_args(args_str: List[str]) -> argparse.Namespace:
+    parser = parser_initialisation()
+    return parser.parse_args(args_str)
+
+
+def main(args_str: List[str]):
+    args = extract_args(args_str)
+    search_print_count(args.needle, args.files, args.regex, args.c)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -3,6 +3,87 @@ import io
 import grep
 
 
+# Проверка корректности работы функции print_in_format
+# в случае одного файла и стандартного потока ввода
+def test_unit_print_in_format_one_argument(monkeypatch, tmp_path, capsys):
+    (tmp_path / 'in.txt').write_text('This is a simple test')
+    monkeypatch.chdir(tmp_path)
+    file = open('in.txt', 'r')
+    grep.print_in_format(file, 'second argument', 1)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'second argument\n'
+    file.close()
+
+
+# Проверка корректности работы функции print_in_format в случае нескольких файлов
+def test_unit_print_in_format_more_than_one_argument(monkeypatch, tmp_path, capsys):
+    p = tmp_path / 'in.text'
+    p.write_text('This is a simple test')
+    monkeypatch.chdir(tmp_path)
+    file = open('in.text', 'r')
+    grep.print_in_format(file, 'second argument', 5)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'in.text:second argument\n'
+    file.close()
+
+
+# Проверка корректности работы функции search_print_count в случае только флага -c
+def test_unit_count_not_regex(capsys):
+    test_text = 'This is a simple test trompompop\n' \
+                'the main idea tram\n' \
+                'is to find out pom pam\n' \
+                'whether this function para\n' \
+                'is working properly pam pam\n'
+    grep.search_print_count('is', [io.StringIO(test_text)], False, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
+# Проверка корректности работы функции search_print_count в случае флагов -c -E
+def test_unit_count_regex(capsys):
+    test_text = 'This is a simple test trompompop\n' \
+                'the main idea tram\n' \
+                'is to find out pom pam\n' \
+                'whether this function para\n' \
+                'is working properly pam pam\n'
+    grep.search_print_count('pa?o?m', [io.StringIO(test_text)], True, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+# Проверка корректности работы функции search_print_count работы в случае отсутствия флагов
+def test_unit_print(monkeypatch, tmp_path, capsys):
+    file_path = tmp_path / 'in.txt'
+    file_path.write_text('No hay nadie como tu en todo el mundo\n'
+                         'no hay nadie como tu mi amor\n'
+                         'mejita yo te queiro\n')
+    monkeypatch.chdir(tmp_path)
+    file = open('in.txt', 'r')
+    grep.search_print_count('mejita', [file], False, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'mejita yo te queiro\n'
+    file.close()
+
+
+def test_unit_extract_args(monkeypatch, tmp_path):
+    (tmp_path / 'in.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'a.txt').write_text('This is a simple test')
+    monkeypatch.chdir(tmp_path)
+    result = grep.extract_args(['-c', '-E', 'needle?', 'in.txt', 'a.txt'])
+    expected_names = ['in.txt', 'a.txt']
+    assert result.c
+    assert result.regex
+    assert result.needle == 'needle?'
+    for i, name in enumerate(expected_names):
+        assert result.files[i].name == name
+
+
+# Проверка корректности работы в случае стандартного потока ввода и отсутсвия флагов
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
@@ -12,6 +93,7 @@ def test_integrate_stdin_grep(monkeypatch, capsys):
     assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
 
 
+# Проверка корректности работы в случае стандартного потока ввода и флага -E
 def test_integrate_stdin_regex_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
@@ -21,6 +103,7 @@ def test_integrate_stdin_regex_grep(monkeypatch, capsys):
     assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
 
 
+# Проверка корректности работы в случае стандартного потока ввода и флага -c
 def test_integrate_stdin_grep_count(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle\nneedle suf\nthe needl\npref needle suf'))
@@ -30,6 +113,7 @@ def test_integrate_stdin_grep_count(monkeypatch, capsys):
     assert out == '3\n'
 
 
+# Проверка корректности работы в случае одного файла и отсутсвия флагов
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
@@ -39,6 +123,7 @@ def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     assert out == 'pref needle suf\n'
 
 
+# Проверка корректности работы в случае нескольких файлов и отсутствия флагов
 def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
@@ -49,6 +134,7 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
 
 
+# Проверка корректности работы в случае нескольких файлов и флага -c
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
@@ -57,3 +143,35 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+# Проверка корректности работы в случае стандартного потока ввода и флага -c
+def test_integrate_stdin_grep_regex_count(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nnedle suf\nthe needl\npref ne*edl?e suf\npref neeeeeede'))
+    grep.main(['-c', 'ne*edl?e'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+# Проверка корректности работы в случае нескольких файлов и флага -E
+def test_integrate_files_regex_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'ne*d?le', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
+# Проверка корректности работы в случае нескольких файлов и флагов -c -E
+def test_integrate_files_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', '-c', 'n*edle?', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:2\na.txt:2\n'
