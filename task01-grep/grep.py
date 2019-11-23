c6288592641016a4b3bb406125f8c0fbea774b23
Tanya <yakushina.tanya@gmail.com>
2019-11-24 00:45:07 +0300

    update of grep.py and test_grep.py

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,30 +1,79 @@
 #!/usr/bin/env python3
 from typing import List
+from pathlib import Path
 import sys
 import re
 import argparse
 
 
+def print_for_lines(line: str,
+                    is_countable: bool, counter: int):
+    if is_countable:
+        print(counter)
+        return
+    print(line)
+
+
+def print_for_files(is_countable: bool, file_name: str,
+                    count: int, string: List[str], files_len: int):
+    if is_countable:
+        print(file_name + ':' + str(count))
+    elif not is_countable and not files_len == 1:
+        for needle in string:
+            print(file_name + ':' + needle)
+    else:
+        for needle in string:
+            print(needle)
+
+
+def for_files_with_file_names(files: List[str], pattern: str, is_countable: bool):
+    for file_name in files:
+        my_path = Path.cwd()
+        file = my_path.joinpath(file_name)
+        count = 0
+        if file.exists() and file.is_file():
+            with file.open() as in_file:
+                string = []
+                count = 0
+                for line in in_file:
+                    line = line.rstrip('\n')
+                    if re.search(pattern, line):
+                        count += 1
+                        string.append(line)
+            print_for_files(is_countable, file_name, count, string, len(files))
+        else:
+            print(f'Error: file {file_name} is not opened.')
+
+
+def for_stdin(pattern: str, is_countable: bool):
+    counter = 0
+    if is_countable:
+        for line in sys.stdin.readlines():
+            line = line.rstrip('\n')
+            if re.search(pattern, line):
+                counter += 1
+        print_for_lines('', is_countable, counter)
+    else:
+        for line in sys.stdin.readlines():
+            line = line.rstrip('\n')
+            if re.search(pattern, line):
+                print_for_lines(line, False, 0)
+
+
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-E', dest='is_regex', action='store_true')
+    parser.add_argument('-c', dest='is_countable', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    pattern = args.pattern if args.is_regex else re.escape(args.pattern)
+    if args.files:
+        for_files_with_file_names(args.files, pattern, args.is_countable)
+    else:
+        for_stdin(pattern, args.is_countable)
 
 
-if __name__ == '__main__':
+if __name__ == 'main':
     main(sys.argv[1:])
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,48 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_print_file_with_counter(capsys):
+    grep.print_for_files(True, 'a.txt', 3, [], 2)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:3\n'
+
+
+def test_print_file_without_counter(capsys):
+    grep.print_for_files(2, 'a.txt', 'abc', False, 0)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:abc\n'
+
+
+def test_print_line_without_counter(capsys):
+    grep.print_for_lines('abc', False, 0)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'abc\n'
+
+
+def test_print_result_line_with_counter(capsys):
+    grep.print_for_lines('abc', True, 1)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_for_files_with_file_names_error(capsys):
+    grep.for_files_with_file_names(['a.txt', 'b.txt'], 'pattern', True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'Error: file a.txt is not opened.\nError: file b.txt is not opened.\n'
+
+
+def test_for_files_with_file_names_with_counter(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.for_files_with_file_names(['a.txt', 'b.txt'], 'needle', True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:1\n'
