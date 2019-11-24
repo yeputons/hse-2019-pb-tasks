5fc9920ae61dbf0401bc22dab9646d7dd1c5608f
xbreathoflife <xbreathoflife@gmail.com>
2019-11-24 17:25:18 +0300

    Add flags -x, -i, -v to grep functional
    -x: Displays lines that match the specified pattern exactly
        with no additional characters
    -v: Displays all lines not matching the specified pattern
    -i: Ignores the case of letters when making comparisons

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -14,7 +14,12 @@ def parse_arguments(args_str: List[str]) -> argparse.Namespace:
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-c', dest='count', action='store_true')
+    parser.add_argument('-l', dest='files_with_matches', action='store_true')
+    parser.add_argument('-L', dest='files_without_match', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-i', dest='ignore_case', action='store_true')
+    parser.add_argument('-v', dest='invert_match', action='store_true')
+    parser.add_argument('-x', dest='full_match', action='store_true')
     args = parser.parse_args(args_str)
     return args
 
@@ -34,18 +39,35 @@ def print_answer(lines: List[str], number_of_files: int, file_name: str, count_m
             print(prefix, line, sep='')
 
 
-def is_regex(needle: str, line: str) -> bool:
+def is_regex(needle: str, line: str, ignore_case_mode: bool, full_match_mode: bool) -> bool:
     """ Returns true if regex is found false otherwise """
-    return re.search(needle, line)
+    if full_match_mode:
+        search_mode = re.fullmatch
+    else:
+        search_mode = re.search
+    if ignore_case_mode:
+        return search_mode(needle, line, re.IGNORECASE) is not None
+    else:
+        return search_mode(needle, line) is not None
 
 
-def is_substring(needle: str, line: str) -> bool:
+def is_substring(needle: str, line: str, ignore_case_mode: bool, full_match_mode: bool) -> bool:
     """ Returns true if substring is found false otherwise """
-    return needle in line
+    if ignore_case_mode:
+        new_needle = needle.lower()
+        new_line = line.lower()
+    else:
+        new_needle = needle
+        new_line = line
+    if full_match_mode:
+        return new_needle == new_line
+    else:
+        return new_needle in new_line
 
 
-def find_in_input(count_mode: bool, regex_mode: bool, needle: str,
-                  file: TextIO, file_name: str, number_of_input_data: int) -> None:
+def find_in_input(count_mode: bool, regex_mode: bool, ignore_case_mode: bool, invert_mode: bool,
+                  full_match_mode: bool, needle: str, file: TextIO,
+                  file_name: str, number_of_input_data: int) -> None:
     """ Prints lines with needles (substring or regex) or amount of them
         Works for -c and -E flags and input: stdin or file(s) """
     if regex_mode:
@@ -55,7 +77,7 @@ def find_in_input(count_mode: bool, regex_mode: bool, needle: str,
     answer_lines = []
     for line in file.readlines():
         line = line.rstrip('\n')
-        if search_mode(needle, line):
+        if search_mode(needle, line, ignore_case_mode, full_match_mode) != invert_mode:
             answer_lines.append(line)
     print_answer(answer_lines, number_of_input_data, file_name, count_mode)
 
@@ -66,8 +88,8 @@ def find_in_files(args: argparse.Namespace) -> None:
         many files: prints <file_name> before searching results """
     for current_file in args.files:
         with open(current_file, 'r') as in_file:
-            find_in_input(args.count, args.regex, args.needle,
-                          in_file, current_file, len(args.files))
+            find_in_input(args.count, args.regex, args.ignore_case, args.invert_match, args.full_match,
+                          args.needle, in_file, current_file, len(args.files))
 
 
 def main(args_str: List[str]):
@@ -77,7 +99,8 @@ def main(args_str: List[str]):
     if args.files:
         find_in_files(args)
     else:
-        find_in_input(args.count, args.regex, args.needle, sys.stdin, '', 0)
+        find_in_input(args.count, args.regex, args.ignore_case, args.invert_match, args.full_match,
+                      args.needle, sys.stdin, '', 0)
 
 
 if __name__ == '__main__':
