b89da224c86d1448cc203d4202d5a220b05bfa4d
xbreathoflife <xbreathoflife@gmail.com>
2019-11-24 14:42:33 +0300

    Finished first part of the task01-grep

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,83 @@
 #!/usr/bin/env python3
-from typing import List
+"""
+Grep(task01-grep)
+by Obriadina Aleksandra
+"""
+from typing import List, TextIO
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
+def parse_arguments(args_str: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
+    parser.add_argument('-c', dest='count', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
     args = parser.parse_args(args_str)
+    return args
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
+
+def print_answer(lines: List[str], number_of_files: int, file_name: str, count_mode: bool) -> None:
+    """ (1) if number of files is more than 1, prints <file_name>:<line>
+        (2) if -c flag, prints the number of needles in stdin or in files;
+        (3) if (1) and (2), prints <file_name>:<number of needles in <file_name>>"""
+    if number_of_files <= 1:
+        prefix = ''
+    else:
+        prefix = file_name + ':'
+    if count_mode:
+        print(prefix, len(lines), sep='')
+    else:
+        for line in lines:
+            print(prefix, line, sep='')
+
+
+def is_regex(needle: str, line: str) -> bool:
+    """ Returns true if regex is found false otherwise """
+    return re.search(needle, line)
+
+
+def is_substring(needle: str, line: str) -> bool:
+    """ Returns true if substring is found false otherwise """
+    return needle in line
+
+
+def find_in_input(count_mode: bool, regex_mode: bool, needle: str,
+                  file: TextIO, file_name: str, number_of_input_data: int) -> None:
+    """ Prints lines with needles (substring or regex) or amount of them
+        Works for -c and -E flags and input: stdin or file(s) """
+    if regex_mode:
+        search_mode = is_regex
+    else:
+        search_mode = is_substring
+    answer_lines = []
+    for line in file.readlines():
         line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+        if search_mode(needle, line):
+            answer_lines.append(line)
+    print_answer(answer_lines, number_of_input_data, file_name, count_mode)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+
+def find_in_files(args: argparse.Namespace) -> None:
+    """ For each file, function opens it and calls find_in_input function
+        one file: prints all lines with needles or amount of them (if -c)
+        many files: prints <file_name> before searching results """
+    for current_file in args.files:
+        with open(current_file, 'r') as in_file:
+            find_in_input(args.count, args.regex, args.needle,
+                          in_file, current_file, len(args.files))
+
+
+def main(args_str: List[str]):
+    """ Performs functionality of grep with flags -c and -E
+        Input data: stdin or file(s) """
+    args = parse_arguments(args_str)
+    if args.files:
+        find_in_files(args)
+    else:
+        find_in_input(args.count, args.regex, args.needle, sys.stdin, '', 0)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import io
+import sys
 import grep
+
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
@@ -30,6 +32,15 @@ def test_integrate_stdin_grep_count(monkeypatch, capsys):
     assert out == '3\n'
 
 
+def test_integrate_stdin_grep_regex_count(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'))
+    grep.main(['-c', '-E', 'needle?'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
@@ -49,6 +60,15 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
 
 
+def test_integrate_file_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
@@ -57,3 +77,92 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle?', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
+
+
+def test_integrate_files_grep_regex(tmp_path, monkeypatch, capsys): ####
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle? suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle?', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle?\na.txt:needle? suf\nb.txt:the needl\nb.txt:pref needle? suf\n'
+
+
+def test_integrate_file_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle?', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
+def test_integrate_files_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle?\nneedle? suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle?', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:2\n'
+
+
+def test_parse_arguments_files():
+    arg = grep.parse_arguments(['-c', 'needle', 'a.txt', 'b.txt'])
+    assert arg.count
+    assert not arg.regex
+    assert arg.needle == 'needle'
+    assert arg.files == ['a.txt', 'b.txt']
+
+
+def test_parse_arguments_stdin():
+    arg = grep.parse_arguments(['-c', '-E', 'needle'])
+    assert arg.count
+    assert arg.regex
+    assert arg.needle == 'needle'
+    assert arg.files == []
+
+
+def test_print_answer(capsys):
+    grep.print_answer(['needle', 'no needle', 'no need'], 2, 'a.txt', False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:needle\na.txt:no needle\na.txt:no need\n'
+
+
+def test_is_substring():
+    assert grep.is_substring('needle', 'no needle is here')
+
+
+def test_is_regex():
+    assert grep.is_regex('h*i?', 'ahhhe')
+
+
+def test_find_in_input(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    grep.find_in_input(False, False, 'needle', sys.stdin, '', 0)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
+
+
+def test_find_in_files(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\nthe needl\n')
+    (tmp_path / 'b.txt').write_text('pref needle suf\ncat\n')
+    monkeypatch.chdir(tmp_path)
+    arg = grep.parse_arguments(['-c', 'needle', 'a.txt', 'b.txt'])
+    grep.find_in_files(arg)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:1\n'
