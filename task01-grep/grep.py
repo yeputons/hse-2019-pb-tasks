880f8ed69fc0d40894eca56551a0bf4033405800
Filippov Denis <fild10@yandex.ru>
2019-11-25 09:28:59 +0300

    Add my task-01-grep solution

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -5,25 +5,67 @@ import re
 import argparse
 
 
-def main(args_str: List[str]):
+def parse_args(args_str: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    return args
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+
+def find_pattern_in_line(line: str, pattern: str, is_pattern_regex: bool) -> bool:
+    return is_pattern_regex and bool(re.search(pattern, line)) or \
+                not is_pattern_regex and pattern in line
+
+
+def filter_matched_lines(lines: List[str], pattern: str, is_pattern_regex: bool) -> List[str]:
+    return [line for line in lines
+            if find_pattern_in_line(line, pattern, is_pattern_regex)]
+
+
+def count_lines_with_pattern(lines: List[str], pattern: str, is_pattern_regex: bool) -> int:
+    return len(filter_matched_lines(lines, pattern, is_pattern_regex))
+
+
+def find_lines_from_grep(read_lines: List[str], pattern: str,
+                         is_pattern_regex=False, is_only_count=False) -> List:
+    if is_only_count:
+        cnt = count_lines_with_pattern(read_lines, pattern, is_pattern_regex)
+        return [cnt] if cnt else []
+    return filter_matched_lines(read_lines, pattern, is_pattern_regex)
+
+
+def find_lines_from_grep_files(files: str, pattern: str, is_pattern_regex=False,
+                               is_only_count=False) -> List[str]:
+    all_lines: List[str] = []
+    for file_name in files:
+        with open(file_name, 'r') as in_file:
+            lines = find_lines_from_grep([line.rstrip('\n') for line in in_file.readlines()],
+                                         pattern, is_pattern_regex, is_only_count)
+            all_lines += ['{}{}'.format((file_name + ':') * (len(files) > 1), line)
+                          for line in lines]
+    return all_lines
+
+
+def find_lines_from_grep_stdin(pattern: str, is_pattern_regex=False,
+                               is_only_count=False) -> List[str]:
+    return find_lines_from_grep([line.rstrip('\n') for line in sys.stdin.readlines()],
+                                pattern, is_pattern_regex, is_only_count)
+
+
+def main(args_str: List[str]):
+    args = parse_args(args_str)
+
+    if args.files:
+        lines = find_lines_from_grep_files(args.files, args.pattern, args.regex, args.count)
+    else:
+        lines = find_lines_from_grep_stdin(args.pattern, args.regex, args.count)
+
+    for line in lines:
+        print(line)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import io
 import grep
 
@@ -57,3 +56,57 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_unit_parse_args_files_count_regex():
+    parsed_args = grep.parse_args(['-c', '-E', 'date', 'text.txt', 'hw.tex'])
+    assert parsed_args == grep.argparse.Namespace(count=True, files=['text.txt', 'hw.tex'],
+                                                  pattern='date', regex=True)
+
+
+def test_unit_parse_args_files_not_count_not_regex():
+    parsed_args = grep.parse_args(['date', 'text.txt', 'hw.tex'])
+    assert parsed_args == grep.argparse.Namespace(count=False, files=['text.txt', 'hw.tex'],
+                                                  pattern='date', regex=False)
+
+
+def test_unit_parse_args_no_files():
+    parsed_args = grep.parse_args(['-E', 'date'])
+    assert parsed_args == grep.argparse.Namespace(count=False, files=[],
+                                                  pattern='date', regex=True)
+
+
+def test_unit_filter_matched_lines_regex():
+    lines = grep.filter_matched_lines(['b.cpp', 'a.c', 'mmmc'], '[a-z]+\\.cp*', True)
+    assert lines == ['b.cpp', 'a.c']
+
+
+def test_unit_filter_matched_lines_not_regex():
+    lines = grep.filter_matched_lines(['hello world', 'CGSG_FOREVER_30', 'DF5'], 'F', False)
+    assert lines == ['CGSG_FOREVER_30', 'DF5']
+
+
+def test_unit_count_lines_with_pattern_regex():
+    cnt = grep.count_lines_with_pattern(['b.cpp', 'a.c', 'mmmc'], '[a-z]+\\.cp*', True)
+    assert cnt == 2
+
+
+def test_unit_count_lines_with_pattern_not_regex():
+    cnt = grep.count_lines_with_pattern(['hello world', 'CGSG_FOREVER_30', 'DF5'], 'F', False)
+    assert cnt == 2
+
+
+def test_unit_find_lines_from_grep_not_regex_not_count():
+    parsed_args = grep.parse_args(['needle?'])
+    lines = grep.find_lines_from_grep(['pref needle?', 'needle? suf', 'the needl',
+                                       'pref needle? suf'], parsed_args.pattern,
+                                      parsed_args.regex, parsed_args.count)
+    assert lines == ['pref needle?', 'needle? suf', 'pref needle? suf']
+
+
+def test_unit_find_lines_from_grep_regex_count():
+    parsed_args = grep.parse_args(['-c', 'needle?', '-E'])
+    result = grep.find_lines_from_grep(['pref needle?', 'needle? suf',
+                                        'the needl', 'pref needle? suf'], parsed_args.pattern,
+                                       parsed_args.regex, parsed_args.count)
+    assert result == [4]
