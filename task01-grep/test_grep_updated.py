b1a7cf4aad7c582214a61c8478ccd561fc41ca1a
KirillBrilliantov <ki.br178@gmail.com>
2019-11-25 03:13:33 +0300

    More tests

--- task01-grep/test_grep_updated.py
+++ task01-grep/test_grep_updated.py
@@ -88,4 +88,40 @@ def test_integrate_inverted_only_files(tmp_path, capsys, monkeypatch):
     grep.main(['-lvE', 'oq?s', 'a.txt', 'b.txt'])
     out, err = capsys.readouterr()
     assert err == ''
+    assert out == 'a.txt\nb.txt\n'
+
+
+def test_integrate_all_keys_print_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
     assert out == 'b.txt\n'
+
+
+def test_integrate_all_keys_print_not_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text(
+        'hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(
+        ['-Livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\n'
+
+
+def test_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text(
+        'fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text(
+        'hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(
+        tmp_path)
+    grep.main(
+        ['-civx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:3\na.txt:0\n'
