b9e2818113e719040d3feaa8297948e53776de27
Роман Венедиктов <e2e4b6b7@gmail.com>
2019-11-26 10:22:46 +0300

    Commit first task solution

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,74 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, TextIO, Callable
 import sys
 import re
 import argparse
 
 
+def read_input(file_names: List[str]) -> List[List[str]]:
+    if not file_names:
+        return [split_into_lines(sys.stdin)]
+    else:
+        return [split_into_lines_file(file_name)
+                for file_name in file_names]
+
+
+def split_into_lines_file(file_name: str) -> List[str]:
+    with open(file_name, 'r') as in_file:
+        return split_into_lines(in_file)
+
+
+def split_into_lines(input_stream: TextIO) -> List[str]:
+    return [line.rstrip('\n') for line in input_stream.readlines()]
+
+
+def detect_requested_lines(pattern: str, inp_lines: List[List[str]], need_regex: bool) -> List[List[str]]:
+    def detection_func(s: str) -> bool:
+        if need_regex:
+            return bool(re.search(pattern, s))
+        else:
+            return pattern in s
+
+    return [detect_lines_by_function(detection_func, lines_in_one) for lines_in_one in inp_lines]
+
+
+def detect_lines_by_function(detection_func: Callable[[str], bool], lines: List[str]) -> List[str]:
+    out = []
+    for line in lines:
+        if detection_func(line):
+            out.append(line)
+    return out
+
+
+def prepare_output(detected_lines: List[List[str]], need_count: bool) -> List[List[str]]:
+    if need_count:
+        return [[str(len(detected_lines_in_one))]
+                for detected_lines_in_one in detected_lines]
+    else:
+        return detected_lines
+
+
+def output(file_names: List[str], out_lines: List[List[str]]):
+    if len(out_lines) == 1:
+        for out_line in out_lines[0]:
+            print(out_line)
+    else:
+        for out_lines_file in zip(out_lines, file_names):
+            for detected_line in out_lines_file[0]:
+                print(f'{out_lines_file[1]}:{detected_line}')
+
+
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
     args = parser.parse_args(args_str)
-
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    inp_lines = read_input(args.files)
+    detected_lines = detect_requested_lines(args.pattern, inp_lines, args.regex)
+    out_lines = prepare_output(detected_lines, args.count)
+    output(args.files, out_lines)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import io
+import sys
 import grep
 
 
@@ -57,3 +58,100 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_grep_count_multifile(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    (tmp_path / 'c.txt').write_text('need something\nneedl one more\nneedler is here')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'b.txt', 'a.txt', 'b.txt', 'c.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\nb.txt:1\nc.txt:1\n'
+
+
+def test_unit_read_input_files(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('Is it real?\nNot_:)\n')
+    (tmp_path / 'b.txt').write_text('')
+    (tmp_path / 'c.txt').write_text('\n')
+    monkeypatch.chdir(tmp_path)
+    out = grep.read_input(['a.txt', 'b.txt', 'c.txt'])
+    assert out == [['Is it real?', 'Not_:)'], [], ['']]
+
+
+def test_unit_read_input_stdin(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\nneedle? suf\nit is not needle'))
+    out = grep.read_input([])
+    assert out == [['pref needle?', 'needle? suf', 'the needl', 'needle? suf', 'it is not needle']]
+
+
+def test_unit_detect_lines_substring():
+    input_lines = [['cry', "don't cry"], ['crack', 'hehe'], ['', 'rc']]
+    out = grep.detect_requested_lines('cr', input_lines, False)
+    assert out == [['cry', "don't cry"], ['crack'], []]
+
+
+def test_unit_detect_lines_substring_strange():
+    input_lines = [['cry', "don't cry"], ['crack', 'hehe'], ['', 'rc']]
+    out = grep.detect_requested_lines('', input_lines, False)
+    assert out == input_lines
+
+
+def test_unit_detect_lines_regex():
+    input_lines = [['cry', "don't cry"], ['crack', 'hehe'], ['', 'rc'], ['not here']]
+    out = grep.detect_requested_lines('cr?', input_lines, True)
+    assert out == [['cry', "don't cry"], ['crack'], ['rc'], []]
+
+
+def test_unit_detect_lines_regex_strange():
+    input_lines = [['cry', "don't cry"], ['crack', 'hehe'], ['', 'rc'], ['not here']]
+    out = grep.detect_requested_lines('', input_lines, True)
+    assert out == input_lines
+
+
+def test_unit_prepare_output_lines():
+    input_lines = [['for what?', 'hz', ''], ['', 'right'], ['']]
+    out = grep.prepare_output(input_lines, False)
+    assert out == input_lines
+
+
+def test_unit_prepare_output_lines_count():
+    input_lines = [['for what?', 'hz', ''], ['', 'right'], ['']]
+    out = grep.prepare_output(input_lines, True)
+    assert out == [['3'], ['2'], ['1']]
+
+
+def test_unit_split_into_lines_files(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('some strange line\none more\n ')
+    monkeypatch.chdir(tmp_path)
+    out = grep.split_into_lines_file('a.txt')
+    assert out == ['some strange line', 'one more', ' ']
+
+
+def test_unit_split_into_lines(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref need?\nneedle? suf\nthe needl\npref suf\nit is not needle\n '))
+    out = grep.split_into_lines(sys.stdin)
+    assert out == ['pref need?', 'needle? suf', 'the needl', 'pref suf', 'it is not needle', ' ']
+
+
+def test_unit_detect_lines_by_function_strange():
+    input_lines = ['one', 'two', 'three', '   ', ' ']
+    out = grep.detect_lines_by_function(lambda s: len(s) == 3, input_lines)
+    assert out == ['one', 'two', '   ']
+
+
+def test_unit_detect_lines_by_function():
+    input_lines = ['one', 'two', 'three', '   ', ' ', 'totoro']
+    out = grep.detect_lines_by_function(lambda s: 't' in s, input_lines)
+    assert out == ['two', 'three', 'totoro']
+
+
+def test_unit_output(capsys):
+    input_lines = [['oh', 'my', 'god', '', ' ', 'end']]
+    grep.output([], input_lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'oh\nmy\ngod\n\n \nend\n'
