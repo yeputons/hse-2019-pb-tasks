7a9d6cc5e32cf3a579a096a774f195e2e48738fd
Роман Венедиктов <e2e4b6b7@gmail.com>
2019-11-26 21:35:16 +0300

    Add new flags and test for them

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -22,12 +22,29 @@ def split_into_lines(input_stream: TextIO) -> List[str]:
     return [line.rstrip('\n') for line in input_stream.readlines()]
 
 
-def detect_requested_lines(pattern: str, inp_lines: List[List[str]], need_regex: bool) -> List[List[str]]:
+def detect_requested_lines(pattern: str, inp_lines: List[List[str]], need_regex: bool = False,
+                           ignore_register: bool = False, invert_detection: bool = False,
+                           need_full_match: bool = False) -> List[List[str]]:
     def detection_func(s: str) -> bool:
         if need_regex:
-            return bool(re.search(pattern, s))
+            if ignore_register:
+                regex_pattern = re.compile(pattern, re.IGNORECASE)
+            else:
+                regex_pattern = re.compile(pattern, re.IGNORECASE)
+            if need_full_match:
+                regex_detect_function = re.fullmatch
+            else:
+                regex_detect_function = re.search
+            return bool(regex_detect_function(regex_pattern, s)) ^ invert_detection
         else:
-            return pattern in s
+            str_pattern = pattern
+            if ignore_register:
+                str_pattern = str_pattern.lower()
+                s = s.lower()
+            if need_full_match:
+                return (str_pattern == s) ^ invert_detection
+            else:
+                return (str_pattern in s) ^ invert_detection
 
     return [detect_lines_by_function(detection_func, lines_in_one) for lines_in_one in inp_lines]
 
@@ -40,7 +57,7 @@ def detect_lines_by_function(detection_func: Callable[[str], bool], lines: List[
     return out
 
 
-def prepare_output(detected_lines: List[List[str]], need_count: bool) -> List[List[str]]:
+def prepare_output(detected_lines: List[List[str]], need_count: bool = False) -> List[List[str]]:
     if need_count:
         return [[str(len(detected_lines_in_one))]
                 for detected_lines_in_one in detected_lines]
@@ -48,8 +65,14 @@ def prepare_output(detected_lines: List[List[str]], need_count: bool) -> List[Li
         return detected_lines
 
 
-def output(file_names: List[str], out_lines: List[List[str]]):
-    if len(out_lines) == 1:
+def output(file_names: List[str], out_lines: List[List[str]],
+           need_detected_file_names: bool = False,
+           need_undetected_file_names: bool = False):
+    if need_detected_file_names or need_undetected_file_names:
+        for out_lines_file in zip(out_lines, file_names):
+            if bool(out_lines_file[0]) ^ need_undetected_file_names:
+                print(out_lines_file[1])
+    elif len(out_lines) == 1:
         for out_line in out_lines[0]:
             print(out_line)
     else:
@@ -62,13 +85,20 @@ def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
     parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
+    output_method = parser.add_mutually_exclusive_group(required=False)
+    output_method.add_argument('-c', dest='count', action='store_true')
+    output_method.add_argument('-l', dest='detected_file_names', action='store_true')
+    output_method.add_argument('-L', dest='undetected_file_names', action='store_true')
+    parser.add_argument('-i', dest='ignore_register', action='store_true')
+    parser.add_argument('-v', dest='invert', action='store_true')
+    parser.add_argument('-x', dest='full_match', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
-    parser.add_argument('-c', dest='count', action='store_true')
     args = parser.parse_args(args_str)
     inp_lines = read_input(args.files)
-    detected_lines = detect_requested_lines(args.pattern, inp_lines, args.regex)
+    detected_lines = detect_requested_lines(args.pattern, inp_lines, args.regex,
+                                            args.ignore_register, args.invert, args.full_match)
     out_lines = prepare_output(detected_lines, args.count)
-    output(args.files, out_lines)
+    output(args.files, out_lines, args.detected_file_names, args.undetected_file_names)
 
 
 if __name__ == '__main__':
---
+++ task01-grep/test_grep_updated.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+import grep
+
+
+def test_integrate_all_keys_print_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt\n'
+
+
+def test_integrate_all_keys_print_not_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-Livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\n'
+
+
+def test_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-civx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:3\na.txt:0\n'
+
+
+def test_integrate_all_keys_print_not_files_grep_empty(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    (tmp_path / 'c.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-Livx', '-E', 'fo?o', 'b.txt', 'a.txt', 'c.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\nc.txt\n'
+
+
+def test_integrate_all_keys_print_files_grep_empty(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    (tmp_path / 'c.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-livx', '-E', 'fo?o', 'b.txt', 'a.txt', 'c.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt\n'
+
+
+def test_integrate_all_keys_print_files_grep_empty_2(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    (tmp_path / 'c.txt').write_text('\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-livx', '-E', 'fo?o', 'b.txt', 'a.txt', 'c.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt\nc.txt\n'
+
+
+def test_integrate_invert_ignore_register_print_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    (tmp_path / 'c.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-lix', '-E', 'fo?o', 'b.txt', 'a.txt', 'c.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\n'
+
+
+def test_integrate_substr_full_match_print_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFreoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz2\nfooo2\n')
+    (tmp_path / 'c.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-lix', 'fo', 'b.txt', 'a.txt', 'c.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\n'
+
+
+def test_unit_output_detected_files_names(capsys):
+    input_files = ['a.txt', 'b.txt', 'c.txt']
+    input_lines = [['oh', 'my', 'god', '', ' ', 'end'], [], ['']]
+    grep.output(input_files, input_lines, need_detected_file_names=True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\nc.txt\n'
+
+
+def test_unit_output_undetected_files_names(capsys):
+    input_files = ['a.txt', 'b.txt', 'c.txt', 'd']
+    input_lines = [['oh', 'my', 'god', '', ' ', 'end'], [], [''], []]
+    grep.output(input_files, input_lines, need_undetected_file_names=True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt\nd\n'
+
+
+def test_unit_detect_lines_substring():
+    input_lines = [['Cry', "don't cry"], ['cRack', 'hehe'], ['car', 'rC']]
+    out = grep.detect_requested_lines('cR', input_lines, ignore_register=True)
+    assert out == [['Cry', "don't cry"], ['cRack'], []]
+
+
+def test_unit_detect_lines_substring_invert():
+    input_lines = [['Cry', "don't cry"], ['cRack', 'hehe'], ['car', 'rC']]
+    out = grep.detect_requested_lines('cR', input_lines, ignore_register=True,
+                                      invert_detection=True)
+    assert out == [[], ['hehe'], ['car', 'rC']]
+
+
+def test_unit_detect_lines_substring_full_match():
+    input_lines = [['Cry', "don't cry"], ['cRack', 'hehe'],
+                   ['car', 'rC'], ['cR']]
+    out = grep.detect_requested_lines('cR', input_lines, ignore_register=True,
+                                      invert_detection=True, need_full_match=True)
+    assert out == [['Cry', "don't cry"], ['cRack', 'hehe'], ['car', 'rC'], []]
+
+
+def test_unit_detect_lines_regex():
+    input_lines = [['Cry', "don't cry"], ['cRack', 'hehe'], ['car', 'rC'], [], ['rrc']]
+    out = grep.detect_requested_lines('c', input_lines, need_regex=True, ignore_register=True)
+    assert out == [['Cry', "don't cry"], ['cRack'], ['car', 'rC'], [], ['rrc']]
+
+
+def test_unit_detect_lines_regex_invert():
+    input_lines = [['Cry', "don't cry"], ['cRack', 'hehe'], ['car', 'rC']]
+    out = grep.detect_requested_lines('c', input_lines, need_regex=True,
+                                      ignore_register=True, invert_detection=True)
+    assert out == [[], ['hehe'], []]
+
+
+def test_unit_detect_lines_regex_full_match():
+    input_lines = [['Cry', "don't cry"], ['cRack', 'hehe'], ['car', 'rC'], [], ['rrc']]
+    out = grep.detect_requested_lines('c.*', input_lines, need_regex=True,
+                                      ignore_register=True, need_full_match=True)
+    assert out == [['Cry'], ['cRack'], ['car'], [], []]
