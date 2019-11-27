6192d171dfee2a291caaab748daa51f33c5a7cb8
NikitaAbramov <nik_abramov22@mail.ru>
2019-11-28 02:27:07 +0300

    Add reTest and substTest

--- task01-grep/test_grep_updated.py
+++ task01-grep/test_grep_updated.py
@@ -30,3 +30,28 @@ def test_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:3\na.txt:0\n'
+
+
+def test_re_search_modifier():
+    line = 'ahhhe'
+    needle = 'h*i?'
+    assert grep.re_search_modifier(line, needle, False, False, False)
+    assert not grep.re_search_modifier(line, needle, True, False, False)
+    assert grep.re_search_modifier(line, needle, True, False, True)
+    line = 'hey HI world'
+    needle = 'hi'
+    assert grep.re_search_modifier(line, needle, False, True, False)
+    line = 'HeI'
+    assert not grep.re_search_modifier(line, needle, False, True, False)
+
+
+def test_substring_search_modifier():
+    line = 'ahhhe'
+    needle = 'hH?'
+    assert not grep.substring_search_modifier(line, needle, False, True, False)
+    assert grep.substring_search_modifier(line, needle, False, True, True)
+    needle = 'ahhhe'
+    assert grep.substring_search_modifier(line, needle, True, False, False)
+    line = 'hey HI world'
+    needle = 'hi'
+    assert not grep.substring_search_modifier(line, needle, False, True, True)
