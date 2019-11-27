ad22f8ca7edde57e873cbe960df1d446a3c4ffba
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-11-27 18:41:19 +0300

    The first iteration

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,70 @@
 #!/usr/bin/env python3
 from typing import List
+from typing import Tuple
 import sys
 import re
 import argparse
 
 
+def find(line: str, needle: str, regex: bool) -> bool:
+    result = re.search(needle, line) if regex else needle in line
+    return True if result else False
+
+
+def get_lines_with_needle(lines: List[str], needle: str, regex: bool) -> List[str]:
+    lines_with_needle = []
+    for line in lines:
+        if find(line, needle, regex):
+            lines_with_needle.append(line)
+    return lines_with_needle
+
+
+def print_lines(files_and_lines: List[Tuple[str, List[str]]], format_str: str, count: bool):
+    for file, lines in files_and_lines:
+        if count:
+            print(format_str.format(file, len(lines)))
+        else:
+            for line in lines:
+                print(format_str.format(file, line))
+
+
+def read_from_stdin() -> List[str]:
+    lines = []
+    for line in sys.stdin.readlines():
+        lines.append(line.rstrip('\n'))
+    return lines
+
+
+def read_from_file(file_name: str) -> List[str]:
+    lines = []
+    with open(file_name, 'r') as in_file:
+        for line in in_file.readlines():
+            lines.append(line.rstrip('\n'))
+    return lines
+
+
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', dest='count', action='store_true')
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    format_str = '{}:{}' if len(args.files) > 1 else '{}{}'
+    files_and_lines_with_needle = []
+    if args.files:
+        for file_name in args.files:
+            lines = read_from_file(file_name)
+            lines_with_needle = get_lines_with_needle(lines, args.needle, args.regex)
+            files_and_lines_with_needle.append(
+                (file_name if len(args.files) > 1 else '', lines_with_needle))
+    else:
+        lines = read_from_stdin()
+        lines_with_needle = get_lines_with_needle(lines, args.needle, args.regex)
+        files_and_lines_with_needle.append(('', lines_with_needle))
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    print_lines(files_and_lines_with_needle, format_str, args.count)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,107 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_unit_get_lines_with_needle(capsys):
+    lines = ['ball', 'all', 'tall', 'pal lamp', 'hello']
+    result = grep.get_lines_with_needle(lines, 'all', False)
+    correct = ['ball', 'all', 'tall']
+    out, err = capsys.readouterr()
+    assert correct == result
+    assert out == ''
+    assert err == ''
+
+
+def test_unit_get_lines_with_needle_with_regex(capsys):
+    lines = ['ball', 'all', 'tall', 'pal lamp', 'hello']
+    result = grep.get_lines_with_needle(lines, 'a?ll', True)
+    correct = ['ball', 'all', 'tall', 'hello']
+    out, err = capsys.readouterr()
+    assert correct == result
+    assert out == ''
+    assert err == ''
+
+
+def test_unit_print_needle_with_name(capsys):
+    files_and_lines = [('name1', ['ball', 'all', 'tall']), ('name2', ['pal lamp', 'hello'])]
+    grep.print_lines(files_and_lines, '{}:{}', False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'name1:ball\nname1:all\nname1:tall\nname2:pal lamp\nname2:hello\n'
+
+
+def test_unit_print_needle_without_name(capsys):
+    files_and_lines = [('', ['ball', 'all', 'tall', 'pal lamp', 'hello'])]
+    grep.print_lines(files_and_lines, '{}{}', False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'ball\nall\ntall\npal lamp\nhello\n'
+
+
+def test_unit_print_lines_with_count_and_name(capsys):
+    files_and_lines = [('name1', ['ball', 'all']), ('name2', ['tall', 'pal lamp', 'hello'])]
+    grep.print_lines(files_and_lines, '{}:{}', True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'name1:2\nname2:3\n'
+
+
+def test_unit_find_false(capsys):
+    line = 'hi hello good morning'
+    needle = 'ell'
+    result = grep.find(line, needle, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+    assert result
+
+
+def test_unit_not_find_false(capsys):
+    line = 'hi hello good morning'
+    needle = 'god'
+    result = grep.find(line, needle, False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+    assert not result
+
+
+def test_unit_find_true(capsys):
+    line = 'hi hello good morning gd'
+    needle = 'goo?d'
+    result = grep.find(line, needle, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+    assert result
+
+
+def test_unit_not_find_true(capsys):
+    line = 'hi hello good morning'
+    needle = 'go?ne'
+    result = grep.find(line, needle, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+    assert not result
+
+
+def test_read_from_stdin(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    result = grep.read_from_stdin()
+    assert result == ['pref needle?', 'needle? suf', 'the needl', 'pref needle? suf']
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+
+
+def test_read_from_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    result = grep.read_from_file('a.txt')
+    assert result == ['the needl', 'pref needle suf']
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
