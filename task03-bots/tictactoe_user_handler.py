e5b48ed26fb0a09f3873a3590845bc9a4ab1124c
kuzyaross <nkozubaeff@gmail.com>
2019-11-27 21:39:31 +0300

    Beta testing

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -9,13 +9,57 @@ class TicTacToeUserHandler(UserHandler):
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        raise NotImplementedError
+        if message == 'start':
+            self.start_game()
+        else:
+            if not self.game:
+                self.send_message('Game is not started')
+                return
+
+            player, row, col = message.split()
+            try:
+                row = int(row)
+                col = int(col)
+            except ValueError:
+                self.send_message('Please send number of row and col. Ex: X 0 2')
+                return
+
+            if player not in ('X', 'O'):
+                self.send_message('Player should be only X or O')
+                return
+
+            pl = Player.X if player == 'X' else Player.O
+            try:
+                if self.game.can_make_turn(pl, row=row, col=col):
+                    self.make_turn(pl, row=row, col=col)
+                else:
+                    print(f'Cannot make this turn {row}:{col} by {pl}')
+                    # debug  - change to Invalid turn message
+            except AssertionError:
+                self.send_message('Invalid values of col or row')
+
 
     def start_game(self) -> None:
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
+        self.send_message("Game is started!")
+        #debug delete print later
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        raise NotImplementedError
+        self.game.make_turn(player, row=row, col=col)
+        self.send_field()
+        if self.game.is_finished():
+            self.send_message(f'{player} wins')
+            #change to smth better
 
     def send_field(self) -> None:
-        raise NotImplementedError
+        for row in range(3):
+            for col in range(3):
+                if self.game.field[row][col]:
+                    print(self.game.field[row][col], end=' ')
+                else:
+                    print('.', end=' ')
+            print()
+
+
+
