ec8ed22c39da1b20f45b8b04ece5047de238a54d
Илья Смирнов <smirok123@mail.ru>
2019-11-27 22:47:41 +0300

    commit task01 solution

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,94 @@
 #!/usr/bin/env python3
+""" analog GREP to work with some flags """
 from typing import List
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
+def init_arguments(args_str: List[str]) -> argparse.Namespace:
+    """initializaton of input arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
+    parser.add_argument('-i', dest='ignore', action='store_true')
+    parser.add_argument('-v', dest='invert', action='store_true')
+    parser.add_argument('-x', dest='match', action='store_true')
+    parser.add_argument('-l', dest='file_with_str', action='store_true')
+    parser.add_argument('-L', dest='file_without_str', action='store_true')
+
     args = parser.parse_args(args_str)
+    return args
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+def translate_str_to_re(pattern: str, is_regular: bool) -> str:
+    """translate str to regular expression and return it"""
+    return re.escape(pattern) if not is_regular else pattern
+
+
+def filter_list(pattern: str, list_: List[str]) -> List[str]:
+    """find pattern in words of list and return list comprehension of them"""
+    return [word for word in list_ if re.search(pattern, word)]
+
+
+def strip(lines: List[str]) -> List[str]:
+    """delete '\n' in list and return it"""
+    return [line.rstrip('\n') for line in lines]
+
+
+def process_flag_c(result: List[str]) -> List[str]:
+    """process flag '-c' and return changed list"""
+    return [str(len(result))]
+
+
+def work_with_files(files: List[str], pattern: str, flags: dict) -> None:
+    """branch to work with files"""
+    for file in files:
+        with open(file, 'r') as input_file:
+            word_list = input_file.readlines()
+            result = full_processing_result(word_list, pattern, flags)
+            print_grep_results(result, file + ':' if len(files) > 1 else '')
+
+
+def work_with_stdin(pattern: str, flags: dict) -> None:
+    """branch to work with stdin"""
+    word_list = sys.stdin.readlines()
+    result = full_processing_result(word_list, pattern, flags)
+    # это не дублирование,а вызов основной функции
+    print_grep_results(result, '')
+    # так проще для 2 итерации
+
+
+def full_processing_result(word_list: List[str], pattern: str, flags: dict) -> List[str]:
+    """full processing one file or stdin with returned result"""
+    result = strip(word_list)
+    result = filter_list(pattern, result)
+    if flags['c']:
+        result = process_flag_c(result)
+    return result
+
+
+def print_grep_results(result: List[str], output_format: str) -> None:
+    """print results in necessary format"""
+    for word in result:
+        print(f'{output_format}{word}')
+
+
+def main(args_str: List[str]):
+    """main function"""
+    args = init_arguments(args_str)
+
+    pattern = translate_str_to_re(args.pattern, args.regex)
+
+    flags = {'c': args.count, 'i': args.ignore, 'v': args.invert,
+             'x': args.match, 'l': args.file_with_str, 'L': args.file_without_str}
+
+    if args.files:
+        work_with_files(args.files, pattern, flags)
+    else:
+        work_with_stdin(pattern, flags)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""plug"""
 import io
 import grep
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
+    """plug"""
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
     grep.main(['needle?'])
@@ -13,6 +15,7 @@ def test_integrate_stdin_grep(monkeypatch, capsys):
 
 
 def test_integrate_stdin_regex_grep(monkeypatch, capsys):
+    """plug"""
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
     grep.main(['-E', 'needle?'])
@@ -22,6 +25,7 @@ def test_integrate_stdin_regex_grep(monkeypatch, capsys):
 
 
 def test_integrate_stdin_grep_count(monkeypatch, capsys):
+    """plug"""
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle\nneedle suf\nthe needl\npref needle suf'))
     grep.main(['-c', 'needle'])
@@ -30,7 +34,18 @@ def test_integrate_stdin_grep_count(monkeypatch, capsys):
     assert out == '3\n'
 
 
+def test_integrate_stdin_grep_regex_count(monkeypatch, capsys):
+    """plug"""
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        '112\nWWW\nSU35\nF'))
+    grep.main(['-c', '-E', '[0-9]'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
+    """plug"""
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
     grep.main(['needle', 'a.txt'])
@@ -40,6 +55,7 @@ def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
 
 
 def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
+    """plug"""
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
@@ -50,6 +66,7 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
 
 
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
+    """plug"""
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
@@ -57,3 +74,130 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_count(tmp_path, monkeypatch, capsys):
+    """plug"""
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test_integrate_files_regex_grep(tmp_path, monkeypatch, capsys):
+    """plug"""
+    (tmp_path / 'a.txt').write_text('112\nmiu\n')
+    (tmp_path / 'b.txt').write_text('one-two-three\n123')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', '[0-9]', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:123\na.txt:112\n'
+
+
+def test_integrate_file_regex_grep(tmp_path, monkeypatch, capsys):
+    """plug"""
+    (tmp_path / 'a.txt').write_text('wronganswer4\njunkz\npython')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', '[0-9]', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'wronganswer4\n'
+
+
+def test_integrate_file_count_regex_grep(tmp_path, monkeypatch, capsys):
+    """plug"""
+    (tmp_path / 'a.txt').write_text('wronganswer4\njunkz\npython')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', '[0-9]', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_files_count_regex_grep(tmp_path, monkeypatch, capsys):
+    """plug"""
+    (tmp_path / 'a.txt').write_text('wronganswer4\njunkz\npython')
+    (tmp_path / 'b.txt').write_text('LeBron\nJames\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', '[0-9]', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:0\n'
+
+
+def test_init_arguments():
+    """plug"""
+    args = grep.init_arguments(['task', 'input.txt', '-E', '-c'])
+    assert args.pattern == 'task'
+    assert args.files == ['input.txt']
+    assert args.regex
+    assert args.count
+
+
+def test_translate_to_str_re_e():
+    """plug"""
+    test_string = '[0-9]\\d'
+    is_regular = True
+    result_string = grep.translate_str_to_re(test_string, is_regular)
+    assert result_string == '[0-9]\\d'
+
+
+def test_translate_to_str_not_e():
+    """plug"""
+    test_string = 'master'
+    is_regular = False
+    result_string = grep.translate_str_to_re(test_string, is_regular)
+    assert result_string == 'master'
+
+
+def test_filter_list():
+    """plug"""
+    list_ = ['grep', 'clear', 'history', 'bot']
+    pattern = 'r'
+    result_list = grep.filter_list(pattern, list_)
+    assert result_list == ['grep', 'clear', 'history']
+
+
+def test_strip():
+    """plug"""
+    list_ = ['plov\n', 'lavash\n', 'pomidor\n']
+    result_list = grep.strip(list_)
+    assert result_list == ['plov', 'lavash', 'pomidor']
+
+
+def test_process_flag_c():
+    """plug"""
+    result_arr = ['aada', 'adss']
+    new_result = grep.process_flag_c(result_arr)
+    assert new_result == ['2']
+
+
+def test_print_grep_result_without_file_or_with_only_one_file(capsys):
+    """plug"""
+    ans_list = ['a', 'ab', 'abc', 'abcd', 'abcde']
+    output_format = ''
+    grep.print_grep_results(ans_list, output_format)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a\nab\nabc\nabcd\nabcde\n'
+
+
+def test_print_grep_results_with_more_than_one_file(capsys):
+    """plug"""
+    ans_list = ['112', 'shkaf']
+    output_format = 'input.txt:'
+    grep.print_grep_results(ans_list, output_format)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'input.txt:112\ninput.txt:shkaf\n'
+
+
+def test_full_processing_result():
+    word_list = ['google', 'yandex', 'mail', 'bing']
+    pattern = 'a'
+    flags = {'c': False}
+    result = grep.full_processing_result(word_list, pattern, flags)
+    assert result == ['yandex', 'mail']
