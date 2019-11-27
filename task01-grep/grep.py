a7085ad9835431188a9418a820fff221755b4848
Sasha Vorobeva <avorobdm@gmail.com>
2019-11-28 00:10:30 +0300

    3d modification of rep.py

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,67 +1,53 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, IO
 import sys
 import argparse
 import re
 
 
-def parsing(args_str: List[str]):
+def parsing(args_str: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('n', type=str)
-    parser.add_argument('fs', nargs='*')
+    parser.add_argument('needle', type=str)
+    parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
     parser.add_argument('-c', dest='cnt', action='store_true')
     return parser.parse_args(args_str)
 
 
-def line_processing(args: argparse.Namespace) -> str:
-    if not args.regex:
-        return re.escape(args.n)
-    return args.n
-
-
-def print_from_file(lines: List[str], filename: str, len_namespace_files: int):
-    if len_namespace_files == 1:
-        print(*lines, sep='\n')
-    else:
-        for line in lines:
-            print(f'{filename}:{line}', sep='\n')
-
-
-def filew(filename: str, args: argparse.Namespace):
-    with open(filename, 'r') as f:
-        line_pattern = line_processing(args)
-
-        lines = [line.rstrip('\n') for line in f]
-        good_lines = [line for line in lines if re.search(line_pattern, line)]
-        output_lines = [str(len(good_lines))] if args.cnt else good_lines
-
-        print_from_file(output_lines, filename, len(args.fs))
-
-
-def print_from_stdin(lines: List[str]):
-    print(*lines, sep='\n')
-
-
-def stdinw(args: argparse.Namespace):
-    line_pattern = line_processing(args)
-
-    lines = [line.rstrip('\n') for line in sys.stdin]
-    good_lines = [line for line in lines if re.search(line_pattern, line)]
+def format_lines(args: argparse.Namespace, good_lines: List[str], filename: str) -> List[str]:
     output_lines = [str(len(good_lines))] if args.cnt else good_lines
+    if len(args.files) <= 1:
+        return output_lines
+    else:
+        for i, line in enumerate(output_lines):
+            output_lines[i] = f'{filename}:{line}'
+        return output_lines
 
-    print_from_stdin(output_lines)
+
+def working(args: argparse.Namespace, source: [IO, bool], filename: str) -> None:
+    line_pattern = args.needle if args.regex else re.escape(args.needle)
+
+    if args.files:
+        lines = [line.rstrip('\n') for line in source]
+    else:
+        lines = [line.rstrip('\n') for line in sys.stdin]
+
+    good_lines = [line for line in lines if re.search(line_pattern, line)]
+    output_lines = format_lines(args, good_lines, filename)
+
+    print(*output_lines, sep='\n')
 
 
-def main(args_str: List[str]):
+def main(args_str: List[str]) -> None:
     args = parsing(args_str)
 
-    if args.fs:
-        for file in args.fs:
-            filew(file, args)
+    if args.files:
+        for file in args.files:
+            with open(file, 'r') as f:
+                working(args, f, file)
 
     else:
-        stdinw(args)
+        working(args, source=False, filename='')
 
 
 if __name__ == '__main__':
