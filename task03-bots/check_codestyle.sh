e2bfd97369f8830cbb2e803bb0a8f3b9409b1fad
Peter Surkov <petyo333@gmail.com>
2019-11-29 00:22:21 +0300

    Simplify

--- task03-bots/check_codestyle.sh
+++ task03-bots/check_codestyle.sh
@@ -2,11 +2,8 @@
 flake8_flags="--max-line-length=100"
 pylint_flags="--max-line-length=100 --disable=invalid-name,missing-docstring,global-statement,too-many-lines,R --enable=simplifiable-if-statement,redefined-variable-type"
 mypy_flags="--ignore-missing-imports"
-for file in `find . -type f -name "*.py"`
-do
-   flake8 $flake8_flags $file;
-done
+files=`find . -type f -name "*.py"`
 
-flake8 $flake8_flags `find . -type f -name "*.py"`
-pylint $pylint_flags `find . -type f -name "*.py"`
-mypy $mypy_flags `find . -type f -name "*.py"`
+flake8 $flake8_flags $files
+pylint $pylint_flags $files
+mypy $mypy_flags $files
