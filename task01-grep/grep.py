fe01f055c361f79bd1e4e44fbfc497a765f970b5
hwanglight <stray.polina.study@gmail.com>
2019-11-28 09:53:48 +0300

    First part of the task: added keys -E, -c and tests for them

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -5,25 +5,61 @@ import re
 import argparse
 
 
-def main(args_str: List[str]):
-    parser = argparse.ArgumentParser()
+def init_parser() -> argparse.ArgumentParser:   # Инициализируем парсер,
+    parser = argparse.ArgumentParser(              # теперь им можно пользоваться ^^
+        'You can use this program for searching strings in files... If you really want it.\n'
+        'To use you just need to write what you want to search, and where (file).\n'
+        "If you don't input any file we will search in stdin).\n"
+        '(Please make sure that you and your enter with EOF symbol)')
     parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    parser.add_argument('files', nargs='*', default=[sys.stdin], type=argparse.FileType('r'))
+    parser.add_argument('-E', dest='regex', action='store_true',
+                        help='use needle as regular expression')
+    parser.add_argument('-c', dest='cflag', action='store_true',
+                        help="count number of needle's enters")
+    return parser
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
+def add_found_needle(line: str, needle: str, reg: bool, file, num: int, listres: list):   # Если
+    if (needle in line and not reg) or (reg and re.search(needle, line)):  # needle -подстрока line
+        listres.append(f"{file.name + ':' if num > 1 else ''}{line}")  # добавляем её в список
+
+
+def update_cnt_needle(line: str, needle: str, reg: bool, cnt: int) -> int:   # Обновляем счётчик
+    if (needle in line and not reg) or (reg and re.search(needle, line)):     # если needle
+        cnt += 1                                                             # это подстрочка line
+    return cnt
+
+
+def add_cnt(cnt: int, file, num: int, listres: list):  # Добавляем в список результат счёта строчек
+    listres.append(f"{file.name + ':' if num > 1 else ''}{cnt}")
+
+
+def print_res(listres: list):  # Выводим результат на экран
+    for line in listres:  # (переданный список в правильном формате)
+        print(line)
+
+
+def process_data(args):  # Эта функция - своеобразный заместитель главной. Тут вся логика программы
+    listres = []
+    for file in args.files:
+        cnt = 0
+        for line in file.readlines():
             line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+            if args.cflag:
+                cnt = update_cnt_needle(line, args.needle, args.regex, cnt)
+            else:
+                add_found_needle(line, args.needle, args.regex, file, len(args.files), listres)
+        if args.cflag:
+            add_cnt(cnt, file, len(args.files), listres)
+        file.close()
+    print_res(listres)
+
+
+def main(args_str: List[str]):   # Самая главная функция ^^, координирует всю работу
+    parser = init_parser()
+    args = parser.parse_args(args_str)
+    process_data(args)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import io
+import sys
 import grep
 
 
@@ -30,6 +31,15 @@ def test_integrate_stdin_grep_count(monkeypatch, capsys):
     assert out == '3\n'
 
 
+def test_integrate_stdin_regex_grep_count(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    grep.main(['-E', '-c', 'needle?'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
@@ -57,3 +67,61 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_regex_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_parser_init():
+    parser = grep.init_parser()
+    assert parser is not None
+
+
+def test_add_found_needle():
+    listtest = []
+    grep.add_found_needle('Are you sleeping?', 'sleeping?', False, sys.stdin, 1, listtest)
+    assert listtest == ['Are you sleeping?']
+
+
+def test_add_found_regex_needle():
+    listtest = []
+    grep.add_found_needle('Are you sleeping?', 'sleep?', True, sys.stdin, 1, listtest)
+    assert listtest == ['Are you sleeping?']
+
+
+def test_update_cnt_needle():
+    cnt = 0
+    cnt = grep.update_cnt_needle('sleep', 'ee', False, cnt)
+    assert cnt == 1
+
+
+def test_update_cnt_needle_not_changed():
+    cnt = 0
+    cnt = grep.update_cnt_needle('sleep', 'ee1', False, cnt)
+    assert cnt == 0
+
+
+def test_update_regex_cnt_needle():
+    cnt = 0
+    cnt = grep.update_cnt_needle('Are you sleeping?', 'sleep?', True, cnt)
+    assert cnt == 1
+
+
+def test_add_cnt():
+    listtest = []
+    grep.add_cnt(1, sys.stdin, 1, listtest)
+    assert listtest == ['1']
+
+
+def test_print_res(capsys):
+    grep.print_res(['We want to sleep', 'Really want to sleep', 'But we should to write tests TT'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'We want to sleep\nReally want to sleep\nBut we should to write tests TT\n'
