29d7f92813f6e185fad3f3ba80b0953dea4e2c86
chizhova marija aleksandrovna <mariachizhova17@gmail.com>
2019-11-28 17:22:13 +0300

    Implementation of TicTacToe bot

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -9,13 +9,40 @@ class TicTacToeUserHandler(UserHandler):
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        raise NotImplementedError
+        if message == 'start':
+            self.start_game()
+        elif not self.game:
+            self.send_message('Game is not started')
+        else:
+            mark, col, row = message.split()
+            player = Player.X if mark == 'X' else Player.O
+            self.make_turn(player, row=int(row), col=int(col))
 
     def start_game(self) -> None:
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        raise NotImplementedError
+        assert self.game
+        if not self.game.can_make_turn(player, row=row, col=col):
+            self.send_message('Invalid turn')
+        else:
+            self.game.make_turn(player, row=row, col=col)
+            self.send_field()
+            if self.game.is_finished():
+                if not self.game.winner():
+                    self.send_message('Game is finished, draw')
+                else:
+                    self.send_message(
+                        'Game is finished, {} wins'.format('X' if
+                                                           self.game.winner() == Player.X else 'O'))
+                self.game = None
 
     def send_field(self) -> None:
-        raise NotImplementedError
+        assert self.game
+        field = ''
+        for row in self.game.field:
+            for col in row:
+                field += 'X' if col == Player.X else 'O' if col == Player.O else '.'
+            field += '\n'
+        self.send_message(field)
