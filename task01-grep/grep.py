3499289327b3255c97e6c42fcbb1d8e3c8e81f58
shirokov-kirill <k.s.shirokov@mail.ru>
2019-11-28 20:35:16 +0300

    Both First and Second Grep(all flags are added)

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -5,66 +5,96 @@ import re
 import argparse
 
 
-def process(args) -> None:
-    if len(args.files) > 0:
-        for value in args.files:
-            lst_of_strings = read_file(value)
-            lst_of_strings = string_processing(lst_of_strings, args.needle, args.regex, args.count)
-            lst_of_strings = string_format(value, lst_of_strings, len(args.files))
-            printing(lst_of_strings)
-    if len(args.files) == 0:
-        lst = read_stdin()
-        lst = string_processing(lst, args.needle, args.regex, args.count)
-        printing(lst)
+def read_stdin() -> List[str]:
+    new_lines = [line for line in sys.stdin.readlines()]
+    return new_lines
 
 
-def read_stdin() -> list:
-    lst = list()
-    for line in sys.stdin.readlines():
-        lst.append(line.rstrip('\n'))
-    return lst
-
-
-def read_file(filename: str) -> list:
-    lst = list()
+def read_file(filename: str) -> List[str]:
     with open(filename, 'r') as in_file:
-        for line in in_file.readlines():
-            lst.append(line.rstrip('\n'))
-    return lst
+        new_lines = [line for line in in_file.readlines()]
+    return new_lines
 
 
-def string_processing(lst: list, needle: str, regex: bool, count: bool) -> list:
-    lst1 = list()
-    for line in lst:
-        if regex and re.search(needle, line) or not regex and needle in line:
-            lst1.append(line)
-    if count:
-        lst1 = [len(lst1)]
-    return lst1
+def is_line_in_answer(line: str, pattern: str, text_flags: dict) -> bool:
+    result = False
+    if not text_flags.get('is_regex'):
+        pattern = re.escape(pattern)
+    if text_flags.get('full_matches'):
+        pattern = '^' + pattern + '$'
+    if text_flags.get('is_ignore_case') and \
+            re.search(re.compile(pattern.lower()), line.lower()) or \
+            not text_flags.get('is_ignore_case') and \
+            re.search(re.compile(pattern), line):
+        result = True
+    if text_flags.get('invert_result'):
+        if result:
+            result = False
+        else:
+            result = True
+    return result
 
 
-def string_format(filename: str, lst_of_strings: list, length: int) -> list:
-    if length > 1:
-        for i, string in enumerate(lst_of_strings):
-            lst_of_strings[i] = filename + ':' + str(string)
-    return lst_of_strings
+def working_with_text_flags(lines: List[str], pattern: str, flags: dict) -> List[str]:
+    new_list_of_lines = [line for line in lines if is_line_in_answer(line, pattern, flags)]
+    return new_list_of_lines
 
 
-def printing(lst: list) -> None:
-    for value in lst:
-        print(str(value))
+def working_with_print_flags(lines: List[str], prefix: str,
+                             number_of_files: int, flags: dict) -> List[str]:
+    if flags.get('count_mode'):
+        lines = [str(len(lines))]
+    if number_of_files > 2:
+        lines = [f'{prefix}:{line}' for line in lines]
+    lines = [line.rstrip('\n') for line in lines]
+    if flags.get('filenames_matches_exist'):
+        if len(lines) > 0:
+            lines = [prefix]
+        else:
+            lines = []
+    if flags.get('filenames_matches_not_exist'):
+        if len(lines) > 0:
+            lines = []
+        else:
+            lines = [prefix]
+    return lines
+
+
+def printing(lines: list) -> None:
+    for line in lines:
+        print(line)
+
+
+def format_and_print_lines(list_of_lines: List[List[str]], files: List[str],
+                           flags: dict, pattern: str) -> None:
+    files.append('')
+    for i, lines in enumerate(list_of_lines):
+        lines = working_with_text_flags(lines, pattern, flags)
+        lines = working_with_print_flags(lines, files[i], len(files), flags)
+        printing(lines)
 
 
 def main(args_str: List[str]) -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
     parser.add_argument('-c', dest='count', action='store_true')
+    parser.add_argument('-i', dest='ignore', action='store_true')
+    parser.add_argument('-v', dest='invert', action='store_true')
+    parser.add_argument('-x', dest='full_matches', action='store_true')
+    parser.add_argument('-l', dest='filenames_exist', action='store_true')
+    parser.add_argument('-L', dest='filenames_not_exist', action='store_true')
     args = parser.parse_args(args_str)
-    if args.regex:
-        args.needle = re.compile(args.needle)
-    process(args)
+    flags = {'is_regex': args.regex, 'count_mode': args.count, 'is_ignore_case': args.ignore,
+             'invert_result': args.invert, 'full_matches': args.full_matches,
+             'filenames_matches_exist': args.filenames_exist,
+             'filenames_matches_not_exist': args.filenames_not_exist}
+    if len(args.files) > 0:
+        list_of_lines = [read_file(filename) for filename in args.files]
+    else:
+        list_of_lines = [read_stdin()]
+    format_and_print_lines(list_of_lines, args.files, flags, args.pattern)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -112,3 +112,33 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
+def test_integrate_all_keys_print_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt\n'
+
+
+def test_integrate_all_keys_print_not_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-Livx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt\n'
+
+
+def test_integrate_all_keys_count_files_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('fO\nFO\nFoO\n')
+    (tmp_path / 'b.txt').write_text('hello fo?o world\nxfooyfoz\nfooo\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-civx', '-E', 'fo?o', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:3\na.txt:0\n'
