cc1318b3712320a854617550422ece75f6bede68
annapotr <potryasaeva.anna@nail.ru>
2019-11-28 18:58:59 +0300

    Grep, keys: -c, -E

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -5,25 +5,57 @@ import re
 import argparse
 
 
-def main(args_str: List[str]):
+def print_result(result):  # выводит результат
+    for line in result:
+        print(line)
+
+
+def format_lines(result, name_file):  # форматирует строки в вид имя файла : результат
+    return f'{name_file}:{result}'
+
+
+def find_all_lines(args_needle, lines, args_regex):
+    # возвращает все строки, в которых присутсвует строка или регулярное выражение
+    if args_regex:
+        return [line for line in lines if re.search(args_needle, line)]
+    else:
+        return [line for line in lines if args_needle in line]
+
+
+def calculate_result(name_file, args_needle, args_regex, args_count, count_files, lines):
+    # совмещает функции: поиск подстрок и форматирования строк в нужный формат
+    result = find_all_lines(args_needle, lines, args_regex)
+    if args_count:
+        result = [str(len(result))]
+    if count_files > 1:
+        result = [format_lines(line, name_file) for line in result]
+    return result
+
+
+def parse_args(args_str):
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
+    parser.add_argument('files', nargs='*', type=str)
+    parser.add_argument('-c', dest='count', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    return parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+def print_grep(args_needle, args_regex, args_count, in_file, size, file):
+    # вызывает функцию print и calculate
+    lines = [line.rstrip('\n') for line in in_file]
+    print_result(calculate_result(file, args_needle, args_regex, args_count, size, lines))
+
+
+def main(args_str: List[str]):
+    args = parse_args(args_str)
+    if not args.files:
+        print_grep(args.needle, args.regex, args.count, sys.stdin.readlines(), len(args.files), '')
+    else:
+        for file in args.files:
+            with open(file, 'r') as in_file:
+                print_grep(args.needle, args.regex, args.count,
+                           in_file.readlines(), len(args.files), file)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,6 +1,80 @@
 #!/usr/bin/env python3
 import io
+from argparse import Namespace
+
 import grep
+
+
+def test_print_result(tmp_path, monkeypatch, capsys):
+    monkeypatch.chdir(tmp_path)
+    grep.print_result(['b.txt:1', 'a.txt:2'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_print_result_first(tmp_path, monkeypatch, capsys):
+    monkeypatch.chdir(tmp_path)
+    grep.print_result(['b.txt:aba'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:aba\n'
+
+
+def test_print_result_first_second(tmp_path, monkeypatch, capsys):
+    monkeypatch.chdir(tmp_path)
+    grep.print_result(['1'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_format_lines():
+    assert grep.format_lines('a', 'a.txt') == 'a.txt:a'
+    assert grep.format_lines('2', 'a.txt') == 'a.txt:2'
+
+
+def test_find_all_lines():
+    lines = ['aba', 'bcd', 'banana']
+    assert grep.find_all_lines('a', lines, False) == ['aba', 'banana']
+    assert grep.find_all_lines('b', lines, False) == ['aba', 'bcd', 'banana']
+    assert grep.find_all_lines('a*b', lines, True) == ['aba', 'bcd', 'banana']
+    assert grep.find_all_lines('a*b', lines, False) == []
+    assert grep.find_all_lines('abc', lines, False) == []
+
+
+def test_calculate_result():
+    assert grep.calculate_result('a.txt', 'a', False, False, 1, ['abc']) == ['abc']
+    assert grep.calculate_result('a.txt', 'a', False, True, 1, ['abc']) == ['1']
+    assert grep.calculate_result('a.txt', 'a', True, True, 1, ['abc']) == ['1']
+    assert grep.calculate_result('a.txt', 'a', True, False, 1, ['abc']) == ['abc']
+    assert grep.calculate_result('a.txt', 'a', False, False, 2, ['abc']) == ['a.txt:abc']
+    assert grep.calculate_result('a.txt', 'a', False, True, 2, ['abc']) == ['a.txt:1']
+    assert grep.calculate_result('a.txt', 'a', True, True, 2, ['abc']) == ['a.txt:1']
+    assert grep.calculate_result('a.txt', 'a', True, False, 2, ['abc']) == ['a.txt:abc']
+    assert grep.calculate_result('', 'a', False, False, 0, ['abc']) == ['abc']
+    assert grep.calculate_result('', 'a', False, True, 0, ['abc']) == ['1']
+    assert grep.calculate_result('', 'a', True, True, 0, ['abc']) == ['1']
+    assert grep.calculate_result('', 'a', True, False, 0, ['abc']) == ['abc']
+
+
+def test_parse_args():
+    assert grep.parse_args(['-c', 'a', 'a.txt', 'b,txt']) == Namespace(
+        count=True, files=['a.txt', 'b,txt'], needle='a', regex=False)
+    assert grep.parse_args(['a', 'a.txt', 'b,txt']) == Namespace(
+        count=False, files=['a.txt', 'b,txt'], needle='a', regex=False)
+    assert grep.parse_args(['-E', 'a', 'a.txt', 'b,txt']) == Namespace(
+        count=False, files=['a.txt', 'b,txt'], needle='a', regex=True)
+    assert grep.parse_args(['-E', '-c', 'a', 'a.txt', 'b,txt']) == Namespace(
+        count=True, files=['a.txt', 'b,txt'], needle='a', regex=True)
+    assert grep.parse_args(['-c', 'a']) == Namespace(
+        count=True, files=[], needle='a', regex=False)
+    assert grep.parse_args(['a']) == Namespace(
+        count=False, files=[], needle='a', regex=False)
+    assert grep.parse_args(['-E', 'a']) == Namespace(
+        count=False, files=[], needle='a', regex=True)
+    assert grep.parse_args(['-E', '-c', 'a']) == Namespace(
+        count=True, files=[], needle='a', regex=True)
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
@@ -50,10 +124,78 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
 
 
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
-    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'a.txt').write_text('pref needle'
+                                    '\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
     grep.main(['-c', 'needle', 'b.txt', 'a.txt'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_grep_count_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_count_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_stdin_grep_count_regex(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nneedle suf\nthe needl\npref needle suf'))
+    grep.main(['-c', '-E', 'needle'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test_integrate_the_same_files_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'b.txt', 'a.txt', 'b.txt', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\nb.txt:1\nb.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_empty_out(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'first.txt').write_text("what's\nwrong\nwith\nyou?\n")
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'wro*ng?s+', 'first.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+
+
+def test_format_lines_1(capsys):
+    result = 'a'
+    name_file = 'a.txt'
+    lines = grep.format_lines(result, name_file)
+    out, err = capsys.readouterr()
+    assert lines == 'a.txt:a'
+    assert out == ''
+    assert err == ''
+
+
+def test_format_lines_2(capsys):
+    result = 'a', 'bfg'
+    name_file = 'a.txt'
+    lines = grep.format_lines(result, name_file)
+    out, err = capsys.readouterr()
+    assert lines == "a.txt:('a', 'bfg')"
+    assert out == ''
+    assert err == ''
