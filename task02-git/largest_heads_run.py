b80c6478b00c084cb6ef34eb13dbe2f56e58a6fc
Kate <ekaterinaicenko@gmail.com>
2019-11-28 17:17:24 +0300

    Update s in the outer loop

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -17,11 +17,11 @@ def main():
             flip = random.choice([0, 1])
             if flip:
                 cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-                    s += 1
             else:
                 cur_run = 0
+            if max_run < cur_run:
+                max_run = cur_run
+        s += max_run
     print(s, total, s / total)
 
 
