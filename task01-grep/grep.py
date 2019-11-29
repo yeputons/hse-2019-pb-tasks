4099775ea9fcb1556ee220322508e7bd0761f132
nani-des-ka <nikkazakov909@gmail.com>
2019-11-29 20:02:38 +0300

    grep changes

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -5,24 +5,75 @@ import re
 import argparse
 
 
+def regular_expression_check(pattern: str, line: str, amount_output_format: bool,
+                             file_name: str, name_in_output: bool) -> int:
+    if re.search(pattern, line):
+        return print_format(file_name, line, name_in_output, amount_output_format)
+    return 0
+
+
+def which_keys_included(regular_expression_format: bool, amount_output_format: bool,
+                        pattern: str, line: str, file_name: str, name_in_output: bool) -> int:
+    if regular_expression_format:
+        return regular_expression_check(pattern, line, amount_output_format,
+                                        file_name, name_in_output)
+    elif pattern in line:
+        return print_format(file_name, line, name_in_output, amount_output_format)
+    return 0
+
+
+def print_format(file_name: str, line: str, name_in_output: bool,
+                 amount_output_format: bool) -> int:
+    if amount_output_format:
+        return 1
+    if name_in_output:
+        print(f'{file_name}:{line}')
+    else:
+        print(line)
+    return 0
+
+
+def input_from_file(files: List[str], pattern: str, regular_expression_format: bool,
+                    amount_output_format: bool) -> None:
+    name_in_output = len(files) > 1
+    for file_name in files:
+        counter = 0
+        with open(file_name, 'r') as in_file:
+            for line in in_file.readlines():
+                line = line.rstrip('\n')
+                counter += which_keys_included(regular_expression_format,
+                                               amount_output_format, pattern,
+                                               line, file_name, name_in_output)
+        if amount_output_format:
+            print(f'{file_name}:{counter}')
+
+
+def input_from_console(pattern: str, amount_output_format: bool,
+                       regular_expression_format: bool) -> None:
+    counter = 0
+    for line in sys.stdin.readlines():
+        line = line.rstrip('\n')
+        counter += which_keys_included(regular_expression_format, amount_output_format,
+                                       pattern, line, '', False)
+    if amount_output_format:
+        print(counter)
+
+
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-E', dest='regular_expression_format', action='store_true')
+    parser.add_argument('-c', dest='amount_output_format', action='store_true')
     args = parser.parse_args(args_str)
 
     # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
+    if args.files:
+        input_from_file(args.files, args.pattern, args.regular_expression_format,
+                        args.amount_output_format)
+    else:
+        input_from_console(args.pattern, args.amount_output_format,
+                           args.regular_expression_format)
     # STUB ENDS
 
 
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+
 import io
 import grep
-
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
@@ -57,3 +57,92 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle\n'
+
+
+def test_integrate_files_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\n')
+    (tmp_path / 'b.txt').write_text('the needle\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\nb.txt:the needle\n'
+
+
+def test_integrate_file_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\n'
+
+
+def test_integrate_files_grep_regex_failed(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\n')
+    (tmp_path / 'b.txt').write_text('the needl\n')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:pref needle\n'
+
+
+def test_regular_expression_check():
+    if grep.regular_expression_check('mem', 'memory', True, '', False) == 1:
+        print("Test of regular_expression_check is OK")
+    else:
+        print("Test of regular_expression_check FAILED")
+
+
+def test_second_regular_expression_check():
+    if grep.regular_expression_check('mem', 'memory', False, '', False) == 0:
+        print("Test of regular_expression_check is OK")
+    else:
+        print("Test of regular_expression_check FAILED")
+
+
+def test_third_regular_expression_check(capsys):
+    grep.regular_expression_check('m*m', 'memory', False, 'some.txt', True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'some.txt:memory\n'
+
+
+def test_which_keys_included():
+    if grep.which_keys_included(False, False, 'mem', 'memory', '', False) == 1:
+        print("Test of regular_expression_check is OK")
+    else:
+        print("Test of regular_expression_check FAILED")
+
+
+def test_2_which_keys_included():
+    if grep.which_keys_included(True, True, 'm*mm', 'memory', 'mem', False) == 1:
+        print("Test of regular_expression_check is OK")
+    else:
+        print("Test of regular_expression_check FAILED")
+
+
+def test_print_format(capsys):
+    grep.print_format('some.txt', 'mem', True,
+                      False)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'some.txt:mem\n'
+
+
+def test_2_print_format(capsys):
+    grep.print_format('some.txt', 'mem', True,
+                      True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
