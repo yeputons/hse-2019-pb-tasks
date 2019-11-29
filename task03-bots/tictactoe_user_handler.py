302b900cc33d87b119bc309142030da15b2f323f
OlesiaSub <lesya.sub@mail.ru>
2019-11-29 22:33:11 +0300

    add cli.single and tictactoe

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -9,13 +9,49 @@ class TicTacToeUserHandler(UserHandler):
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        raise NotImplementedError
+        command = message.rstrip('\n')
+        if command == 'start':
+            self.start_game()
+            return
+        if not self.game:
+            self.send_message('Game is not started')
+            return
+        name, col, row = command.rstrip('\n').split(maxsplit=2)
+        if name == 'O':
+            self.make_turn(player=Player.O, row=int(row), col=int(col))
+        elif name == 'X':
+            self.make_turn(player=Player.X, row=int(row), col=int(col))
 
     def start_game(self) -> None:
-        raise NotImplementedError
+        self.game = None
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        raise NotImplementedError
+        assert self.game
+        if not self.game.can_make_turn(player, row=row, col=col):
+            self.send_message('Invalid turn')
+            return
+        else:
+            self.game.make_turn(player, row=row, col=col)
+            self.send_field()
+            if self.game.is_finished():
+                if self.game.current_player() == Player.X:
+                    self.send_message('Game is finished, O wins')
+                elif self.game.current_player() == Player.O:
+                    self.send_message('Game is finished, X wins')
+                else:
+                    self.send_message('Game is finished, draw')
+                self.game = None
 
     def send_field(self) -> None:
-        raise NotImplementedError
+        assert self.game
+        field_print = ''
+        for column in self.game.field:
+            for row in column:
+                if row:
+                    field_print += row.name
+                else:
+                    field_print += '.'
+            field_print += '\n'
+        self.send_message(field_print.rstrip('\n'))
