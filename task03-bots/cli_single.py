eb79039e277573a0bd989da58d4d0597e2e106be
Лопатин Михаил <mihan.oi@mail.ru>
2019-11-29 17:21:41 +0500

    Task 3

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
+import sys
+import traceback
+from alarm_user_handler import AlarmUserHandler
+
+
+def send_message(message: str) -> None:
+    print(message)
+
+
 def main() -> None:
-    raise NotImplementedError
+    bot = AlarmUserHandler(send_message=send_message)
+    for line in sys.stdin:
+        try:
+            bot.handle_message(line)
+        except Exception:  # pylint: disable=W0703
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
