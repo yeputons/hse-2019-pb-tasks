d01cf99943b90b78119d149970a73819c8ab9c89
alexandr <sanya89_muravlenko@mail.ru>
2019-11-29 20:31:16 +0300

    Do part 1

--- task03-bots/cli_multiple.py
+++ task03-bots/cli_multiple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import traceback
-from chat_bot import ChatBot
+# from chat_bot import ChatBot
+from alarm_user_handler import AlarmUserHandler
+from bot import UserIndependentBot
 
 
 def send_message(to_user_id: int, message: str) -> None:
@@ -11,7 +13,11 @@ def send_message(to_user_id: int, message: str) -> None:
 
 
 def main() -> None:
-    bot = ChatBot(send_message=send_message)
+    # bot = ChatBot(send_message=send_message)
+    bot = UserIndependentBot(
+        send_message=send_message,
+        user_handler=AlarmUserHandler
+    )
     for line in sys.stdin:
         try:
             user_id, message = line.rstrip('\n').split(maxsplit=1)
