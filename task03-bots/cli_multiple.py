0b0ed5977684889693130357d4698502fb8c697d
Павел <feloom@DESKTOP-QT22092.localdomain>
2019-11-29 22:18:02 +0300

    cli_multiple.py was changed. Now it works like an alarm

--- task03-bots/cli_multiple.py
+++ task03-bots/cli_multiple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import sys
 import traceback
-from chat_bot import ChatBot
+from bot import UserIndependentBot
+from alarm_user_handler import AlarmUserHandler
 
 
 def send_message(to_user_id: int, message: str) -> None:
@@ -12,7 +13,7 @@ def send_message(to_user_id: int, message: str) -> None:
 
 def main() -> None:
     """Пример работы с ботом через консоль."""
-    bot = ChatBot(send_message=send_message)
+    bot = UserIndependentBot(send_message=send_message, user_handler=AlarmUserHandler)
     for line in sys.stdin:
         try:
             user_id, message = line.rstrip('\n').split(maxsplit=1)
