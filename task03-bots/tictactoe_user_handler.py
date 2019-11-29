76f58a9e373fa029eaaefbb3cf4b5f4390b03cae
Kira5005-code <anisimova5005@gmail.com>
2019-11-29 23:15:34 +0300

    Task 4: realize tictactoe-bot

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -4,23 +4,52 @@ from tictactoe import Player, TicTacToe
 
 
 class TicTacToeUserHandler(UserHandler):
-    """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        """Обрабатывает очередное сообщение от пользователя."""
-        raise NotImplementedError
+        message = message.rstrip(' ')
+        if message == 'start':
+            self.start_game()
+        elif not self.game:
+            self.send_message('Game is not started')
+        else:
+            player, row, col = message.rstrip('\n').split(maxsplit=2)
+            now_player = Player.X if player == 'X' else Player.O
+            self.make_turn(now_player, row=int(row), col=int(col))
 
     def start_game(self) -> None:
-        """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        """Обрабатывает ход игрока player в клетку (row, col)."""
-        raise NotImplementedError
+        if self.game.can_make_turn(player, row=row, col=col):
+            self.game.make_turn(player, row=row, col=col)
+        else:
+            self.send_message('Invalid turn')
+            return
+        self.send_field()
+        if self.game.is_finished():
+            if self.game.winner():
+                message = 'Game is finished, '
+                win = self.game.winner()
+                message += 'X wins' if win == Player.X else 'O wins'
+                self.send_message(message)
+            else:
+                self.send_message('Game is finished, draw')
+            self.game = None
 
     def send_field(self) -> None:
-        """Отправляет пользователю сообщение с текущим состоянием игры."""
-        raise NotImplementedError
+        message = ''
+        for i in self.game.field:
+            for j in i:
+                if j:
+                    message += j.name
+                elif j == 2:
+                    message += 'O'
+                else:
+                    message += '.'
+            if len(message) < 8:
+                message += '\n'
+        self.send_message(message)
