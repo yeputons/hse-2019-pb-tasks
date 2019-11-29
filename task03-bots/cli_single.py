c88a0b7a82bbaf04840a056b036dc231f13d55da
Glebanister <glebmar2001@gmail.com>
2019-11-29 21:40:09 +0300

    Implement cli_single.py

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+import sys
+import traceback
+
+from alarm_user_handler import AlarmUserHandler
+from bot import UserIndependentBot
+
+
 def main() -> None:
-    raise NotImplementedError
+    bot = AlarmUserHandler(send_message=print)
+    for line in sys.stdin:
+        try:
+            message = line.rstrip('\n')
+            bot.handle_message(message)
+        except Exception:  # pylint: disable=W0703
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
