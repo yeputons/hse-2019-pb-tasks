79d7c874ae39dd5a1e39cea13df5dd9df0907c53
nani-des-ka <nikkazakov909@gmail.com>
2019-11-29 21:56:17 +0300

    New func get_max_argument, removed 'flips'

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
+from typing import List
 import random
+
+
+def get_max_run(flips: List[int]) -> int:
+    cur_run = 0
+    max_run = 0
+    for flip in flips:
+        if flip:
+            cur_run += 1
+            if cur_run > max_run:
+                max_run += 1
+        else:
+            cur_run = 0
+    return max_run
+
 
 ITERS = 1000
 FLIPS = 100
@@ -10,18 +25,8 @@ def main():
     s = 0
     total = 0
     for _ in range(ITERS):
-        flips = [random.choice([0, 1]) for _ in range(FLIPS)]
         total += 1
-        cur_run = 0
-        max_run = 0
-        for flip in flips:
-            if flip:
-                cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-            else:
-                cur_run = 0
-        s += max_run
+        s += get_max_run(random.choice([0, 1]) for _ in range(FLIPS))
     print(s, total, s / total)
 
 
