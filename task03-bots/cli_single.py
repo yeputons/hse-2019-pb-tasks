d0b20feae8c47ef0d2c26cf454553e6c7a49657e
Peter Surkov <petyo333@gmail.com>
2019-11-29 19:29:19 +0300

    Add alarm bot for single user

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+import sys
+import traceback
+from alarm_user_handler import AlarmUserHandler
+
+
 def main() -> None:
-    raise NotImplementedError
+    alarm_bot = AlarmUserHandler(print)
+    for line in sys.stdin:
+        try:
+            message = line.rstrip('\n')
+            alarm_bot.handle_message(message)
+        except Exception:  # pylint: disable=W0703
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
