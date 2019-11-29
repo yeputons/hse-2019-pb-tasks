95b4c39eece6ff862436c1af52d664eb0d857286
Alexander Morozov <scanhex@gmail.com>
2019-11-29 20:32:17 +0300

    Do the third task

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
+import sys
+import traceback
+from alarm_user_handler import AlarmUserHandler
+
+
+def send_message(message: str) -> None:
+    print(message)
+
+
 def main() -> None:
-    raise NotImplementedError
+    user_handler = AlarmUserHandler(send_message=send_message)
+    for line in sys.stdin:
+        try:
+            message = line.rstrip('\n')
+            user_handler.handle_message(message)
+        except Exception:  # pylint: disable=W0703
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
