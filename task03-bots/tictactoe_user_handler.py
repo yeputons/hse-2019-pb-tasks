6b591052c3a58599bc859c0be73f7f68f7211851
vsgol <v.s.g.clev@gmail.com>
2019-11-30 01:46:07 +0300

    Made TicTacToe bot

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -1,3 +1,4 @@
+import traceback
 from typing import Callable, Optional
 from bot import UserHandler
 from tictactoe import Player, TicTacToe
@@ -9,13 +10,59 @@ class TicTacToeUserHandler(UserHandler):
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        raise NotImplementedError
+        if message == 'start':
+            self.start_game()
+            return
+        if self.game is None:
+            self.send_message('Game is not started')
+            return
+        try:
+            player, col, row = message.split(maxsplit=2)
+            if player == 'X':
+                self.make_turn(Player.X, row=int(row), col=int(col))
+            elif player == 'O':
+                self.make_turn(Player.O, row=int(row), col=int(col))
+            else:
+                print('Invalid turn')
+        except Exception:
+            print('Invalid turn')
 
     def start_game(self) -> None:
-        raise NotImplementedError
+        try:
+            self.game = TicTacToe()
+            self.send_field()
+        except Exception:
+            traceback.print_exc()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        raise NotImplementedError
+        if self.game.can_make_turn(player, row=row, col=col):
+            self.game.make_turn(player, row=row, col=col)
+            self.send_field()
+            if self.game.is_finished():
+                if self.game.winner() is None:
+                    self.send_message('Game is finished, draw')
+                    return
+                if self.game.winner() == Player.X:
+                    self.send_message('Game is finished, X wins')
+                    return
+                if self.game.winner() == Player.O:
+                    self.send_message('Game is finished, O wins')
+                    return
+        else:
+            self.send_message('Invalid turn')
 
     def send_field(self) -> None:
-        raise NotImplementedError
+        try:
+            for line in self.game.field:
+                result_line = ''
+                for i in line:
+                    if i == Player.X:
+                        result_line += 'X'
+                    elif i == Player.O:
+                        result_line += 'O'
+                    else:
+                        result_line += '.'
+                result_line += '\n'
+            self.send_message(result_line.rstrip('\n'))
+        except Exception:
+            traceback.print_exc()
