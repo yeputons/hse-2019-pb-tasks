c33e8a3ba85d529daaf13ff9912d0abcf0b90e93
bikepatch <ivanivanfomenko@gmail.com>
2019-11-29 16:35:29 +0300

    Realised UserHandler in cli_single.py

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
+import sys
+import traceback
+from alarm_user_handler import AlarmUserHandler
+
+
+def send_message(message: str) -> None:
+    print(message)
+
+
 def main() -> None:
+    bot = AlarmUserHandler(send_message)
+    for line in sys.stdin:
+        try:
+            message = line.rstrip('\n')
+            bot.handle_message(message)
+        except Exception:  # pylint: disable=W0703
+            traceback.print_exc()
     raise NotImplementedError
 
 
