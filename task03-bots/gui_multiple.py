f9b01bf7eaabe11f19a08c316698ef56ab5a8a21
Filippov Denis <fild10@yandex.ru>
2019-11-29 16:00:55 +0300

    Add first and second part of task03-bot

--- task03-bots/gui_multiple.py
+++ task03-bots/gui_multiple.py
@@ -2,7 +2,7 @@
 import tkinter
 import traceback
 from typing import Callable, Dict, Optional
-from alarm_user_handler import AlarmUserHandler
+from chat_bot import ChatBot
 from bot import UserIndependentBot
 
 
@@ -47,10 +47,8 @@ class UserWidget(tkinter.LabelFrame):
 def main() -> None:
     user_widgets: Dict[int, UserWidget] = {}
 
-    bot = UserIndependentBot(
-        send_message=lambda user_id, message: user_widgets[user_id].received_message(message),
-        user_handler=AlarmUserHandler
-    )
+    bot = ChatBot(
+        send_message=lambda user_id, message: user_widgets[user_id].received_message(message))
 
     def handle_message(user_id: int, message: str) -> None:
         try:
