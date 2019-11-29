603527440fd73ad99b0b6c6db8cb974fbbfe528a
vavilov mark <vavilovmark@gmail.com>
2019-11-29 21:04:43 +0300

    Apply grep-1 old changes

--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -3,6 +3,34 @@ import io
 import grep
 
 
+def test_search_needle_in_line():
+    assert grep.search_needle_in_line('test?', 'tes', True)
+    assert not grep.search_needle_in_line('test?', 'tst', True)
+    assert not grep.search_needle_in_line('tes?', 'test', False)
+    assert grep.search_needle_in_line('tes?', 'tes?to', False)
+
+
+def helper(check, capsys):
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == check
+
+
+def test_print_asked_string(capsys):
+    grep.print_asked_string([], True, '')
+    helper('0\n', capsys)
+    grep.print_asked_string([1, 2, 3, 4, 5], True, '')
+    helper('5\n', capsys)
+
+
+def test_find_in_file(tmp_path, capsys):
+    (tmp_path / 'a.txt').write_text(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    with open(tmp_path / 'a.txt', 'r') as file:
+        grep.find_in_file(file, 'needle?', True, False)
+        helper('pref needle?\nneedle? suf\nthe needl\npref needle? suf\n', capsys)
+
+
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
