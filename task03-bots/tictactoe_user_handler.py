dcb9eeda64da4ba95cc3615ba6ecb735f3250cda
Худяков Юрий <catchitoo@mail.ru>
2019-11-29 22:50:54 +0300

    Add tictactoe_user_handler, without tests yet

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -3,24 +3,80 @@ from bot import UserHandler
 from tictactoe import Player, TicTacToe
 
 
+class InvalidTurnException(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
 class TicTacToeUserHandler(UserHandler):
-    """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        """Обрабатывает очередное сообщение от пользователя."""
-        raise NotImplementedError
+        if message == 'start':
+            self.start_game()
+        elif self.game:
+            try:
+                new_turn = message.split(' ')
+
+                if len(new_turn) != 3:
+                    raise InvalidTurnException
+
+                if new_turn[0] == 'X':
+                    player = Player.X
+                elif new_turn[0] == 'O':
+                    player = Player.O
+                else:
+                    raise InvalidTurnException
+
+                try:
+                    col, row = map(int, new_turn[1:3])
+                except ValueError:
+                    raise InvalidTurnException
+
+                try:
+                    self.make_turn(player=player, row=row, col=col)
+                except AssertionError:
+                    raise InvalidTurnException
+
+                self.send_field()
+                if self.game.is_finished():
+                    winner = self.game.winner()
+                    if winner is None:
+                        self.send_message('Game is finished, draw')
+                    elif winner == Player.X:
+                        self.send_message('Game is finished, X wins')
+                    elif winner == Player.O:
+                        self.send_message('Game is finished, O wins')
+                    else:
+                        assert False
+
+                    self.game = None
+
+            except InvalidTurnException:
+                self.send_message('Invalid turn')
+        else:
+            self.send_message('Game is not started')
 
     def start_game(self) -> None:
-        """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        """Обрабатывает ход игрока player в клетку (row, col)."""
-        raise NotImplementedError
+        assert self.game
+        self.game.make_turn(player=player, row=row, col=col)
 
     def send_field(self) -> None:
-        """Отправляет пользователю сообщение с текущим состоянием игры."""
-        raise NotImplementedError
+        assert self.game
+        for row in self.game.field:
+            for cell in row:
+                if cell is None:
+                    print('.', end='')
+                elif cell == Player.X:
+                    print('X', end='')
+                elif cell == Player.O:
+                    print('O', end='')
+                else:
+                    assert False
+            print('')
