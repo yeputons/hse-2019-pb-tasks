6194915e88b1e692ab0350dd92727ed2289d4130
Jimiliani <dikorolyov@mail.ru>
2019-11-29 16:37:40 +0300

    Внутренний цикл вынесен в отдельную функцию, улучшена читабельность кода

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -6,23 +6,27 @@ ITERS = 1000
 FLIPS = 100
 
 
+def get_max_run(flips: List[int]) -> int:
+    cur_run = 0
+    max_run = 0
+    for flip in flips:
+        if flip:
+            cur_run += 1
+            if cur_run > max_run:
+                max_run += 1
+        else:
+            cur_run = 0
+    return max_run
+
+
 def main():
     random.seed(123456)
     s = 0
     total = 0
     for _ in range(ITERS):
         total += 1
-        cur_run = 0
-        max_run = 0
         flips = [random.choice([0, 1]) for _ in range(FLIPS)]
-        for flip in flips:
-            if flip:
-                cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-            else:
-                cur_run = 0
-        s += max_run
+        s += get_max_run(flips)
     print(s, total, s / total)
 
 
