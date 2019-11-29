e22b8b2128bdbf9053716957c2c7e80337f5c4e6
sarnatk <tanya200217@gmail.com>
2019-11-29 22:58:49 +0300

    Add part 4 but without tests

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -4,18 +4,50 @@ from tictactoe import Player, TicTacToe
 
 
 class TicTacToeUserHandler(UserHandler):
+    # Реализация логики бота для игры в крестики-нолики с одним пользователем
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        raise NotImplementedError
+        # Обрабатывает очередное сообщение от пользователя
+        if message == 'start':
+            self.start_game()
+            return
+        if self.game is None:
+            self.send_message('Game is not started')
+            return
+        player, row, col = message.split(maxsplit=2)
+        self.make_turn(Player[player], row=int(row), col=int(col))
 
     def start_game(self) -> None:
-        raise NotImplementedError
+        # Начинает новую игру в крестики-нолики и сообщает об этом пользователю
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        raise NotImplementedError
+        # Обрабатывает ход игрока player в клетку (row, col)
+        assert self.game
+        if not self.game.can_make_turn(player, row=row, col=col):
+            self.send_message('Invalid turn')
+            return
+        self.game.make_turn(player, row=row, col=col)
+        self.send_field()
+        if self.game.is_finished():
+            winner = self.game.winner()
+            if winner is None:
+                self.send_message('Game is finished, draw')
+                return
+            self.send_message(f'Game is finished, {winner.name} wins')
+            self.game = None
+        return
 
     def send_field(self) -> None:
-        raise NotImplementedError
+        # Отправляет пользователю сообщение с текущим состоянием игры
+        assert self.game
+        field = ''
+        for row in self.game.field:
+            for col in row:
+                field += col.name if col else '.'
+            field += '\n'
+        self.send_message(field.rstrip('\n'))
