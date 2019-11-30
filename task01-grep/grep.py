04e96eeee88e24efb3be6cb5a8f17e99b28c555a
Vladislav Nosivskoy <vladnosiv@gmail.com>
2019-11-30 18:21:53 +0300

    Second iteration complete

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,121 @@
 #!/usr/bin/env python3
 from typing import List
+from typing import Tuple
+from typing import TextIO
 import sys
 import re
 import argparse
 
 
+def get_lines_of_file(current_stream: TextIO) -> List[str]:
+    return [line.rstrip('\n') for line in current_stream.readlines()]
+
+
+def parse_files(files: List[Tuple[str, TextIO]]) -> List[Tuple[str, List[str]]]:
+    return [(current_file, get_lines_of_file(stream)) for current_file, stream in files]
+
+
+def check_entry(
+        line: str, pattern: str, is_regex: bool, rev: bool, full_match: bool,
+        case_ignore: bool) -> bool:
+    if is_regex:
+        searcher = re.search
+        if full_match:
+            searcher = re.fullmatch
+        if case_ignore:
+            return bool(searcher(pattern, line, flags=re.IGNORECASE)) ^ rev
+        return bool(searcher(pattern, line)) ^ rev
+    else:
+        if case_ignore:
+            pattern = pattern.casefold()
+            line = line.casefold()
+        if full_match:
+            return (pattern == line) ^ rev
+        return (pattern in line) ^ rev
+
+
+def search_in_file(
+        name_file: str, is_regex: bool, rev: bool, full_match: bool, case_ignore: bool,
+        lines: List[str], pattern: str, fmt: str) -> bool:
+    entry = 0
+    for line in lines:
+        if check_entry(line, pattern, is_regex, rev, full_match, case_ignore):
+            entry = 1
+            if fmt != '{0}':
+                print(fmt.format(name_file, line))
+    return bool(entry)
+
+
+def count_in_lines(
+        lines: List[str], is_regex: bool, rev: bool, full_match: bool,
+        case_ignore: bool, pattern: str) -> int:
+    res = 0
+    for line in lines:
+        res += check_entry(line, pattern, is_regex, rev, full_match, case_ignore)
+    return res
+
+
+def search_in_files(
+        parse_data: List[Tuple[str, List[str]]], regex_flag: bool,
+        rev: bool, full_match: bool, case_ignore: bool, pattern: str,
+        count_output: bool, fmt: str):
+    files_with_entry = []
+    for current_file, lines_of_file in parse_data:
+        if count_output:
+            print(fmt.format(
+                current_file, count_in_lines(
+                    lines_of_file, regex_flag, rev, full_match, case_ignore, pattern
+                    )
+                ))
+        else:
+            entry = search_in_file(
+                current_file, regex_flag, rev, full_match, case_ignore,
+                lines_of_file, pattern, fmt
+            )
+            if entry:
+                files_with_entry.append(current_file)
+    if fmt == '{0}':
+        for current_file in files_with_entry:
+            print(fmt.format(current_file, ''))
+
+
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('needle', help='pattern for search', type=str)
     parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-c', help='count pattern in files', dest='count', action='store_true')
+    group.add_argument('-l', help='names files with pattern', dest='names', action='store_true')
+    group.add_argument(
+        '-L', help='names files without pattern', dest='rev_names', action='store_true'
+    )
+    parser.add_argument('-E', help='if pattern is regex', dest='regex', action='store_true')
+    parser.add_argument('-i', help='ignore case', dest='case_ignore', action='store_true')
+    parser.add_argument('-v', help='rev result', dest='rev', action='store_true')
+    parser.add_argument('-x', help='full match', dest='full_match', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    fmt = ''
+    if args.names or args.rev_names:
+        if args.rev_names:
+            args.rev ^= 1
+        fmt = '{0}'
+    elif len(args.files) > 1:
+        fmt = '{0}:{1}'
+    else:
+        fmt = '{1}'
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    files = []
+    if len(args.files) > 0:
+        for current_file in args.files:
+            files.append((current_file, open(current_file, 'r')))
+    else:
+        files = [('sys.stdin', sys.stdin)]
+    parse_data = parse_files(files)
+    search_in_files(
+        parse_data, args.regex, args.rev, args.full_match, args.case_ignore,
+        args.needle, args.count, fmt
+    )
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -3,6 +3,68 @@ import io
 import grep
 
 
+def test_check_entry():
+    assert not grep.check_entry('trash_needle_trash', 'needle', False, True, False, False)
+    assert not grep.check_entry('trash_nedle_trash', 'needle', False, False, False, False)
+    assert grep.check_entry('needle', 'ne.d.e', True, False, False, False)
+    assert not grep.check_entry('needle', 'NeEdLe', False, True, True, True)
+
+
+def test_get_lines_of_file(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('first line\nsecond line\nthird line')
+    monkeypatch.chdir(tmp_path)
+    lines = grep.get_lines_of_file(open('a.txt', 'r'))
+    assert lines == ['first line', 'second line', 'third line']
+
+
+def test_parse_files(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('first line\nsecond line\nthird line')
+    (tmp_path / 'b.txt').write_text('FIRST LINE\nSECOND LINE\nTHIRD LINE')
+    monkeypatch.chdir(tmp_path)
+    lines = grep.parse_files([('a.txt', open('a.txt', 'r')), ('b.txt', open('b.txt', 'r'))])
+    assert lines == [('a.txt', ['first line', 'second line', 'third line']), (
+        'b.txt', ['FIRST LINE', 'SECOND LINE', 'THIRD LINE'])]
+
+
+def test_search_in_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needle\nnedle\nneedle needle needle\ntrash')
+    monkeypatch.chdir(tmp_path)
+    grep.search_in_file('a.txt', False, False, False, False, grep.get_lines_of_file(
+        open('a.txt', 'r')), 'needle', '{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'needle\nneedle needle needle\n'
+    grep.search_in_file('a.txt', True, False, False, False, grep.get_lines_of_file(
+        open('a.txt', 'r')), 'ne.d.e', '{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'needle\nneedle needle needle\n'
+
+
+def test_count_in_lines():
+    lines = ['needle', 'needle needle needle', 'nedle', 'trash']
+    counter = grep.count_in_lines(lines, False, False, False, False, 'needle')
+    assert counter == 2
+    counter = grep.count_in_lines(lines, True, False, False, False, 'nee?d.e')
+    assert counter == 3
+
+
+def test_search_in_files(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('first line\nsecond line\nthird line')
+    (tmp_path / 'b.txt').write_text('LINE LINE LINE\nTRRRRASH')
+    monkeypatch.chdir(tmp_path)
+    parse_data = grep.parse_files([('a.txt', open('a.txt', 'r')), ('b.txt', open('b.txt', 'r'))])
+    grep.search_in_files(parse_data, False, False, False, True, 'line', True, '{0}:{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:3\nb.txt:1\n'
+    parse_data = grep.parse_files([('a.txt', open('a.txt', 'r')), ('b.txt', open('b.txt', 'r'))])
+    grep.search_in_files(parse_data, False, False, False, False, 'line', False, '{0}:{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:first line\na.txt:second line\na.txt:third line\n'
+
+
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
