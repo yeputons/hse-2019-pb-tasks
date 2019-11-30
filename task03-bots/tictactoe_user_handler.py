23d2ac60ef24f51513170118e59b892682c42762
Peter Surkov <petyo333@gmail.com>
2019-11-30 13:23:09 +0300

    Implement function

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -1,4 +1,5 @@
 from typing import Callable, Optional
+import traceback
 from bot import UserHandler
 from tictactoe import Player, TicTacToe
 
@@ -9,18 +10,68 @@ class TicTacToeUserHandler(UserHandler):
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
+    def player_to_str(self, player: Optional[Player]) -> str:
+        if not player:
+            return '.'
+        elif player == Player.X:
+            return 'X'
+        else:
+            return 'O'
+
+    def str_to_player(self, s: str) -> Player:
+        if s == 'X':
+            return Player.X
+        elif s == 'O':
+            return Player.O
+        else:
+            raise ValueError
+
     def handle_message(self, message: str) -> None:
         """Обрабатывает очередное сообщение от пользователя."""
-        raise NotImplementedError
+        if message == 'start':
+            self.start_game()
+        else:
+            if not self.game:
+                self.send_message('Game is not started')
+            else:
+                try:
+                    str_player, str_row, str_col = message.rstrip('\n').split(maxsplit=3)
+                    row = int(str_row)
+                    col = int(str_col)
+                    player = self.str_to_player(str_player)
+                except Exception:  # pylint: disable=W0703
+                    traceback.print_exc()
+                self.make_turn(player, row=row, col=col)
 
     def start_game(self) -> None:
         """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
         """Обрабатывает ход игрока player в клетку (row, col)."""
-        raise NotImplementedError
+        assert self.game
+        try:
+            self.game.make_turn(player, row=row, col=col)
+            self.send_field()
+            self.try_finish()
+        except Exception:  # pylint: disable=W0703
+            self.send_message('Invalid turn')
 
     def send_field(self) -> None:
         """Отправляет пользователю сообщение с текущим состоянием игры."""
-        raise NotImplementedError
+        if self.game:
+            self.send_message('\n'.join(
+                [''.join(map(self.player_to_str, line)) for line in self.game.field]
+                ))
+
+    def try_finish(self) -> None:
+        if self.game and self.game.is_finished():
+            message = 'Game is finished, '
+            winner = self.game.winner()
+            if winner:
+                message += self.player_to_str(winner) + 'wins'
+            else:
+                message += 'draw'
+            self.send_message(message)
+            self.game = None
