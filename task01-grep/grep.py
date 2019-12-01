e53dde0302845613baba8d7052833c774fb2a08b
Atos1337 <victor.samoilov.2002@mail.ru>
2019-12-01 21:45:55 +0300

    Rewrote comments to functions in grep.py

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -19,13 +19,27 @@ def parse(args_str: List[str]) -> argparse.Namespace:
     parser.add_argument('-l', dest='ans', action='store_true')
     parser.add_argument('-L', dest='not_ans', action='store_true')
     args = parser.parse_args(args_str)
+    if args.not_ans:
+        args.ans = not args.ans
+        args.inverse = not args.inverse
+    if args.ignore and not args.regex:
+        args.needle = args.needle.lower()
     return args
 
 
 def print_lines(lines: List[str],
                 args: argparse.Namespace, name_of_file: str = None) -> None:
-    """Выводит список строк(найденных или ненайденных) в правильном формате,
-        заданном ключами"""
+    """Выводит список найденных в правильном формате,
+        заданном ключами. Ключи, задающие формат вывода:
+        1) -v: inverse - найденные строки считаются ненайденными,
+        и наоборот
+        2) -с: count - выводит количество найденных строк, если файл один или
+        строки были считаны из стандартного потока ввода.
+        Если файлов было несколько, то выводит сначала имя файла,
+        а затем количество найденных строк в этом файле
+        3) -l и -L: строчки не ввыодятся. Выводятся имена файлов,
+        где есть хотя бы одна найденная строчка(-l), или где найденные строчки,
+        наоборот, отсутствуют(-L). Используются только для файлов"""
     if args.ans:
         if len(lines) > 0:
             print(name_of_file)
@@ -40,7 +54,8 @@ def print_lines(lines: List[str],
 def find_lines(args: argparse.Namespace, input: TextIO, name_of_file: str = None) -> None:
     """Ищет все подстроки в заданном формате(строка или регулярное выражение),
         которые находятся в данном файле или стандартном потоке ввода и
-        выводит их в правильном формате"""
+        выводит их в правильном формате.
+        Формат смотреть в описании функции print lines"""
     lines_find = []
     lines_unfind = []
     for line in input.readlines():
@@ -63,8 +78,12 @@ def find_lines(args: argparse.Namespace, input: TextIO, name_of_file: str = None
 
 def find_regex(args: argparse.Namespace,
                line: str) -> bool:
-    """Возвращает True, если нашёл регулярное выражение в указанном формате
-        в строке и False иначе"""
+    """Возвращает True, если нашёл подстроку, удовлетворяющую
+        регулярному выражению в указанном формате
+        в строке и False иначе. Ключи, опрежедяющие формат поиска:
+        1) -x: fullmatch - находит строчки, полностью подходящие
+        регулярному выражению
+        2) -i: ignore - игнорирует регистр символов при поиске совпадений"""
     flags = 0
     reg = re.search
     if args.ignore:
@@ -77,8 +96,10 @@ def find_regex(args: argparse.Namespace,
 
 
 def find_str(args: argparse.Namespace, line: str) -> bool:
-    """Возвращает True, если нашёл подстроку needle в указанном формате
-        в строке и False иначе"""
+    """Возвращает True, если нашел подстроку в заданном формате,
+        False иначе. Ключи, определяющие формат поиска:
+        1) -x: fullmatch - находит только полные совпадения строк;
+        2) -i:ignore - игнорирует регистр символов при поиске совпадений"""
     if args.ignore:
         line = line.lower()
     if args.fullmatch:
@@ -89,11 +110,6 @@ def find_str(args: argparse.Namespace, line: str) -> bool:
 
 def main(args_str: List[str]):
     args = parse(args_str)
-    if args.not_ans:
-        args.ans = not args.ans
-        args.inverse = not args.inverse
-    if args.ignore and not args.regex:
-        args.needle = args.needle.lower()
     if args.files:
         for name in args.files:
             with open(name, 'r') as in_file:
