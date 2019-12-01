a33cc573d8a58b16f7c24a8a0871ba4e433059d7
chizhova marija aleksandrovna <mariachizhova17@gmail.com>
2019-12-01 21:03:43 +0300

    Исправлены замечания

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -15,8 +15,12 @@ class TicTacToeUserHandler(UserHandler):
             self.send_message('Game is not started')
         else:
             mark, col, row = message.split()
-            player = Player.X if mark == 'X' else Player.O
-            self.make_turn(player, row=int(row), col=int(col))
+            player = {'X': Player.X, 'O': Player.O}.get(mark)
+            if mark != 'X' and mark != 'O':
+                self.send_message('Invalid turn')
+            else:
+                assert player
+                self.make_turn(player, row=int(row), col=int(col))
 
     def start_game(self) -> None:
         self.game = TicTacToe()
@@ -30,19 +34,14 @@ class TicTacToeUserHandler(UserHandler):
             self.game.make_turn(player, row=row, col=col)
             self.send_field()
             if self.game.is_finished():
-                if not self.game.winner():
-                    self.send_message('Game is finished, draw')
-                else:
-                    self.send_message(
-                        'Game is finished, {} wins'.format('X' if
-                                                           self.game.winner() == Player.X else 'O'))
+                self.send_message(
+                    'Game is finished, {}'.format({None: 'draw', Player.X: 'X wins',
+                                                   Player.O: 'O wins'}.get(self.game.winner())))
                 self.game = None
 
     def send_field(self) -> None:
         assert self.game
-        field = ''
+        field = []
         for row in self.game.field:
-            for col in row:
-                field += 'X' if col == Player.X else 'O' if col == Player.O else '.'
-            field += '\n'
-        self.send_message(field)
+            field.append(''.join([col.name if col else '.' for col in row]))
+        self.send_message('\n'.join(field))
