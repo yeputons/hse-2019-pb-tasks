e1386f10a5db54dc7e3100cfc0ffde5debea98c1
chizhova marija aleksandrovna <mariachizhova17@gmail.com>
2019-12-02 12:02:02 +0300

    Первая часть grep

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
+from typing import Iterable
+from typing import Optional
 from typing import List
 import sys
 import re
+import os
 import argparse
+
+
+def print_result(result: Iterable) -> None:
+    for line in result:
+        print(line)
+
+
+def match_pattern(pattern: str, line: str, is_regex: bool) -> bool:
+    if is_regex:
+        return bool(re.search(pattern, line))
+    else:
+        return pattern in line
+
+
+def filter_lines(pattern: str, lines: Iterable, is_regex: bool) -> List[str]:
+    return [line for line in lines if match_pattern(pattern, line, is_regex)]
+
+
+def grep_lines(lines: Iterable, filename: Optional[str], pattern: str, is_regex: bool,
+               counting_mode: bool) -> List[str]:
+    result = filter_lines(pattern, lines, is_regex)
+    if counting_mode:
+        result = [str(len(result))]
+    if filename:
+        result = [f'{filename}:{line}' for line in result]
+    return result
+
+
+def strip_lines(lines: Iterable) -> List[str]:
+    return [line.rstrip('\n') for line in lines]
 
 
 def main(args_str: List[str]):
@@ -10,20 +43,25 @@ def main(args_str: List[str]):
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    if not args.files:
+        all_lines = [sys.stdin.readlines()]
+        filenames = [None]
+    else:
+        all_lines, filenames = [], []
+        for filename in args.files:
+            if os.path.isfile(filename):
+                filenames.append(filename)
+        for filename in filenames:
+            with open(filename, 'r') as input_file:
+                all_lines.append(input_file.readlines())
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    for lines, filename in zip(all_lines, filenames):
+        lines = strip_lines(lines)
+        print_result(grep_lines(lines, filename if len(args.files) > 1 else None, args.needle,
+                                args.regex, args.count))
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -57,3 +57,105 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_file_grep_count_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_stdin_grep_count_regex(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nneedle suf\nthe needl\npref needle suf'))
+    grep.main(['-c', '-E', 'needle'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test_integrate_files_grep_count_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_print_result_filename_and_text(capsys):
+    grep.print_result(['b.txt:1', 'a.txt:2'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_print_result_text(capsys):
+    grep.print_result(['a', 'b', 'c'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a\nb\nc\n'
+
+
+def test_strip_lines():
+    lines = ['London\n', '\nis\n', 'the capital\n of Great Britain.\n\n']
+    lines = grep.strip_lines(lines)
+    assert lines == ['London', '\nis', 'the capital\n of Great Britain.']
+
+
+def test_filter_lines_without_regex_with_substring():
+    assert grep.filter_lines('b', ['aba', 'bcd', 'banana'],
+                             is_regex=False) == ['aba', 'bcd', 'banana']
+
+
+def test_filter_lines_with_regex_with_regular_expression():
+    assert grep.filter_lines('a*b', ['aba', 'bcd', 'banana'],
+                             is_regex=True) == ['aba', 'bcd', 'banana']
+
+
+def test_filter_lines_without_regex_with_regular_expression():
+    assert grep.filter_lines('a*b', ['aba', 'bcd', 'banana'], is_regex=False) == []
+
+
+def test_filter_lines_with_regex_with_substring():
+    assert grep.filter_lines('a', ['aba', 'bcd', 'banana'], is_regex=True) == ['aba', 'banana']
+
+
+def test_grep_lines_without_regex_without_counting_mode():
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', is_regex=False,
+                           counting_mode=False) == ['file.txt:abc']
+
+
+def test_grep_lines_without_regex_with_counting_mode():
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', is_regex=False,
+                           counting_mode=True) == ['file.txt:1']
+
+
+def test_grep_lines_with_regex_with_counting_mode():
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', is_regex=True,
+                           counting_mode=True) == ['file.txt:1']
+
+
+def test_grep_lines_with_regex_without_counting_mode():
+    assert grep.grep_lines(['abc'], 'file.txt', 'a', is_regex=True,
+                           counting_mode=False) == ['file.txt:abc']
+
+
+def test_match_pattern_without_regex_with_substring():
+    assert grep.match_pattern('Ma', 'Masha', is_regex=False)
+
+
+def test_match_pattern_with_regex_with_regular_expression():
+    assert grep.match_pattern('a*', 'Masha', is_regex=True)
+
+
+def test_match_pattern_with_regex_with_substring():
+    assert grep.match_pattern('Ma', 'Masha', is_regex=True)
+
+
+def test_match_pattern_without_regex_with_regular_expression():
+    assert not grep.match_pattern('a*', 'Masha', is_regex=False)
