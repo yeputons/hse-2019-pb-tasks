03852208a33752ba59606a958ff2f10330ad90b6
Peter Surkov <petyo333@gmail.com>
2019-12-03 23:57:42 +0300

    Add tests

--- task03-bots/tictactoe_user_handler_test.py
+++ task03-bots/tictactoe_user_handler_test.py
@@ -1,3 +1,4 @@
+import pytest
 import pytest_mock
 from tictactoe_user_handler import TicTacToeUserHandler
 
@@ -33,6 +34,15 @@ def test_integrate_invalid_turn(mocker: pytest_mock.MockFixture):
     handler.handle_message('X 0 0')
     handler.handle_message('O 1 1')
 
+    with pytest.raises(AssertionError):
+        handler.handle_message('O -1 0')
+    with pytest.raises(AssertionError):
+        handler.handle_message('O 3 0')
+    with pytest.raises(AssertionError):
+        handler.handle_message('O 0 -1')
+    with pytest.raises(AssertionError):
+        handler.handle_message('O 0 3')
+
     handler.handle_message('O 0 0')
 
     handler.handle_message('O 2 2')
@@ -53,7 +63,7 @@ def test_integrate_invalid_turn(mocker: pytest_mock.MockFixture):
     ]
 
 
-def test_integrate_player_win(mocker: pytest_mock.MockFixture):
+def test_integrate_player_x_win(mocker: pytest_mock.MockFixture):
     send_message = mocker.stub(name='send_message_stub')
     handler = TicTacToeUserHandler(send_message=send_message)
     handler.handle_message('start')
@@ -65,6 +75,23 @@ def test_integrate_player_win(mocker: pytest_mock.MockFixture):
     handler.handle_message('O 2 2')
     assert send_message.call_args_list[-2:] == [
         mocker.call('Game is finished, X wins'),
+        mocker.call('Game is not started'),
+    ]
+
+
+def test_integrate_player_o_win(mocker: pytest_mock.MockFixture):
+    send_message = mocker.stub(name='send_message_stub')
+    handler = TicTacToeUserHandler(send_message=send_message)
+    handler.handle_message('start')
+    handler.handle_message('X 0 0')
+    handler.handle_message('O 2 0')
+    handler.handle_message('X 0 1')
+    handler.handle_message('O 2 1')
+    handler.handle_message('X 1 1')
+    handler.handle_message('O 2 2')
+    handler.handle_message('X 1 2')
+    assert send_message.call_args_list[-2:] == [
+        mocker.call('Game is finished, O wins'),
         mocker.call('Game is not started'),
     ]
 
