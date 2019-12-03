eaee2a120304dae82cc451160496930fdc6925a2
Peter Surkov <petyo333@gmail.com>
2019-12-03 22:22:36 +0300

    Add gitignore

--- task03-bots/check_codestyle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-flake8_flags="--max-line-length=100"
-pylint_flags="--max-line-length=100 --disable=invalid-name,missing-docstring,global-statement,too-many-lines,R --enable=simplifiable-if-statement,redefined-variable-type"
-mypy_flags="--ignore-missing-imports"
-files=`find . -type f -name "*.py"`
-
-flake8 $flake8_flags $files
-pylint $pylint_flags $files
-mypy $mypy_flags $files
