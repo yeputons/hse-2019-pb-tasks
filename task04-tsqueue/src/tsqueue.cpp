01a31d5af3140557f3384b6157f0a4d0977dad36
Tarasov Denis <dt6cgsg@gmail.com>
2019-12-04 23:06:08 +0300

    Implement thread save queue

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,26 +1,31 @@
+#include <assert.h>
 #include "tsqueue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    queue_init(&(q->q));
+    assert(pthread_mutex_init(&(q->mutex), NULL) == 0);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    queue_destroy(&(q->q));
+    pthread_mutex_destroy(&(q->mutex));
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    static_cast<void>(data);  // Как-нибудь используем переменную.
+    pthread_mutex_lock(&(q->mutex));   
+    queue_push(&(q->q), data);
+    pthread_mutex_unlock(&(q->mutex));
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    // TODO
-    static_cast<void>(q);
-    static_cast<void>(data);
-    return false;
+    pthread_mutex_lock(&(q->mutex));   
+    bool rv = !queue_empty(&(q->q));
+
+    if (rv)
+        *data = queue_pop(&(q->q));
+    pthread_mutex_unlock(&(q->mutex));
+    
+    return rv;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
