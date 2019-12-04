14699367eacddff2bcd17c995d404c1e62ff6a40
Sasha Vorobeva <avorobdm@gmail.com>
2019-12-05 01:46:40 +0300

    Add test for empty string

--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -45,28 +45,38 @@ def test_format_lines_one_cnt(tmp_path, capsys):
     assert out == ['3']
 
 
-def test_file_working(tmp_path, capsys):
+def test_file_processing_lines(tmp_path, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     with open('a.txt', 'r') as f:
-        grep.working(argparse.Namespace(
+        grep.processing_lines(argparse.Namespace(
             needle='.ref', files=['a.txt', 'b.txt'], regex=True, cnt=False), f, 'a.txt')
     with open('b.txt', 'r') as f:
-        grep.working(argparse.Namespace(
+        grep.processing_lines(argparse.Namespace(
             needle='.ref', files=['a.txt', 'b.txt'], regex=True, cnt=False), f, 'b.txt')
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'a.txt:pref needle\nb.txt:pref needle suf\n'
 
 
-def test_stdin_working(monkeypatch, capsys):
+def test_stdin_processing_lines(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    grep.working(argparse.Namespace(needle='needle', files=[], regex=False, cnt=False),
+    grep.processing_lines(argparse.Namespace(needle='needle', files=[], regex=False, cnt=False),
                  source=sys.stdin, filename='')
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
+
+
+def test_empty_lines(tmp_path, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle gref\n')
+    with open('a.txt', 'r') as f:
+        grep.processing_lines(argparse.Namespace(
+            needle='if', files=['a.txt'], regex=False, cnt=False), f, 'a.txt')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
 
 
 def test_complex_files_grep_count(tmp_path, monkeypatch, capsys):
