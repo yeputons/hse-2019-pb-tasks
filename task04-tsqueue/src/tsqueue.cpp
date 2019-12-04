7ccf6722b118722f2e4aa6308650dcd3fe77cf62
Tarasov Denis <dt6cgsg@gmail.com>
2019-12-04 23:14:20 +0300

    clang format fix

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,5 +1,5 @@
-#include <assert.h>
 #include "tsqueue.h"
+#include <assert.h>
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     queue_init(&(q->q));
@@ -12,19 +12,19 @@ void threadsafe_queue_destroy(ThreadsafeQueue *q) {
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    pthread_mutex_lock(&(q->mutex));   
+    pthread_mutex_lock(&(q->mutex));
     queue_push(&(q->q), data);
     pthread_mutex_unlock(&(q->mutex));
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    pthread_mutex_lock(&(q->mutex));   
+    pthread_mutex_lock(&(q->mutex));
     bool rv = !queue_empty(&(q->q));
 
     if (rv)
         *data = queue_pop(&(q->q));
     pthread_mutex_unlock(&(q->mutex));
-    
+
     return rv;
 }
 
