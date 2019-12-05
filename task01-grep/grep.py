94612ab84a50f32969bd41002f0a72261603492d
Ilya Onofriychuk <ilyaonof@yandex.ru>
2019-12-06 01:01:57 +0300

    Initial commit

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,70 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, Pattern, Iterable, Any
 import sys
-import re
 import argparse
+import re
+
+
+def filter_matching_lines(pattern: Pattern, lines: List[str]) -> List[str]:
+    return [line for line in lines if re.search(pattern, line)]
+
+
+def print_lines(lines: Iterable) -> None:
+    for line in lines:
+        print(line)
+
+
+def build_pattern(string: str, is_regex: bool) -> Pattern:
+    if not is_regex:
+        string = re.escape(string)
+    return re.compile(string)
+
+
+def rstrip_lines(lines: List[str], chars='\n') -> List[str]:
+    return [line.rstrip(chars) for line in lines]
+
+
+def count_lines(line_sets: List[List[Any]]) -> List[int]:
+    return [len(lines) for lines in line_sets]
+
+
+def add_prefix(prefix: str, lines: Iterable, chars_between='') -> List[str]:
+    return list(map(lambda x: '{}{}{}'.format(prefix, chars_between, x), lines))
 
 
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
+    parser.add_argument('-c', dest='count', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    pattern = build_pattern(args.needle, args.regex)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    result = []
+
+    for file_name in args.files:
+        with open(file_name, 'r') as file:
+            result.append(file.readlines())
+
+    if not args.files:
+        result.append(sys.stdin.readlines())
+
+    result = list(map(
+        lambda lines: filter_matching_lines(pattern, rstrip_lines(lines)),
+        result
+    ))
+
+    if args.count:
+        result = [[str(x)] for x in count_lines(result)]
+
+    if len(args.files) > 1:
+        for i, file_name in enumerate(args.files):
+            result[i] = add_prefix(file_name, result[i], chars_between=':')
+
+    for lines in result:
+        print_lines(lines)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,6 +1,65 @@
-#!/usr/bin/env python3
 import io
+import re
 import grep
+
+
+def test_unit_filter_matching_lines():
+    lines = ['incorrect line', 'correct line', 'right line', 'correct', 'c\no\nr\nr\ne\nc\nt']
+    answer = grep.filter_matching_lines(re.escape('correct'), lines)
+    assert answer == ['incorrect line', 'correct line', 'correct']
+
+
+def test_unit_filter_matching_lines_regex():
+    lines = ['...', 'dot', 'dot', 'abc', 'c', '']
+    answer = grep.filter_matching_lines(re.compile('...'), lines)
+    assert answer == ['...', 'dot', 'dot', 'abc']
+
+
+def test_unit_print_lines(capsys):
+    lines = ['first line', '<prefix>:second line', 'third line']
+    grep.print_lines(lines)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'first line\n' \
+                  '<prefix>:second line\n' \
+                  'third line\n'
+
+
+def test_unit_count_lines():
+    lines = [['first', '2th', 'third'], [], [], ['word']]
+    lines = grep.count_lines(lines)
+    assert lines == [3, 0, 0, 1]
+
+
+def test_unit_count_lines_empty():
+    lines = []
+    lines = grep.count_lines(lines)
+    assert lines == []
+
+
+def test_unit_build_pattern():
+    substr = 'needle'
+    result = grep.build_pattern(substr, False)
+    assert result == re.compile(re.escape(substr))
+
+
+def test_unit_build_pattern_regex():
+    regex = '[0-9][a-z][Z-Z]*'
+    result = grep.build_pattern(regex, True)
+    assert result == re.compile(regex)
+
+
+def test_unit_rstrip_lines():
+    lines = ['begin? \n middle end?', '\n\n\n\n\n\n\n', '', 'pref\n', '\ntext...text\t']
+    lines = grep.rstrip_lines(lines, '\n')
+    assert lines == ['begin? \n middle end?', '', '', 'pref', '\ntext...text\t']
+
+
+def test_unit_add_prefix():
+    lines = [' <- prefix', 'The str need a prefix', '']
+    prefix = 'title'
+    lines = grep.add_prefix(prefix, lines, chars_between=')')
+    assert lines == ['title) <- prefix', 'title)The str need a prefix', 'title)']
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
@@ -57,3 +116,48 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_grep_same_files(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('Прямой автострады тугая полоска\n'
+                                    'И я по бетонной гремящей струне\n'
+                                    'Лечу на железной гремящей повозке\n'
+                                    'Где прадед мой ездил на рыжем коне')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'гремя', 'a.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\na.txt:2\n'
+
+
+def test_integrate_files_grep_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('Living easy, living free\n'
+                                    'Season ticket on a one-way ride\n'
+                                    'Asking nothing, leave me be\n'
+                                    'Taking everything in my stride...')
+    (tmp_path / 'b.txt').write_text('Not a song\n'
+                                    '(((\n'
+                                    'Not a song\n'
+                                    '(((')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['Not a song', 'a.txt', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:Not a song\nb.txt:Not a song\n'
+
+
+def test_integrate_grep_empty_stdin(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(''))
+    grep.main(['Not a song'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+
+
+def test_integrate_grep_empty_file_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'Not a song', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '0\n'
