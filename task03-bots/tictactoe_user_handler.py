455093b460a94454f151d61f59bf55ddbd8f607b
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-12-05 19:15:47 +0300

    I have finally done this

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -4,23 +4,52 @@ from tictactoe import Player, TicTacToe
 
 
 class TicTacToeUserHandler(UserHandler):
-    """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        """Обрабатывает очередное сообщение от пользователя."""
-        raise NotImplementedError
+        message = message.rstrip(' ')
+        if message == 'start':
+            self.start_game()
+        elif not self.game:
+            self.send_message('Game is not started')
+        else:
+            player, col, row = message.rstrip(' ').split(maxsplit=2)
+            right_player = Player.X if player == 'X' else Player.O
+            self.make_turn(right_player, row=int(row), col=int(col))
 
     def start_game(self) -> None:
-        """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        """Обрабатывает ход игрока player в клетку (row, col)."""
-        raise NotImplementedError
+        assert self.game
+        if not self.game.can_make_turn(player, row=row, col=col):
+            self.send_message('Invalid turn')
+        else:
+            self.game.make_turn(player, row=row, col=col)
+            self.send_field()
+            if self.game.is_finished():
+                message = 'Game is finished, '
+                winner = self.game.winner()
+                if not winner:
+                    message += 'draw'
+                else:
+                    message += 'X' if winner == Player.X else 'O'
+                    message += ' wins'
+                self.send_message(message)
+                self.game = None
 
     def send_field(self) -> None:
-        """Отправляет пользователю сообщение с текущим состоянием игры."""
-        raise NotImplementedError
+        assert self.game
+        message = ''
+        for row in range(3):
+            for col in range(3):
+                if not self.game.field[row][col]:
+                    message += '.'
+                else:
+                    message += 'X' if self.game.field[row][col] == Player.X else 'O'
+            if row < 2:
+                message += '\n'
+        self.send_message(message)
