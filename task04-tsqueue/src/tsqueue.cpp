57c46fb43c5ec15475be194f34fe21384c53193c
PolinaVorobeva <vpd.polina@gmail.com>
2019-12-05 07:15:00 -0500

    make naive realization of 'tsqueue'

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -2,25 +2,24 @@
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    queue_init(&q->q);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
     // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    static_cast<void>(data);  // Как-нибудь используем переменную.
+    queue_push(&q->q, data);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
     // TODO
-    static_cast<void>(q);
-    static_cast<void>(data);
-    return false;
+    if (queue_empty(&q->q)) return false;
+    *data = queue_pop(&q->q);
+    return true;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
