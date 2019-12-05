2f5c38e429a79dceac35f23a43607cb4488ff2fc
Vorobeva Aleksandra <avorobdm@gmail.com>
2019-12-05 07:28:02 -0500

    First part of tsqueue

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,25 +1,26 @@
 #include "tsqueue.h"
 
+
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    queue_init(&q->q);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
     // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    static_cast<void>(data);  // Как-нибудь используем переменную.
+    queue_push(&q->q, &data);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
     // TODO
-    static_cast<void>(q);
-    static_cast<void>(data);
+    if (!queue_empty(&q->q)) {
+        *data = queue_pop(&q->q);
+    }
     return false;
 }
 
