3bb3d52a1dabaafcd24ee6675b15d2754a4e4873
Vorobeva Aleksandra <avorobdm@gmail.com>
2019-12-05 09:00:39 -0500

    Programm with bug:(

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,27 +1,32 @@
 #include "tsqueue.h"
-
+#include <stdio.h>
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    // TODO
+    pthread_mutex_init(&q->mutex, nullptr);
     queue_init(&q->q);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    // TODO
     queue_destroy(&q->q);
+    pthread_mutex_destroy(&q->mutex);   
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    // TODO
+    pthread_mutex_lock(&q->mutex);
     queue_push(&q->q, &data);
+    pthread_mutex_unlock(&q->mutex);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    // TODO
-    if (!queue_empty(&q->q)) {
-        *data = queue_pop(&q->q);
+    pthread_mutex_lock(&q->mutex);
+    if (queue_empty(&q->q)) {
+        pthread_mutex_unlock(&q->mutex);
+        return false;      
     }
-    return false;
+    *data = queue_pop(&q->q);
+    printf("%p\n", data);
+    pthread_mutex_unlock(&q->mutex);
+    return true;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
