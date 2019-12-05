67784b622a1f31baf95968b70143787e2c82cda0
Danil Bubnov <bubnovdanilbk@gmail.com>
2019-12-05 11:38:04 +0300

    Add third task

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
+import sys
+import traceback
+from bot import UserHandler
+from alarm_user_handler import AlarmUserHandler
+from bot import UserIndependentBot
+
+
+def send_message(user_id: int, message: str) -> None:
+    print(message)
+
+
 def main() -> None:
-    raise NotImplementedError
+    bot = UserIndependentBot(send_message=send_message, user_handler=AlarmUserHandler)
+    for line in sys.stdin:
+        try:
+            message = line.rstrip('\n')
+            bot.handle_message(1, message)
+        except Exception:  # pylint: disable=W0703
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
