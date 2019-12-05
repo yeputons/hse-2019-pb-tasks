15c5b8e964a4befaeb1579e6beb34b2d99db5970
PolinaVorobeva <vpd.polina@gmail.com>
2019-12-05 08:44:51 -0500

    add mutexes

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,29 +1,38 @@
 #include "tsqueue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    // TODO
-    queue_init(&q->q);
+  // TODO
+  pthread_mutex_init(&q->m, NULL);
+  queue_init(&q->q);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    // TODO
-    queue_destroy(&q->q);
+  // TODO
+  queue_destroy(&q->q);
+  pthread_mutex_destroy(&q->m);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    // TODO
-    queue_push(&q->q, data);
+  // TODO
+  pthread_mutex_lock(&q->m);
+  queue_push(&q->q, data);
+  pthread_mutex_unlock(&q->m);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    // TODO
-    if (queue_empty(&q->q)) return false;
-    *data = queue_pop(&q->q);
-    return true;
+  // TODO
+  pthread_mutex_lock(&q->m);
+  if (queue_empty(&q->q)) {
+    pthread_mutex_unlock(&q->m);
+    return false;
+  }
+  *data = queue_pop(&q->q);
+  pthread_mutex_unlock(&q->m);
+  return true;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    // TODO(2)
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    return nullptr;
+  // TODO(2)
+  static_cast<void>(q); // Как-нибудь используем переменную.
+  return nullptr;
 }
