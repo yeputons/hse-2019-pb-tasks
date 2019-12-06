580d598b4152946ffd6aa3058d5ee1ecb1f53e63
Mikhaylova Anna <annamikhaylova6263@gmail.com>
2019-12-06 19:36:09 +0300

    Change ChatBot to AlarmUserHandler

--- task03-bots/cli_multiple.py
+++ task03-bots/cli_multiple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import sys
 import traceback
-from chat_bot import ChatBot
+from alarm_user_handler import AlarmUserHandler
+from bot import UserIndependentBot
 
 
 def send_message(to_user_id: int, message: str) -> None:
@@ -11,7 +12,8 @@ def send_message(to_user_id: int, message: str) -> None:
 
 
 def main() -> None:
-    bot = ChatBot(send_message=send_message)
+    """Пример работы с ботом через консоль."""
+    bot = UserIndependentBot(send_message=send_message, user_handler=AlarmUserHandler)
     for line in sys.stdin:
         try:
             user_id, message = line.rstrip('\n').split(maxsplit=1)
