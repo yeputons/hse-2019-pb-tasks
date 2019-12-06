8be6b496d56cfaca5ac1f24f4568a2eb4e54624e
achaikova <57727520+achaikova@users.noreply.github.com>
2019-12-06 18:52:38 +0300

    1st iteration

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,56 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, IO
+from pathlib import Path
 import sys
 import re
 import argparse
 
 
+def print_out_result(result: List[str], is_amount: bool, print_name: bool, name_of_file: str):
+    if is_amount:
+        result = [str(len(result))]
+    if print_name:
+        result = [f'{name_of_file}:' + str(x) for x in result]
+    for x in result:
+        print(x)
+
+
+def grep_input(input_stream: IO[str], pattern: str, is_amount: bool, print_name: bool,
+               name_of_file: str):
+    result = []
+    for line in input_stream:
+        line = line.rstrip('\n')
+        if re.search(pattern, line):
+            result.append(line)
+    print_out_result(result, is_amount, print_name, name_of_file)
+
+
+def redirect_input_stream(paths: List[Path], pattern: str, is_amount: bool):
+    if not paths:
+        grep_input(sys.stdin, pattern, is_amount, False, str(paths))
+    else:
+        for path in paths:
+            path = Path(path)
+            if path.is_dir():
+                print(f'Is a directory: {path}')
+            elif path.is_file():
+                grep_input(path.open('r'), pattern, is_amount, len(paths) > 1, path.name)
+            else:
+                print(f'File not found')
+
+
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
-    parser.add_argument('files', nargs='*')
-    parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('pattern', type=str, help='a pattern to search for')
+    parser.add_argument('files', metavar='FILES', nargs='*',
+                        help='the files(s) to search')
+    parser.add_argument('-c', '--is_amount', action='store_true',
+                        help='print out the amount of found lines')
+    parser.add_argument('-E', '--is_regex', action='store_true',
+                        help='search for a regular expression')
     args = parser.parse_args(args_str)
-
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    pattern = args.pattern if args.is_regex else re.escape(args.pattern)
+    redirect_input_stream(args.files, pattern, args.is_amount)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import io
-import grep
+from pathlib import Path
+from grep import main, redirect_input_stream, grep_input, print_out_result
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    grep.main(['needle?'])
+    main(['needle?'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
@@ -15,7 +16,7 @@ def test_integrate_stdin_grep(monkeypatch, capsys):
 def test_integrate_stdin_regex_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
-    grep.main(['-E', 'needle?'])
+    main(['-E', 'needle?'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
@@ -24,36 +25,217 @@ def test_integrate_stdin_regex_grep(monkeypatch, capsys):
 def test_integrate_stdin_grep_count(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle\nneedle suf\nthe needl\npref needle suf'))
-    grep.main(['-c', 'needle'])
+    main(['-c', 'needle'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == '3\n'
 
 
+def test_integrate_grep_redex_grep(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref hello?\nngggghhello? or not to hello?\nhel\noh hi mark'))
+    main(['hello?'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref hello?\nngggghhello? or not to hello?\n'
+
+
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    grep.main(['needle', 'a.txt'])
+    main(['needle', 'a.txt'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle suf\n'
+
+
+def test_integrate_file_regex_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('GroundControltoMajorTom\nGroundControltoMajortim\n'
+                                    'Ten, Ton, Tep,SevenSixFiveFourThreeTinOneLift off')
+    monkeypatch.chdir(tmp_path)
+    main(['-E', 'T?m', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'GroundControltoMajorTom\nGroundControltoMajortim\n'
+
+
+def test_integrate_file_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('Donot\ngogentleintotthatgoodnight\ngogogo\n'
+                                    'gioio\ngggggg\nog\ngottagofast')
+    monkeypatch.chdir(tmp_path)
+    main(['-c', 'go', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test_integrate_file_regex_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('dejavut\ndddekkk;slkl\nvoooouu\n'
+                                    'vousaveszvu\nredex\nog\nvvvviiivivivivovovlk')
+    monkeypatch.chdir(tmp_path)
+    main(['-c', '-E', 'v*u', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
 
 
 def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    grep.main(['needle', 'b.txt', 'a.txt'])
+    main(['needle', 'b.txt', 'a.txt'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
+def test_integrate_files_regex_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('oooofklklklklkmmmm\nalllllienssssua??\n')
+    (tmp_path / 'b.txt').write_text('oooofofofo\nkkokkkkk\n')
+    monkeypatch.chdir(tmp_path)
+    main(['-E', 'o*f', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:oooofofofo\na.txt:oooofklklklklkmmmm\n'
 
 
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
-    grep.main(['-c', 'needle', 'b.txt', 'a.txt'])
+    main(['-c', 'needle', 'b.txt', 'a.txt'])
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_regex_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('helodddrknsmldfrnd\nururuuruurnrunfrst\nofffffcrs')
+    (tmp_path / 'b.txt').write_text('lalalalla\nallstrkkm\nlilalolulyllelzlxlcl')
+    monkeypatch.chdir(tmp_path)
+    main(['-c', '-E', 'l*a', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:3\na.txt:0\n'
+
+
+def test_invalid_file_name(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('dejavut\ndddekkk;slkl\nvoooouu\n'
+                                    'vousaveszvu\nredex\nog\nvvvviiivivivivovovlk')
+    monkeypatch.chdir(tmp_path)
+    main(['-c', '-E', 'v*u', 'b.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'File not found\n'
+
+
+def test_attempt_at_integration_testing(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    (tmp_path / 'c.txt').write_text('pref neesvsvsvdle\nneedssssssle suf\n')
+    (tmp_path / 'd.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    main(['-c', 'needle', 'a.txt', 'b.txt', 'c.txt', 'c.txt', 'd1.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:1\nc.txt:0\nc.txt:0\nFile not found\n'
+
+
+def test_is_a_directory(tmp_path, monkeypatch, capsys):
+    direct = tmp_path / 'name'
+    direct.mkdir()
+    monkeypatch.chdir(tmp_path)
+    main(['-c', '-E', 'v*u', 'name'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'Is a directory: name\n'
+
+
+def test_grep_files_file_not_found(tmp_path, monkeypatch, capsys):
+    direct = Path(tmp_path / 'b.txt')
+    (tmp_path / 'a.txt').write_text('dejavut\ndddekkk;slkl\nvoooouu\n'
+                                    'vousaveszvu\nredex\nog\nvvvviiivivivivovovlk')
+    monkeypatch.chdir(tmp_path)
+    redirect_input_stream([direct], 'abc', True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'File not found\n'
+
+
+def test_grep_input_file(tmp_path, monkeypatch, capsys):
+    needle = 'needle'
+    p = tmp_path / 'a.txt'
+    p.write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    with p.open() as f:
+        grep_input(f, needle, False, False, 'a.txt')
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == 'pref needle suf\n'
+
+
+def test_grep_input_stdin(capsys):
+    output = io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    needle = 'needle'
+    grep_input(output, needle, False, False, '')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\npref needle? suf\n'
+
+
+def test_grep_input_stdin_is_count(capsys):
+    output = io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    needle = 'needle'
+    grep_input(output, needle, True, False, '')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+
+def test_grep_input_stdin_is_regex(capsys):
+    output = io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    needle = 'need?'
+    grep_input(output, needle, False, False, '')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle?\nneedle? suf\nthe needl\npref needle? suf\n'
+
+
+def test_grep_input_stdin_is_regex_is_amount(capsys):
+    output = io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf')
+    needle = 'need?'
+    grep_input(output, needle, True, False, '')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
+def test_grep_input_files(tmp_path, monkeypatch, capsys):
+    needle = 'needle'
+    p = tmp_path / 'a.txt'
+    f = tmp_path / 'b.txt'
+    p.write_text('the needl\npref needle suf')
+    f.write_text('the needlejnkejbsvkj\npref needlescassuf')
+    monkeypatch.chdir(tmp_path)
+    redirect_input_stream([p, f], needle, True)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:2\n'
+
+
+def test_print_out_result_1(capsys):
+    print_out_result(['hello', 'friend'], False, False, '')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'hello\nfriend\n'
+
+
+def test_print_out_result_2(capsys):
+    print_out_result(['hello', 'friend'], False, True, 'i_say')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'i_say:hello\ni_say:friend\n'
