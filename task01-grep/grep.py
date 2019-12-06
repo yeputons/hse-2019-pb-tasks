b379c2498a73c1b2b87329e3ce141323a10563ce
Alex <luchinin.lescha@yandex.ru>
2019-12-06 20:53:13 +0400

    Realize the first of task

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python3
-from typing import List
+
+from typing import List, Iterable
 import sys
 import re
 import argparse
+
+
+def filter_strings_by_pattern(pattern: str,
+                              data: Iterable[Iterable[str]]) -> List[List[str]]:
+    return [[line for line in item if re.search(pattern, line)] for item in data]
+
+
+def count_filtered_strings(data: Iterable[Iterable[str]]) -> List[List[str]]:
+    return [[str(sum(1 for _ in item))] for item in data]
+
+
+def format_output_string(name_file: str,
+                         line: str,
+                         cond: bool):
+    if cond:
+        return '{}:{}'.format(name_file, line)
+    return line
+
+
+def print_lines(files: Iterable[str],
+                data: Iterable[Iterable[str]],
+                cond: bool):
+    for name_file, item in zip(files, data):
+        for line in item:
+            print(format_output_string(name_file, line, cond))
+
+
+def strip_lines(lines: Iterable[str]) -> List[str]:
+    return [line.rstrip('\n') for line in lines]
 
 
 def main(args_str: List[str]):
@@ -10,20 +40,36 @@ def main(args_str: List[str]):
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
+    parser.add_argument('-c', dest='count', action='store_true')
+
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    pattern = args.needle
+    count = args.count
+    regex = args.regex
+    files = args.files
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    input_data = []
+    if files:
+        for file in files:
+            with open(file, 'r') as input_file:
+                input_data.append(strip_lines(input_file.readlines()))
+    else:
+        input_data.append(strip_lines(sys.stdin.readlines()))
+        files.append(None)
+
+    if not regex:
+        pattern = re.escape(pattern)
+    pattern = re.compile(pattern)
+
+    filtered_strings = filter_strings_by_pattern(pattern, input_data)
+
+    output_data = filtered_strings
+
+    if count:
+        output_data = count_filtered_strings(filtered_strings)
+
+    print_lines(files, output_data, len(files) > 1)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
 import io
+import re
 import grep
 
 
@@ -57,3 +57,94 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_regex_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('ahaha\natata\n')
+    (tmp_path / 'b.txt').write_text('fasdkfj\naaaaa')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'a+a+a', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:1\na.txt:0\n'
+
+
+def test_unit_filter_strings_by_pattern_first():
+    pattern = 'a?lex?'
+    pattern = re.compile(pattern)
+    data = [['alex', '?', 'alex?', '????alex', 'ale', 'le'],
+            ['alex?', 'ale', 'alex', 'ale'],
+            ['hello'],
+            ['alx?', '?alex?', '?ale', 'lex']]
+    ans = grep.filter_strings_by_pattern(pattern, data)
+    assert ans == [['alex', 'alex?', '????alex', 'ale', 'le'],
+                   ['alex?', 'ale', 'alex', 'ale'],
+                   [],
+                   ['?alex?', '?ale', 'lex']]
+
+
+def test_unit_filter_strings_by_pattern_second():
+    pattern = 'a+b*a?'
+    pattern = re.compile(pattern)
+    data = [['a', '', 'aa'],
+            ['aaaaba'],
+            [],
+            ['abba', 'aba'],
+            ['aaaaaa', 'baa']]
+    ans = grep.filter_strings_by_pattern(pattern, data)
+    assert ans == [['a', 'aa'],
+                   ['aaaaba'],
+                   [],
+                   ['abba', 'aba'],
+                   ['aaaaaa', 'baa']]
+
+
+def test_unit_filter_strings_by_pattern_third():
+    pattern = 'a?'
+    pattern = re.compile(re.escape(pattern))
+    data = [['a', 'aa'],
+            ['a?', '?aa'],
+            ['a?', 'a?', 'a?'],
+            ['aaaaaaa']]
+    ans = grep.filter_strings_by_pattern(pattern, data)
+    assert ans == [[],
+                   ['a?'],
+                   ['a?', 'a?', 'a?'],
+                   []]
+
+
+def test_unit_count_filtered_strings():
+    data = [['alex', 'alex?', '????alex', 'ale'],
+            ['alex?', 'ale', 'alex', 'ale'],
+            [],
+            ['?alex?', '?ale']]
+    ans = grep.count_filtered_strings(data)
+    assert ans == [['4'], ['4'], ['0'], ['2']]
+
+
+def test_unit_format_output_string():
+    name_file = 'input.txt'
+    line = 'test input.txt'
+    cond = True
+    ans = grep.format_output_string(name_file, line, cond)
+    assert ans == 'input.txt:test input.txt'
+
+
+def test_unit_print_lines(capsys):
+    files = ['a.txt', 'b.txt', 'a.txt']
+    data = [['aa', 'a', 's adf '],
+            ['alex', 's sf '],
+            ['aa', 'a', 's adf ']]
+    cond = True
+    grep.print_lines(files, data, cond)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:aa\na.txt:a\na.txt:s adf \n'\
+                  'b.txt:alex\nb.txt:s sf \na.txt:a'\
+                  'a\na.txt:a\na.txt:s adf \n'
+
+
+def test_unit_strip_lines():
+    lines = ['jsdafkj  pgia\n', 'sdafjha sf', 'alsdjfkdsa\n', '\n', 'n', '\n']
+    ans = grep.strip_lines(lines)
+    assert ans == ['jsdafkj  pgia', 'sdafjha sf', 'alsdjfkdsa', '', 'n', '']
