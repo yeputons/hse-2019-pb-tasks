e021974081d433d02bdecf7bdbe9d8b496309c28
kirill <kirillturov20012013@gmail.com>
2019-12-06 18:52:08 +0300

    Add old grep

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,80 @@
 #!/usr/bin/env python3
 from typing import List
+from typing import Iterable
+from typing import Pattern
+from typing import Tuple
+
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
+def parse_args(args_str: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('needle', type=str)
+    parser.add_argument('pattern', type=str)
     parser.add_argument('files', nargs='*')
+    parser.add_argument('-c', dest='count', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    return parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+def strip_lines(file: Iterable) -> List[str]:
+    return [line.rstrip('\n') for line in file]
+
+
+def get_re_pattern(pattern: str, regex_flag: bool) -> Pattern[str]:
+    if not regex_flag:
+        pattern = re.escape(pattern)
+    return re.compile(pattern)
+
+
+def filter_lines_by_re(lines: List[str], re_pattern: Pattern[str]) -> List[str]:
+    return [line for line in lines if re.search(re_pattern, line)]
+
+
+def filter_blocks(blocks: List[List[str]], re_pattern: Pattern[str]) -> List[List[str]]:
+    return [filter_lines_by_re(lines, re_pattern) for lines in blocks]
+
+
+def map_blocks(blocks: List[List[str]]) -> List[List[str]]:
+    return [[str(len(lines))] for lines in blocks]
+
+
+def add_filename_prefix_to_lines(lines_source_tuple: Tuple[List[str], str]) -> List[str]:
+    lines, source = lines_source_tuple
+    return [f'{source}:{line}' for line in lines]
+
+
+def print_blocks(blocks: list):
+    for lines in blocks:
+        for line in lines:
+            print(line)
+
+
+def main(args_str: List[str]):
+    args = parse_args(args_str)
+
+    blocks = []
+    for file_name in args.files:
+        with open(file_name, 'r') as file:
+            blocks.append(strip_lines(file))
+    if not args.files:
+        blocks.append(strip_lines(sys.stdin))
+
+    sources = args.files
+    if not sources:
+        sources = ['_stdin_']
+
+    re_pattern = get_re_pattern(args.pattern, args.regex)
+    blocks = filter_blocks(blocks, re_pattern)
+
+    if args.count:
+        blocks = map_blocks(blocks)
+
+    if len(sources) > 1:
+        blocks = list(map(add_filename_prefix_to_lines, zip(blocks, sources)))
+
+    print_blocks(blocks)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import io
+import sys
+import re
 import grep
+
+
+# --------------------------------INTEGRATE-----------------------------------------------
+
+# stdin
 
 
 def test_integrate_stdin_grep(monkeypatch, capsys):
@@ -30,6 +37,17 @@ def test_integrate_stdin_grep_count(monkeypatch, capsys):
     assert out == '3\n'
 
 
+def test_integrate_stdin_grep_count_regex(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
+    grep.main(['-E', '-c', 'needle?'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
+# one FILE
+
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
@@ -38,6 +56,35 @@ def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     assert err == ''
     assert out == 'pref needle suf\n'
 
+
+def test_integrate_file_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_file_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle?', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'the needl\npref needle suf\n'
+
+
+def test_integrate_file_grep_count_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle?', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+# many FILES
 
 def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
@@ -57,3 +104,77 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_grep_regex(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle?', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:the needl\nb.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
+def test_integrate_files_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', '-c', 'needle?', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:2\na.txt:2\n'
+
+
+# --------------------------------------------UNIT------------------------------------------
+
+
+def test_unit_parse_args():
+    ans = grep.parse_args(['-E', 'needle?', 'b.txt', 'a.txt'])
+    assert ans.regex is True
+    assert ans.count is False
+    assert ans.pattern == 'needle?'
+    assert ans.files == ['b.txt', 'a.txt']
+
+
+def test_strip_lines_from_file(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('one\ntwo\n')
+    monkeypatch.chdir(tmp_path)
+    with open('a.txt', 'r') as file:
+        assert grep.strip_lines(file) == ['one', 'two']
+
+
+def test_strip_lines_from_stdin(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('one\ntwo\n'))
+    assert grep.strip_lines(sys.stdin) == ['one', 'two']
+
+
+def test_filter_lines_by_re():
+    lines = ['kek', 'lol ke', 'lol']
+    pattern = re.compile('ke?')
+    assert grep.filter_lines_by_re(lines, pattern) == ['kek', 'lol ke']
+
+
+def test_filter_blocks():
+    blocks = [['lol ke', 'lol'], ['kek']]
+    pattern = re.compile('ke?')
+    assert grep.filter_blocks(blocks, pattern) == [['lol ke'], ['kek']]
+
+
+def test_map_blocks():
+    blocks = [['lol ke', 'lol'], ['kek']]
+    assert grep.map_blocks(blocks) == [['2'], ['1']]
+
+
+def test_add_filename_prefix_to_lines():
+    lines = ['boomer', 'or not']
+    source = 'ok,'
+    assert grep.add_filename_prefix_to_lines((lines, source)) == ['ok,:boomer', 'ok,:or not']
+
+
+def test_print_blocks(capsys):
+    blocks = [['a', 'x'], ['b']]
+    grep.print_blocks(blocks)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a\nx\nb\n'
