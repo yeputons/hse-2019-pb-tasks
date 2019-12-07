58b16deb76df79f09e98da027f65f1c69d6aff62
Глеб Соловьев <lionyoung@mail.ru>
2019-12-07 17:07:46 +0300

    Commit task01-grep

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,71 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, Dict
 import sys
 import re
 import argparse
+
+
+def read_files(files: List) -> List:
+    res = []
+    for file in files:
+        with open(file, 'r') as in_file:
+            for line in in_file.readlines():
+                res.append([file, line.rstrip('\n')])
+    return res
+
+
+def read_stdin() -> List:
+    return [['', line.rstrip('\n')] for line in sys.stdin.readlines()]
+
+
+def search(pattern: str, line: str, regex: bool) -> bool:
+    return bool(re.search(pattern, line)) if regex else pattern in line
+
+
+def search_in_lines(pattern: str, input_lines: List[List], regex: bool) -> Dict:
+    found: Dict[str, List] = {}  # {filename or '': List of lines with pattern}
+    for [filename, line] in input_lines:
+        if filename not in found:
+            found[filename] = []
+        if search(pattern, line, regex):
+            found[filename].append(line)
+    return found
+
+
+def print_ans_lines(found: Dict, files: List, answer_format: str) -> None:
+    for filename in files:  # filename = '' as stdin
+        for line in found[filename]:
+            print(answer_format.format(filename, line))
+
+
+def print_ans_count(found: Dict, files: List, answer_format: str) -> None:
+    for filename in files:
+        print(answer_format.format(filename, len(found[filename])))
 
 
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
+    parser.add_argument('-c', dest='count_task', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
     args = parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+    input_lines: List[List] = []  # List of objs: obj = [filename or '' for stdin, line]
+    if args.files:
+        input_lines = read_files(args.files)
+    else:
+        input_lines = read_stdin()
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    # find answer
+    found = search_in_lines(args.needle, input_lines, args.regex)
+
+    # print answer
+    ans_format = '{0}:{1}' if len(args.files) > 1 else '{1}'
+    if args.count_task:  # flag -c
+        print_ans_count(found, args.files if args.files else [''], ans_format)
+    else:  # print lines
+        print_ans_lines(found, args.files if args.files else [''], ans_format)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -3,6 +3,83 @@ import io
 import grep
 
 
+def test_unit_read_files(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('pyt\npyt\n')
+    (tmp_path / 'b.txt').write_text('pytpytpyt')
+    (tmp_path / 'c.txt').write_text('pyt pyt pyt\n pyt')
+    monkeypatch.chdir(tmp_path)
+    saved_lines = [['a.txt', 'pyt'],
+                   ['a.txt', 'pyt'],
+                   ['b.txt', 'pytpytpyt'],
+                   ['c.txt', 'pyt pyt pyt'],
+                   ['c.txt', ' pyt']]
+    assert grep.read_files(['a.txt', 'b.txt', 'c.txt']) == saved_lines
+
+
+def test_unit_read_stdin(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('pyt\n pyt pyt \n \n pyt\n'))
+    assert grep.read_stdin() == [['', 'pyt'], ['', ' pyt pyt '], ['', ' '], ['', ' pyt']]
+
+
+def test_unit_search():
+    assert grep.search('pyt', 'ptpyppytyp', False)
+    assert not grep.search('pyt', 'ptpypptyp', False)
+    assert grep.search('pyt', 'ptp!65/,`dsv>04pyt/-fjsw', False)
+    assert not grep.search('pyt', '', False)
+
+    assert grep.search('[p,a]yt+', 'ayttt', True)
+    assert grep.search('(p{1,3}[Y,t,s][5-7])z+', 'pY6z', True)
+    assert grep.search('(p{1,3}[Y,t,s][5-7])z+', 'ppt5zzzz', True)
+    assert not grep.search('(p{1,3}[Y,t,s][5-7])z+', 'pppptt7zz', True)
+    assert not grep.search('(p{1,3}[Y,t,s][5-7])z+', '(p{1,3}[Y,t,s][5-7])z+', True)
+    assert not grep.search('(p{1,3}[Y,t,s][5-7])z+', '', True)
+
+
+def test_unit_search_in_lines():
+    input_lines = [['a', 'ptpyppytyp'],
+                   ['a', 'ptpypptyp'],
+                   ['a', 'pyt'],
+                   ['', 'pt`dsv>04pyt/-f'],
+                   ['d', ' ']]
+    answer = {'a': ['ptpyppytyp', 'pyt'], '': ['pt`dsv>04pyt/-f'], 'd': []}
+    assert grep.search_in_lines('pyt', input_lines, False) == answer
+
+    input_lines = [['a', 'pY6z'],
+                   ['a', 'ptpypptyp'],
+                   ['c', 'ppt5zzzz'],
+                   ['d', '(p{1,3}[Y,t,s][5-7])z+']]
+    answer = {'a': ['pY6z'], 'c': ['ppt5zzzz'], 'd': []}
+    assert grep.search_in_lines('(p{1,3}[Y,t,s][5-7])z+', input_lines, True) == answer
+
+
+def test_unit_print_ans_lines(capsys):
+    grep.print_ans_lines({'a': ['ptpyppytyp', 'pyt', 'pt`dsv>04pyt/-f']}, ['a'], '{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'ptpyppytyp\npyt\npt`dsv>04pyt/-f\n'
+
+    grep.print_ans_lines({'a.txt': ['ptpyppytyp', 'pyt'],
+                          'b.txt': ['pt`dsv>04pyt/-f'], 'c.txt': []},
+                         ['a.txt', 'b.txt', 'c.txt'], '{0}:{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:ptpyppytyp\na.txt:pyt\nb.txt:pt`dsv>04pyt/-f\n'
+
+
+def test_unit_print_ans_count(capsys):
+    grep.print_ans_count({'': ['ptpyppytyp', 'pyt', 'pt`dsv>04pyt/-f']}, [''], '{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '3\n'
+
+    grep.print_ans_count({'a.txt': ['ptpyppytyp', 'pyt'],
+                          'b.txt': ['pt`dsv>04pyt/-f'], 'c.txt': []},
+                         ['a.txt', 'b.txt', 'c.txt'], '{0}:{1}')
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:2\nb.txt:1\nc.txt:0\n'
+
+
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
@@ -47,6 +124,16 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
+def test_integrate_files_regex_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\nnot needL')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle?', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:the needl\na.txt:pref needle\na.txt:needle suf\n'
 
 
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
