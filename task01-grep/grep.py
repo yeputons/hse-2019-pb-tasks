2aa791060c3fa64cfb739a37068b4915a9068a17
Павел <feloom@DESKTOP-QT22092.localdomain>
2019-12-07 12:02:39 +0300

    grep.py and new tests were add

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,8 +1,72 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+
 from typing import List
+from typing import TextIO
 import sys
 import re
 import argparse
+import os.path
+
+
+def what_to_return(lines: List[str], name_of_file: str, onefile: bool, onlyfiles: bool,
+                   reverseof: bool, count: bool):
+    if onlyfiles or reverseof:
+        if (onlyfiles and len(lines) > 0) or (reverseof and len(lines) == 0):
+            print(name_of_file)
+    else:
+        if count:
+            print_count(len(lines), name_of_file, onefile)
+        else:
+            print_result(lines, name_of_file, onefile)
+
+
+def choosing_type_of_search(regex: bool, full: bool, caseignore: bool,
+                            needle: str, line: str) -> bool:
+    if caseignore and not regex:
+        line = line.lower()
+        needle = needle.lower()
+    if regex:
+        if full:
+            return bool(re.fullmatch(needle, line,
+                                     re.IGNORECASE if caseignore else not re.IGNORECASE))
+        return bool(re.search(needle, line,
+                              re.IGNORECASE if caseignore else not re.IGNORECASE))
+    elif full:
+        return bool(needle == line)
+    else:
+        return bool(needle in line)
+
+
+def print_result(lines: List[str], name_of_file: str, onefile: bool):
+    for line in lines:
+        if onefile:
+            print(line)
+        else:
+            print(name_of_file + ':' + line)       
+
+
+def print_count(count: int, name_of_file: str, onefile: bool):
+    if onefile:
+        print(str(count))
+    else:
+        print(name_of_file + ':' + str(count))
+
+
+def run_all(source: TextIO, regex: bool, count: bool, full: bool,
+            inversion: bool, caseignore: bool, onlyfiles: bool,
+            reverseof: bool, onefile: bool, needle: str, name_of_file: str):
+    correct_lines = []
+    inversion_lines = []
+    for line in source.readlines():
+        line = line.rstrip('\n')
+        if choosing_type_of_search(regex, full, caseignore, needle, line):
+            correct_lines.append(line)
+        else:
+            inversion_lines.append(line)
+    if inversion:
+        what_to_return(inversion_lines, name_of_file, onefile, onlyfiles, reverseof, count)
+    else:
+        what_to_return(correct_lines, name_of_file, onefile, onlyfiles, reverseof, count)
 
 
 def main(args_str: List[str]):
@@ -10,20 +74,32 @@ def main(args_str: List[str]):
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
-
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    parser.add_argument('-c', dest='count', action='store_true')
+    parser.add_argument('-x', dest='full', action='store_true')
+    parser.add_argument('-v', dest='inversion', action='store_true')
+    parser.add_argument('-i', dest='caseignore', action='store_true')
+    parser.add_argument('-l', dest='onlyfiles', action='store_true')
+    parser.add_argument('-L', dest='reverseof', action='store_true')
+    namespace = parser.parse_args(args_str)
+    for file in namespace.files:
+        if not os.path.isfile(file):
+            print(f'File {file} does not exist')
+            namespace.files.remove(file)
+    onefile = False
+    if len(namespace.files) <= 1:
+        onefile = True
+    if not len(namespace.files) == 0:
+        run_all(sys.stdin, namespace.regex, namespace.count, namespace.full,
+                namespace.inversion, namespace.caseignore,
+                namespace.onlyfiles, namespace.reverseof, onefile,
+                namespace.needle, 'sys.stdin')
+    else:
+        for file in namespace.files:
+            with open(file, 'r') as in_file:
+                run_all(in_file, namespace.regex, namespace.count, namespace.full,
+                        namespace.inversion, namespace.caseignore,
+                        namespace.onlyfiles, namespace.reverseof, onefile,
+                        namespace.needle, file)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -30,6 +30,15 @@ def test_integrate_stdin_grep_count(monkeypatch, capsys):
     assert out == '3\n'
 
 
+def test_integrate_stdin_grep_regex_count(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO(
+        'pref needle\nneedle suf\nthe needl\npref needle suf'))
+    grep.main(['-E', '-c', 'needle?'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '4\n'
+
+
 def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
     monkeypatch.chdir(tmp_path)
@@ -37,6 +46,15 @@ def test_integrate_file_grep(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'pref needle suf\n'
+
+
+def test_integrate_file_regex_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle?', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'the needl\npref needle suf\n'
 
 
 def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
@@ -49,6 +67,16 @@ def test_integrate_files_grep(tmp_path, monkeypatch, capsys):
     assert out == 'b.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
 
 
+def test_integrate_files_regex_grep(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', 'needle?', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:the needl\nb.txt:pref needle suf\na.txt:pref needle\na.txt:needle suf\n'
+
+
 def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
     (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
@@ -57,3 +85,41 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_files_grep_regex_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle?', 'b.txt', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'b.txt:2\na.txt:2\n'
+
+
+def test_integrate_file_regex_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-E', '-c', 'needle?', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '2\n'
+
+
+def test_integrate_file_grep_count(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', 'needle', 'a.txt'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_files_grep_regex_count_error(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref needle\nneedle suf\n')
+    (tmp_path / 'b.txt').write_text('the needl\npref needle suf')
+    monkeypatch.chdir(tmp_path)
+    grep.main(['-c', '-E', 'needle?', 'b.txt', 'a.txt', 'keklol'])
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'File keklol does not exist\nb.txt:2\na.txt:2\n'
