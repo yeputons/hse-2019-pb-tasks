a1c3924efaefd11ccd85cff21d948bf95edd0725
Lebedev Egor <lebedev.3gor@gmail.com>
2019-12-07 03:16:38 +0300

    oh no no no no no no

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -4,35 +4,29 @@ from tictactoe import Player, TicTacToe
 
 
 class TicTacToeUserHandler(UserHandler):
-    """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        """Обрабатывает очередное сообщение от пользователя."""
         if message == 'start':
             self.start_game()
         elif not self.game:
             self.send_message('Game is not started')
         else:
-            player, col, row=message.rstrip(' ').split(maxsplit=2)
-            if player == 'X':
-                self.make_turn(Player.X, row=row, col=col)
-            else:
-                self.make_turn(Player.O, row=row, col=col)
-
+            player, col, row = message.rstrip(' ').split(maxsplit=2)
+            player_ = Player.X if player == 'X' else Player.O
+            self.make_turn(player_, row=int(row), col=int(col))
 
     def start_game(self) -> None:
         self.game = TicTacToe()
         self.send_field()
 
-
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        if not self.game.can_make_turn(Player, row=int(row), col=int(col)):
+        if not self.game.can_make_turn(player, row=row, col=col):
             print('Invalid turn')
             return
-        self.game.make_turn(Player, row=row, col=col)
+        self.game.make_turn(player, row=row, col=col)
         self.send_field()
         if self.game.is_finished():
             if not self.game.winner():
@@ -43,17 +37,15 @@ class TicTacToeUserHandler(UserHandler):
                 self.send_message('Game is finished, O wins')
             self.game = None
 
-
-
     def send_field(self) -> None:
         msg = ''
-        for row in range(3):
-            for col in range(3):
-                if self.game.field[row][col] == None:
+        for row in self.game.field:
+            for element in row:
+                if element is None:
                     msg += '.'
-                elif self.game.field[row][col] == Player.X:
+                elif element == Player.X:
                     msg += 'X'
                 else:
                     msg += 'O'
             msg += '\n'
-        self.send_message(msg)
+        self.send_message(msg[:-1])
