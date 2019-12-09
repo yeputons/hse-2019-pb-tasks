880577ca3300502ecdac3e0f152745426e945187
Ekaterina Shelukhina <shelukchina2447@gmail.com>
2019-12-09 04:28:41 +0300

    Task3

--- task03-git/cli_single.py
+++ task03-git/cli_single.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
+import sys
+import traceback
+from alarm_user_handler import AlarmUserHandler
+
+
+def send_message(message: str) -> None:
+    print(f'===== Message =====')
+    print(message)
+    print('==========')
+
+
 def main() -> None:
-    raise NotImplementedError
+    bot = AlarmUserHandler(send_message=send_message)
+    for line in sys.stdin:
+        try:
+            message = line.rstrip('\n')
+            bot.handle_message(message)
+        except Exception:
+        traceback.print_exc()
 
 
 if __name__ == '__main__':
