554d4c36dc1c97db0a7247601661ab7ff1b6a222
Glebanister <glabmar2001@gmail.com>
2019-12-11 16:21:49 +0300

    Implement wait_and_pop

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -3,17 +3,20 @@
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     pthread_mutex_init(&q->mutex, nullptr);
+    pthread_cond_init(&q->cond_empty, nullptr);
     queue_init(&q->q);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
     queue_destroy(&q->q);
     pthread_mutex_destroy(&q->mutex);
+    pthread_cond_destroy(&q->cond_empty);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&q->mutex);
     queue_push(&q->q, data);
+    pthread_cond_signal(&q->cond_empty);
     pthread_mutex_unlock(&q->mutex);
 }
 
@@ -29,7 +32,11 @@ bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    // TODO(2)
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    return nullptr;
+    pthread_mutex_lock(&q->mutex);
+    while (queue_empty(&q->q)) {
+        pthread_cond_wait(&q->cond_empty, &q->mutex);
+    }
+    void *data = queue_pop(&q->q);
+    pthread_mutex_unlock(&q->mutex);
+    return data;
 }
