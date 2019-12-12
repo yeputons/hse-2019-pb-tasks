0ed3868272cc61657791142ebedae9598146911f
Kate <ekaterinaicenko@gmail.com>
2019-12-12 19:21:49 +0300

    Add edited 'cli_single.py'

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import traceback
-from alarm_user_handler import AlarmUserHandler
+from tictactoe_user_handler import TicTacToeUserHandler
 
 
 def send_message(message: str) -> None:
@@ -10,7 +10,7 @@ def send_message(message: str) -> None:
 
 
 def main() -> None:
-    bot = AlarmUserHandler(send_message=send_message)
+    bot = TicTacToeUserHandler(send_message=send_message)
     for line in sys.stdin:
         try:
             message = line.rstrip('\n')
