c53e222c8cb2af73d216122d68c71760a212545f
Jane <grindevald666@gmail.com>
2019-12-12 16:34:30 +0300

    Fix 'tsqueue.cpp'

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,37 +1,37 @@
 #include "tsqueue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-  queue_init(&q->q);
-  pthread_mutex_init(&q->mutex, nullptr);
-  return;
+    queue_init(&q->q);
+    pthread_mutex_init(&q->mutex, nullptr);
+    return;
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-  queue_destroy(&q->q);
-  pthread_mutex_destroy(&q->mutex);
-  return;
+    pthread_mutex_destroy(&q->mutex);
+    queue_destroy(&q->q);
+    return;
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-  pthread_mutex_lock(&q->mutex);
-  queue_push(&q->q, data);
-  pthread_mutex_unlock(&q->mutex);
-  return;
+    pthread_mutex_lock(&q->mutex);
+    queue_push(&q->q, data);
+    pthread_mutex_unlock(&q->mutex);
+    return;
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-  pthread_mutex_lock(&q->mutex);
-  if (!queue_empty(&q->q)) {
-    *data = queue_pop(&q->q);
+    pthread_mutex_lock(&q->mutex);
+    if (!queue_empty(&q->q)) {
+        *data = queue_pop(&q->q);
+        pthread_mutex_unlock(&q->mutex);
+        return true;
+    }
     pthread_mutex_unlock(&q->mutex);
-    return true;
-  }
-  pthread_mutex_unlock(&q->mutex);
-  return false;
+    return false;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-  // TODO(2)
-  static_cast<void>(q); // Как-нибудь используем переменную.
-  return nullptr;
+    // TODO(2)
+    static_cast<void>(q);  // Как-нибудь используем переменную.
+    return nullptr;
 }
