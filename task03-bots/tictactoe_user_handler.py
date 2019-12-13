37cbf8f3bb843445acf8c8c953a0845d0bea5fea
hwanglight <stray.polina.study@gmail.com>
2019-12-13 17:56:56 +0300

    Fixed comments

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -5,36 +5,36 @@ from tictactoe import Player, TicTacToe
 
 class TicTacToeUserHandler(UserHandler):
     """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
+
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
         """Обрабатывает очередное сообщение от пользователя."""
-        if self.game is not None:
-            datamess = message.rstrip('\n').split(' ')
-            if len(datamess) != 3:
-                self.send_message('Invalid turn')
-            else:
-                if datamess[0] == 'X':
-                    player = Player.X
-                elif datamess[0] == 'O':
-                    player = Player.O
-                else:
-                    self.send_message('There is no such player')
-                    return
-                try:
-                    row = int(datamess[1])
-                    col = int(datamess[2])
-                except ValueError:
-                    self.send_message('Please send correct turn: Player row col')
-                    return
-                self.make_turn(player=player, row=row, col=col)
-        else:
+        if self.game is None:
             if message != 'start':
                 self.send_message('Game is not started')
+                return
             else:
                 self.start_game()
+                return
+        data_message = message.rstrip('\n').split(' ')
+        if len(data_message) != 3:
+            self.send_message('Invalid turn')
+        else:
+            try:
+                player = Player[data_message[0]]
+            except KeyError:
+                self.send_message('There is no such player')
+                return
+            try:
+                row = int(data_message[1])
+                col = int(data_message[2])
+            except ValueError:
+                self.send_message('Please send correct turn: Player row col')
+                return
+            self.make_turn(player=player, row=row, col=col)
 
     def start_game(self) -> None:
         """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
@@ -43,30 +43,33 @@ class TicTacToeUserHandler(UserHandler):
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
         """Обрабатывает ход игрока player в клетку (row, col)."""
-        if self.game is not None:
-            if self.game.can_make_turn(player, row=row, col=col):
-                self.game.make_turn(player, row=row, col=col)
-                self.send_field()
-                if self.game.is_finished():
-                    if self.game.winner() is None:
-                        self.send_message('Game is finished, draw')
-                        self.game = None
-                    else:
-                        self.send_message(f'Game is finished, {str(self.game.winner())[7]} wins')
-                        self.game = None
+        if self.game is None:
+            self.send_message('Invalid turn')
+            return
+        if not self.game.can_make_turn(player, row=row, col=col):
+            self.send_message('Invalid turn')
+            return
+        self.game.make_turn(player, row=row, col=col)
+        self.send_field()
+        if self.game.is_finished():
+            winner = self.game.winner()
+            if not winner:
+                self.send_message('Game is finished, draw')
             else:
-                self.send_message('Invalid turn')
+                self.send_message(f'Game is finished, {winner.name} wins')
+            self.game = None
 
     def send_field(self) -> None:
         """Отправляет пользователю сообщение с текущим состоянием игры."""
-        if self.game is not None:
-            lineres = ''
-            for line in self.game.field:
-                for el in range(0, 3):
-                    if line[el] is not None:
-                        lineres += (str(line[el]).split('.'))[1]
-                    else:
-                        lineres += '.'
-                lineres += '\n'
-            lineres = lineres.rstrip('\n')
-            self.send_message(lineres)
+        if self.game is None:
+            return
+        resline = ''
+        for line in self.game.field:
+            for el in line:
+                if el is not None:
+                    resline += el.name
+                else:
+                    resline += '.'
+            resline += '\n'
+        resline = resline.rstrip('\n')
+        self.send_message(resline)
