dc9fe5404155913c7f4b12529f36a437f0ba6012
KirillBrilliantov <ki.br178@gmail.com>
2019-12-13 16:33:41 +0300

    Make part 3

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+
+import sys
+import traceback
+from tictactoe_user_handler import TicTacToeUserHandler
+
+
 def main() -> None:
-    raise NotImplementedError
+    bot = TicTacToeUserHandler(send_message=print)
+    for line in sys.stdin:
+        try:
+            message = line.rstrip('\n')
+            bot.handle_message(message)
+        except ValueError:
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
