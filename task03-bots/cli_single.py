e81f412448f21012a380e588c7c059b9ef239ece
Sasha Vorobeva <avorobdm@gmail.com>
2019-12-13 21:32:38 +0300

    4th part of bots task

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 import sys
 import traceback
-from alarm_user_handler import AlarmUserHandler
+from tictactoe_user_handler import TicTacToeUserHandler
 
 
 def main() -> None:
-    bot = AlarmUserHandler(send_message=print)
+    bot = TicTacToeUserHandler(send_message=print)
     for line in sys.stdin:
         try:
-            message = line.rstrip('\n').split(maxsplit=1)
-            bot.handle_message(message[0])
+            message = line.rstrip('\n')
+            bot.handle_message(message)
         except Exception:  # pylint: disable=W0703
             traceback.print_exc()
 
--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -1,26 +1,57 @@
-from typing import Callable, Optional
+from typing import Callable, Optional, Dict
 from bot import UserHandler
 from tictactoe import Player, TicTacToe
 
 
 class TicTacToeUserHandler(UserHandler):
     """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
+
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
         """Обрабатывает очередное сообщение от пользователя."""
-        raise NotImplementedError
+        try:
+            if message == 'start':
+                self.start_game()
+                return
+            if self.game is None:
+                self.send_message('Game is not started')
+                return
+            players = {'X': Player.X, 'O': Player.O}
+            player_sign, column, row = message.split()
+            self.make_turn(players[player_sign], col=int(column), row=int(row))
+        except Exception:  # pylint: disable=W0703
+            self.send_message('Invalid turn')
 
     def start_game(self) -> None:
         """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
         """Обрабатывает ход игрока player в клетку (row, col)."""
-        raise NotImplementedError
+        assert self.game
+        if self.game.can_make_turn(player, row=row, col=col):
+            self.game.make_turn(player, row=row, col=col)
+            self.send_field()
+            if self.game.is_finished():
+                winner = self.game.winner()
+                if winner is None:
+                    self.send_message('Game is finished, draw')
+                else:
+                    self.send_message(f'Game is finished, {winner} wins')
+        else:
+            self.send_message('Invalid turn')
 
     def send_field(self) -> None:
         """Отправляет пользователю сообщение с текущим состоянием игры."""
-        raise NotImplementedError
+        assert self.game
+        player_sgn: Dict[Optional[Player], str] = {None: '.', Player.X: 'X', Player.O: 'O'}
+        stringfield = ''
+        for i in range(3):
+            for j in range(3):
+                stringfield += player_sgn[self.game.field[i][j]]
+            stringfield += '\n'
+        self.send_message(stringfield[:-1])
