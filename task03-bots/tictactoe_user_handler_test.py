084438f212516de2ab2bdfb285b41b7ae24c3a1f
KirillBrilliantov <ki.br178@gmail.com>
2019-12-13 17:27:19 +0300

    Add tests

--- task03-bots/tictactoe_user_handler_test.py
+++ task03-bots/tictactoe_user_handler_test.py
@@ -10,7 +10,7 @@ def test_tictactoe_user_hander_correct_work(mocker: pytest_mock.MockFixture
     for i in range(2):
         bot.handle_message('X ' + str(i) + ' 0')
         bot.handle_message('O ' + str(i) + ' 1')
-        
+
     bot.handle_message('X 2 0')
     bot.handle_message(TicTacToeUserHandler.START_COMMAND)
     assert send_message.call_args_list == [
@@ -23,7 +23,7 @@ def test_tictactoe_user_hander_correct_work(mocker: pytest_mock.MockFixture
         mocker.call('Game is finished, X wins'),
         mocker.call('...\n...\n...')
     ]
-    
+
 
 def test_ticktactoe_user_hadler_incorrect_work(mocker: pytest_mock.MockFixture
                                                ) -> None:
@@ -78,4 +78,27 @@ def test_ticktactoe_user_hadler_draw(mocker: pytest_mock.MockFixture) -> None:
         mocker.call('X.O\nOXX\nXOO'),
         mocker.call('XXO\nOXX\nXOO'),
         mocker.call(TicTacToeUserHandler.GAME_FINISHED + ' draw')
+    ]
+
+
+def test_ticktactoe_user_hadler_O_wins(mocker: pytest_mock.MockFixture
+                                       ) -> None:
+    send_message = mocker.stub(name='send_message_stub')
+    bot = TicTacToeUserHandler(send_message)
+    bot.handle_message(TicTacToeUserHandler.START_COMMAND)
+    bot.handle_message('X 0 0')
+    bot.handle_message('O 1 1')
+    bot.handle_message('X 2 0')
+    bot.handle_message('O 1 0')
+    bot.handle_message('X 0 1')
+    bot.handle_message('O 1 2')
+    assert send_message.call_args_list == [
+        mocker.call('...\n...\n...'),
+        mocker.call('X..\n...\n...'),
+        mocker.call('X..\n.O.\n...'),
+        mocker.call('X.X\n.O.\n...'),
+        mocker.call('XOX\n.O.\n...'),
+        mocker.call('XOX\nXO.\n...'),
+        mocker.call('XOX\nXO.\n.O.'),
+        mocker.call(TicTacToeUserHandler.GAME_FINISHED + ' O wins')
     ]
