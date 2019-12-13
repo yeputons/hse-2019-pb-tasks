ed781afaa8304084ea1ce8edbc0e26c057a7509c
Tarasov Denis <dt6cgsg@gmail.com>
2019-12-13 17:48:04 +0300

    Correct thread-safe queue code

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -2,44 +2,44 @@
 #include <assert.h>
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    queue_init(&(q->q));
-    assert(pthread_mutex_init(&(q->mutex), NULL) == 0);
-    assert(pthread_cond_init(&(q->cond), NULL) == 0);
+    queue_init(&q->q);
+    assert(pthread_mutex_init(&(q->mutex), nullptr) == 0);
+    assert(pthread_cond_init(&(q->cond), nullptr) == 0);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    queue_destroy(&(q->q));
-    pthread_mutex_destroy(&(q->mutex));
-    pthread_cond_destroy(&(q->cond));
+    pthread_cond_destroy(&q->cond);
+    pthread_mutex_destroy(&q->mutex);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    pthread_mutex_lock(&(q->mutex));
-    queue_push(&(q->q), data);
-    pthread_cond_signal(&(q->cond));
-    pthread_mutex_unlock(&(q->mutex));
+    pthread_mutex_lock(&q->mutex);
+    queue_push(&q->q, data);
+    pthread_cond_signal(&q->cond);
+    pthread_mutex_unlock(&q->mutex);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    pthread_mutex_lock(&(q->mutex));
-    bool rv = !queue_empty(&(q->q));
+    pthread_mutex_lock(&q->mutex);
+    bool is_empty = queue_empty(&q->q);
 
-    if (rv)
-        *data = queue_pop(&(q->q));
-    pthread_mutex_unlock(&(q->mutex));
+    if (!is_empty)
+        *data = queue_pop(&q->q);
+    pthread_mutex_unlock(&q->mutex);
 
-    return rv;
+    return !is_empty;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    pthread_mutex_lock(&(q->mutex));
+    pthread_mutex_lock(&q->mutex);
 
-    while (queue_empty(&(q->q)))
-        pthread_cond_wait(&(q->cond), &(q->mutex));
+    while (queue_empty(&q->q))
+        pthread_cond_wait(&q->cond, &q->mutex);
 
-    void *data = queue_pop(&(q->q));
+    void *data = queue_pop(&q->q);
 
-    pthread_mutex_unlock(&(q->mutex));
+    pthread_mutex_unlock(&q->mutex);
 
     return data;
 }
