63676f1b2b7886118e541cab5fe29dc73182ae16
sarnatk <tanya200217@gmail.com>
2019-12-13 20:10:29 +0300

    Again

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -3,11 +3,11 @@
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     queue_init(&q->q);
     pthread_mutex_init(&q->mutex, nullptr);
-    //pthread_cond_init(&q->cond_queue, nullptr);
+    pthread_cond_init(&q->cond_queue, nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    //pthread_cond_destroy(&q->cond_queue, nullptr);
+    pthread_cond_destroy(&q->cond_queue);
     pthread_mutex_destroy(&q->mutex);
     queue_destroy(&q->q);
 }
@@ -15,6 +15,7 @@ void threadsafe_queue_destroy(ThreadsafeQueue *q) {
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&q->mutex);
     queue_push(&q->q, data);
+    pthread_cond_signal(&q->cond_queue);
     pthread_mutex_unlock(&q->mutex);
 }
 
@@ -27,11 +28,16 @@ bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
     *data = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex);
     return true;
-    
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    // TODO(2)
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    return nullptr;
+    void *data;
+    pthread_mutex_lock(&q->mutex);
+    while (queue_empty(&q->q)) {
+        pthread_cond_wait(&q->cond_queue, &q->mutex);
+    }
+    data = queue_pop(&q->q);
+    pthread_mutex_unlock(&q->mutex);
+
+    return data;
 }
