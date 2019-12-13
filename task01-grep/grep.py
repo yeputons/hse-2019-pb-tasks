30aa1bd3d0d791ff1fe19075d186a0fc611f114d
shirokov-kirill <k.s.shirokov@mail.ru>
2019-12-13 13:19:42 +0300

    Fourth iteration
    Without unit-tests

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -24,35 +24,35 @@ def pattern_format(pattern: str, is_regex: bool, full_matches: bool) -> str:
 
 def check_if_line_in_answer(line: str, pattern: str, is_ignore_case: bool, invert_result: bool) -> bool:
     result = False
-    if is_ignore_case and \
-            re.search(re.compile(pattern.lower()), line.lower()) or \
-            not is_ignore_case and \
-            re.search(re.compile(pattern), line):
+    if is_ignore_case and re.search(re.compile(pattern.lower()), line.lower()):
+        result = True
+    if not is_ignore_case and re.search(re.compile(pattern), line):
         result = True
     if invert_result:
         return not result
     return result
 
 
-def working_with_text_flags(lines: List[str], pattern: str, is_ignore_case: bool, invert_result: bool) -> List[str]:
-    lines = [line.rstrip('\n') for line in lines]
-    new_list_of_lines = [line for line in lines if
-                         check_if_line_in_answer(line, pattern, is_ignore_case, invert_result)]
+def format_strings_with_text_flags(lines: List[str], pattern: str, is_ignore_case: bool,
+                                   invert_result: bool) -> List[str]:
+    new_list_of_lines = [line for line in lines
+                         if check_if_line_in_answer(line, pattern, is_ignore_case, invert_result)]
     return new_list_of_lines
 
 
-def working_with_print_flags(lines: List[str], source: str,
-                             number_of_files: bool, flags: dict) -> List[str]:
-    if flags.get('count_mode'):
+def format_lines_with_print_flags(lines: List[str], source: str,
+                                  number_of_files: bool, count_mode: bool,
+                                  filenames_matches_exist: bool, filenames_matches_not_exist: bool) -> List[str]:
+    if count_mode:
         lines = [str(len(lines))]
     if number_of_files:
         lines = [f'{source}:{line}' for line in lines]
-    if flags.get('filenames_matches_exist'):
+    if filenames_matches_exist:
         if lines:
             lines = [source]
         else:
             lines = []
-    if flags.get('filenames_matches_not_exist'):
+    if filenames_matches_not_exist:
         if lines:
             lines = []
         else:
@@ -77,17 +77,18 @@ def main(args_str: List[str]) -> None:
     parser.add_argument('-l', dest='filenames_exist', action='store_true')
     parser.add_argument('-L', dest='filenames_not_exist', action='store_true')
     args = parser.parse_args(args_str)
-    print_flags = {'count_mode': args.count, 'filenames_matches_exist': args.filenames_exist,
-                   'filenames_matches_not_exist': args.filenames_not_exist}
-    if len(args.files) > 0:
+
+    if args.files:
         list_of_lines = [read_file(filename) for filename in args.files]
     else:
         list_of_lines = [read_stdin()]
     args.files.append('')
     pattern = pattern_format(args.pattern, args.regex, args.full_matches)
     for i, lines in enumerate(list_of_lines):
-        lines = working_with_text_flags(lines, pattern, args.ignore, args.invert)
-        lines = working_with_print_flags(lines, args.files[i], len(args.files) > 2, print_flags)
+        lines = [line.rstrip('\n') for line in lines]
+        lines = format_strings_with_text_flags(lines, pattern, args.ignore, args.invert)
+        lines = format_lines_with_print_flags(lines, args.files[i], len(args.files) > 2, args.count,
+                                              args.filenames_exist, args.filenames_not_exist)
         printing(lines)
 
 
