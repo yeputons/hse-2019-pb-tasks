61760a7e42853f369bd11a742e3d78fc0f739a15
Kirill Karnaukhov <bitikit@gmail.com>
2019-12-13 19:02:57 +0300

    Fix style and potentional UB

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -2,13 +2,13 @@
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     queue_init(&q->q);
-    pthread_cond_init(&(q->cond), nullptr);
-    pthread_mutex_init(&(q->mutex), nullptr);
+    pthread_mutex_init(&q->mutex, nullptr);
+    pthread_cond_init(&q->cond, nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_mutex_destroy(&q->mutex);
     pthread_cond_destroy(&q->cond);
+    pthread_mutex_destroy(&q->mutex);
     queue_destroy(&q->q);
 }
 
@@ -20,17 +20,16 @@ void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    if (queue_empty(&q->q)) {
-        return false;
-    }
-    pthread_mutex_lock(&q->mutex);
-    if (queue_empty(&q->q)) {
+    bool is_empty = queue_empty(&q->q);
+    if (!is_empty) {
+        pthread_mutex_lock(&q->mutex);
+        is_empty = queue_empty(&q->q);
+        if (!is_empty) {
+            *data = queue_pop(&q->q);
+        }
         pthread_mutex_unlock(&q->mutex);
-        return false;
     }
-    *data = queue_pop(&q->q);
-    pthread_mutex_unlock(&q->mutex);
-    return true;
+    return !is_empty;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
