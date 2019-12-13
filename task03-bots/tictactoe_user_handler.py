318ee697bb683f2680bcd988a4ab5a67a0ce72dd
KirillBrilliantov <ki.br178@gmail.com>
2019-12-13 17:20:32 +0300

    Add part 4

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -4,23 +4,61 @@ from tictactoe import Player, TicTacToe
 
 
 class TicTacToeUserHandler(UserHandler):
-    """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
+    START_COMMAND = 'start'
+    GAME_IS_NOT_STARTED = 'Game is not started'
+    INVALID_TURN = 'Invalid turn'
+    GAME_FINISHED = 'Game is finished,'
+
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        """Обрабатывает очередное сообщение от пользователя."""
-        raise NotImplementedError
+        if message == self.START_COMMAND:
+            self.start_game()
+            return
+        if self.game is None:
+            self.send_message(self.GAME_IS_NOT_STARTED)
+            return
+        try:
+            player, col, row = message.split(maxsplit=2)
+            self.make_turn(Player[player], row=int(row), col=int(col))
+        except ValueError:
+            self.send_message(self.INVALID_TURN)
 
     def start_game(self) -> None:
-        """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        """Обрабатывает ход игрока player в клетку (row, col)."""
-        raise NotImplementedError
+        assert self.game is not None
+        if not self.game.can_make_turn(player, row=row, col=col):
+            self.send_message(self.INVALID_TURN)
+            return
+
+        self.game.make_turn(player, row=row, col=col)
+        self.send_field()
+        if self.game.is_finished():
+            winner: Optional[Player] = self.game.winner()
+            if winner is None:
+                self.send_message(f'{self.GAME_FINISHED} draw')
+                return
+            self.send_message(f'{self.GAME_FINISHED} {winner.name} wins')
+            self.game = None
 
     def send_field(self) -> None:
-        """Отправляет пользователю сообщение с текущим состоянием игры."""
-        raise NotImplementedError
+        assert self.game is not None
+
+        def get_char(player: Optional[Player]) -> str:
+            if player is None:
+                return '.'
+            if player is Player.X:
+                return 'X'
+            return 'O'
+
+        message = ''
+        for row in range(0, 3):
+            for col in range(0, 3):
+                message += get_char(self.game.field[row][col])
+            message += '\n'
+        self.send_message(message[:-1])
---
+++ task03-bots/tictactoe_user_handler_test.py
@@ -0,0 +1,81 @@
+import pytest_mock
+from tictactoe_user_handler import TicTacToeUserHandler
+
+
+def test_tictactoe_user_hander_correct_work(mocker: pytest_mock.MockFixture
+                                            ) -> None:
+    send_message = mocker.stub(name='send_message_stub')
+    bot = TicTacToeUserHandler(send_message)
+    bot.handle_message('start')
+    for i in range(2):
+        bot.handle_message('X ' + str(i) + ' 0')
+        bot.handle_message('O ' + str(i) + ' 1')
+        
+    bot.handle_message('X 2 0')
+    bot.handle_message(TicTacToeUserHandler.START_COMMAND)
+    assert send_message.call_args_list == [
+        mocker.call('...\n...\n...'),
+        mocker.call('X..\n...\n...'),
+        mocker.call('X..\nO..\n...'),
+        mocker.call('XX.\nO..\n...'),
+        mocker.call('XX.\nOO.\n...'),
+        mocker.call('XXX\nOO.\n...'),
+        mocker.call('Game is finished, X wins'),
+        mocker.call('...\n...\n...')
+    ]
+    
+
+def test_ticktactoe_user_hadler_incorrect_work(mocker: pytest_mock.MockFixture
+                                               ) -> None:
+    send_message = mocker.stub(name='send_message_stub')
+    bot = TicTacToeUserHandler(send_message)
+    bot.handle_message('aavbb')
+    bot.handle_message('X 0 1')
+    bot.handle_message(TicTacToeUserHandler.START_COMMAND)
+    bot.handle_message('X 0 0')
+    bot.handle_message('O 2 2')
+    bot.handle_message('O 1 1')
+    bot.handle_message('O 0 0')
+    bot.handle_message('X 2 2')
+    bot.handle_message('X 0 0')
+    bot.handle_message('X 1 1')
+    assert send_message.call_args_list == [
+        mocker.call(TicTacToeUserHandler.GAME_IS_NOT_STARTED),
+        mocker.call(TicTacToeUserHandler.GAME_IS_NOT_STARTED),
+        mocker.call('...\n...\n...'),
+        mocker.call('X..\n...\n...'),
+        mocker.call('X..\n...\n..O'),
+        mocker.call(TicTacToeUserHandler.INVALID_TURN),
+        mocker.call(TicTacToeUserHandler.INVALID_TURN),
+        mocker.call(TicTacToeUserHandler.INVALID_TURN),
+        mocker.call(TicTacToeUserHandler.INVALID_TURN),
+        mocker.call('X..\n.X.\n..O'),
+    ]
+
+
+def test_ticktactoe_user_hadler_draw(mocker: pytest_mock.MockFixture) -> None:
+    send_message = mocker.stub(name='send_message_stub')
+    bot = TicTacToeUserHandler(send_message)
+    bot.handle_message(TicTacToeUserHandler.START_COMMAND)
+    bot.handle_message('X 1 1')
+    bot.handle_message('O 2 0')
+    bot.handle_message('X 0 0')
+    bot.handle_message('O 2 2')
+    bot.handle_message('X 2 1')
+    bot.handle_message('O 1 2')
+    bot.handle_message('X 0 2')
+    bot.handle_message('O 0 1')
+    bot.handle_message('X 1 0')
+    assert send_message.call_args_list == [
+        mocker.call('...\n...\n...'),
+        mocker.call('...\n.X.\n...'),
+        mocker.call('..O\n.X.\n...'),
+        mocker.call('X.O\n.X.\n...'),
+        mocker.call('X.O\n.X.\n..O'),
+        mocker.call('X.O\n.XX\n..O'),
+        mocker.call('X.O\n.XX\n.OO'),
+        mocker.call('X.O\n.XX\nXOO'),
+        mocker.call('X.O\nOXX\nXOO'),
+        mocker.call('XXO\nOXX\nXOO'),
+        mocker.call(TicTacToeUserHandler.GAME_FINISHED + ' draw')
+    ]
