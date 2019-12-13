2d39e403091fb0cd27c146b048b28a92c4a09458
Kira5005-code <anisimova5005@gmail.com>
2019-12-13 20:45:43 +0300

    Fix bugs in tictactoe_user_handler.py

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -15,7 +15,7 @@ class TicTacToeUserHandler(UserHandler):
         elif not self.game:
             self.send_message('Game is not started')
         else:
-            player, row, col = message.rstrip('\n').split(maxsplit=2)
+            player, row, col = message.rstrip(' ').split(maxsplit=2)
             now_player = Player.X if player == 'X' else Player.O
             self.make_turn(now_player, row=int(row), col=int(col))
 
@@ -25,33 +25,29 @@ class TicTacToeUserHandler(UserHandler):
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
         assert self.game
-        if self.game.can_make_turn(player, row=row, col=col):
-            self.game.make_turn(player, row=row, col=col)
-        else:
+        if not self.game.can_make_turn(player, row=row, col=col):
             self.send_message('Invalid turn')
             return
+        self.game.make_turn(player, row=row, col=col)
         self.send_field()
         if self.game.is_finished():
             if self.game.winner():
-                message = 'Game is finished, '
                 win = self.game.winner()
-                message += 'X wins' if win == Player.X else 'O wins'
-                self.send_message(message)
+                result = 'Game is finished, X wins' if win == Player.X \
+                    else 'Game is finished, O wins'
+                self.send_message(result)
             else:
                 self.send_message('Game is finished, draw')
             self.game = None
 
     def send_field(self) -> None:
         assert self.game
-        message = ''
-        for i in self.game.field:
-            for j in i:
-                if j:
-                    message += j.name
-                elif j == 2:
-                    message += 'O'
+        playing_field = ''
+        for row in range(3):
+            for col in range(3):
+                if not self.game.field[row][col]:
+                    playing_field += '.'
                 else:
-                    message += '.'
-            if len(message) < 8:
-                message += '\n'
-        self.send_message(message)
+                    playing_field += 'X' if self.game.field[row][col] == Player.X else 'O'
+            playing_field += '\n'
+        self.send_message(playing_field[:11])
