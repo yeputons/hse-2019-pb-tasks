fa30fd7a47994d3e2e85cd78f071acb74fac96cd
Валерий Головин <v.s.g.clev@gmail.com>
2019-12-13 23:06:14 +0300

    Task 1,2 without tests

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,30 +1,41 @@
 #include "tsqueue.h"
+#include "queue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    pthread_mutex_init(&q->mutex, nullptr);
+    pthread_cond_init(&q->not_empty_cond, nullptr);
+    queue_init(&q->q);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    pthread_cond_destroy(&q->not_empty_cond);
+    pthread_mutex_destroy(&q->mutex);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    static_cast<void>(data);  // Как-нибудь используем переменную.
+    pthread_mutex_lock(&q->mutex);
+    queue_push(&q->q, data);
+    pthread_cond_signal(&q->not_empty_cond);
+    pthread_mutex_unlock(&q->mutex);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    // TODO
-    static_cast<void>(q);
-    static_cast<void>(data);
-    return false;
+    pthread_mutex_lock(&q->mutex);
+    bool empty = queue_empty(&q->q);
+    if (!empty) {
+        *data = queue_pop(&q->q);
+    }
+    pthread_mutex_unlock(&q->mutex);
+    return !empty;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    // TODO(2)
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    return nullptr;
+    pthread_mutex_lock(&q->mutex);
+    while (queue_empty(&q->q)) {
+        pthread_cond_wait(&q->not_empty_cond, &q->mutex);
+    }
+    void *return_data = queue_pop(&q->q);
+    pthread_mutex_unlock(&q->mutex);
+    return return_data;
 }
