5a2e8d62085dac6a3bc4962a33359113f1395d46
German <tarabonda@bk.ru>
2019-12-13 09:08:32 +0300

    Delete brackets

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,40 +1,40 @@
 #include "tsqueue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    queue_init(&(q->q));
-    pthread_mutex_init(&(q->mutex), nullptr);
-    pthread_cond_init(&(q->cond), nullptr);
+    queue_init(&q->q);
+    pthread_mutex_init(&q->mutex, nullptr);
+    pthread_cond_init(&q->cond, nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_cond_destroy(&(q->cond));
-    pthread_mutex_destroy(&(q->mutex));
-    queue_destroy(&(q->q));
+    pthread_cond_destroy(&q->cond);
+    pthread_mutex_destroy(&q->mutex);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    pthread_mutex_lock(&(q->mutex));
-    queue_push(&(q->q), data);
-    pthread_cond_signal(&(q->cond));
-    pthread_mutex_unlock(&(q->mutex));
+    pthread_mutex_lock(&q->mutex);
+    queue_push(&q->q, data);
+    pthread_cond_signal(&q->cond);
+    pthread_mutex_unlock(&q->mutex);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    pthread_mutex_lock(&(q->mutex));
-    if (queue_empty(&(q->q))) {
-        pthread_mutex_unlock(&(q->mutex));
+    pthread_mutex_lock(&q->mutex);
+    if (queue_empty(&q->q)) {
+        pthread_mutex_unlock(&q->mutex);
         return false;
     }
-    *data = queue_pop(&(q->q));
-    pthread_mutex_unlock(&(q->mutex));
+    *data = queue_pop(&q->q);
+    pthread_mutex_unlock(&q->mutex);
     return true;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    pthread_mutex_lock(&(q->mutex));
-    while (queue_empty(&(q->q)))
-        pthread_cond_wait(&(q->cond), &(q->mutex));
-    void *data = queue_pop(&(q->q));
-    pthread_mutex_unlock(&(q->mutex));
+    pthread_mutex_lock(&q->mutex);
+    while (queue_empty(&q->q))
+        pthread_cond_wait(&q->cond, &q->mutex);
+    void *data = queue_pop(&q->q);
+    pthread_mutex_unlock(&q->mutex);
     return data;
 }
