82204d2d485246a3dd3082619836cbc24d6bbe9a
KirillBrilliantov <ki.br178@gmail.com>
2019-12-13 20:13:16 +0300

    Fix implementation of tsqueue.cpp

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -2,25 +2,30 @@
 #include <cassert>
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
+    assert(q);
     queue_init(&q->q);
     pthread_mutex_init(&q->mtx, nullptr);
     pthread_cond_init(&q->cnd, nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
+    assert(q);
     assert(queue_empty(&q->q));
     queue_destroy(&q->q);
-    pthread_mutex_destroy(&q->mtx);
     pthread_cond_destroy(&q->cnd);
+    pthread_mutex_destroy(&q->mtx);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
+    assert(q);
     pthread_mutex_lock(&q->mtx);
     queue_push(&q->q, data);
+    pthread_cond_signal(&q->cnd);
     pthread_mutex_unlock(&q->mtx);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
+    assert(q);
     if (queue_empty(&q->q))
         return false;
     pthread_mutex_lock(&q->mtx);
@@ -30,6 +35,7 @@ bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
+    assert(q);
     void *ans = 0;
     pthread_mutex_lock(&q->mtx);
     while (queue_empty(&q->q)) {
