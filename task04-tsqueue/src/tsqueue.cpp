a447eeb0257ccb450bef0e2329807f314d63d034
German <tarabonda@bk.ru>
2019-12-13 09:01:08 +0300

    Fix style

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -21,7 +21,7 @@ void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
     pthread_mutex_lock(&(q->mutex));
-    if (queue_empty(&(q->q))){
+    if (queue_empty(&(q->q))) {
         pthread_mutex_unlock(&(q->mutex));
         return false;
     }
