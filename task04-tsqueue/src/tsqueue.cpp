f6ffc54e9bb48aece9313e51e6b4edfd5345bd3b
NikitaAbramov <nik_abramov22@mail.ru>
2019-12-13 13:38:41 +0300

    add ThreadsafeQueue

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,26 +1,30 @@
 #include "tsqueue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    pthread_mutex_init(&(q->m), NULL);
+    queue_init(&q->q);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
+    pthread_mutex_destroy(&(q->m)); 
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    // TODO
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    static_cast<void>(data);  // Как-нибудь используем переменную.
+    pthread_mutex_lock(&(q->m));
+    queue_push(&q->q, data);
+    pthread_mutex_unlock(&(q->m));
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    // TODO
-    static_cast<void>(q);
-    static_cast<void>(data);
-    return false;
+    pthread_mutex_lock(&(q->m));
+    bool cond = false;
+    if (!queue_empty(&q->q)) {
+        *data = queue_pop(&q->q);
+        cond = true;
+    }
+    pthread_mutex_unlock(&(q->m));
+    return cond;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
