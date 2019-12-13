4b92106f2e39f750f87e9993fd6cc1190c2ea1ce
gt22 <igorengel@mail.ru>
2019-12-13 10:04:16 +0300

    Normalize formatting

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -22,7 +22,9 @@ void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
     bool ret;
     pthread_mutex_lock(&q->mutex);
-    if ((ret = !queue_empty(&q->q))) *data = queue_pop(&q->q);
+    if ((ret = !queue_empty(&q->q))) {
+        *data = queue_pop(&q->q);
+    }
     pthread_mutex_unlock(&q->mutex);
     return ret;
 }
