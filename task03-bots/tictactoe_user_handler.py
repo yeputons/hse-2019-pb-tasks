3540037f1118440f686809e4dadd56788f886638
Роман Венедиктов <e2e4b6b7@gmail.com>
2019-12-13 13:22:07 +0300

    Implement TicTacToeUserHandler

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -1,3 +1,4 @@
+import re
 from typing import Callable, Optional
 from bot import UserHandler
 from tictactoe import Player, TicTacToe
@@ -9,13 +10,55 @@ class TicTacToeUserHandler(UserHandler):
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        raise NotImplementedError
+        if message == 'start':
+            self.start_game()
+            self.send_field()
+        elif not self.game:
+            self.send_message('Game is not started')
+        elif re.fullmatch('[XO] [012] [012]', message):
+            self.__try_make_turn(message)
+        else:
+            self.send_message('Invalid turn')
+
+    def __try_make_turn(self, message):
+        player = Player.X if message[0] == 'X' else Player.O
+        row = int(message[2])
+        col = int(message[4])
+        if self.game.can_make_turn(player, row=row, col=col):
+            self.make_turn(player, row=row, col=col)
+            self.send_field()
+            self.__is_finished()
+        else:
+            self.send_message('Invalid turn')
+
+    def __is_finished(self):
+        if self.game.is_finished():
+            winner = self.game.winner()
+            if winner == Player.X:
+                self.send_message('Game is finished, X wins')
+            elif winner == Player.O:
+                self.send_message('Game is finished, O wins')
+            else:
+                self.send_message('Game is finished, draw')
+            self.game = None
 
     def start_game(self) -> None:
-        raise NotImplementedError
+        self.game = TicTacToe()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        raise NotImplementedError
+        assert self.game
+        self.game.make_turn(player, row=row, col=col)
 
     def send_field(self) -> None:
-        raise NotImplementedError
+        assert self.game
+        out = ''
+        for row in self.game.field:
+            for point in row:
+                if point == Player.O:
+                    out += 'O'
+                elif point == Player.X:
+                    out += 'X'
+                else:
+                    out += '.'
+            out += '\n'
+        self.send_message(out[0:11])
