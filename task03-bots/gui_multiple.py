1d2f70e2929b92dc11b708d36fe2f7ea7f88ef9d
Даниил Габитов <work.gabitov@gmail.com>
2019-12-14 17:51:58 +0300

    Complete 2d part of the task.

--- task03-bots/gui_multiple.py
+++ task03-bots/gui_multiple.py
@@ -2,8 +2,7 @@
 import tkinter
 import traceback
 from typing import Callable, Dict, Optional
-from alarm_user_handler import AlarmUserHandler
-from bot import UserIndependentBot
+from chat_bot import ChatBot
 
 
 class UserWidget(tkinter.LabelFrame):
@@ -56,9 +55,8 @@ class UserWidget(tkinter.LabelFrame):
 def main() -> None:
     user_widgets: Dict[int, UserWidget] = {}
 
-    bot = UserIndependentBot(
+    bot = ChatBot(
         send_message=lambda user_id, message: user_widgets[user_id].received_message(message),
-        user_handler=AlarmUserHandler
     )
 
     def handle_message(user_id: int, message: str) -> None:
