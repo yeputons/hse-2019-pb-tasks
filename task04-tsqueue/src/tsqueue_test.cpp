75c160aa9bfdda5769ce4671cea86bb7426a6063
Глеб Соловьев <lionyoung@mail.ru>
2019-12-14 23:35:21 +0300

    Move variable initialization to a loop; replace CHECK with REQUIRE

--- task04-tsqueue/src/tsqueue_test.cpp
+++ task04-tsqueue/src/tsqueue_test.cpp
@@ -75,8 +75,8 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
             int ball = i * 65;
             threadsafe_queue_push(&qs[0], &ball);
             int *ball_back = (int *)threadsafe_queue_wait_and_pop(&qs[1]);
+            REQUIRE(ball_back == &ball);
             CHECK(*ball_back == i * 65 + 1);
-            CHECK(ball_back == &ball);
         }
         return nullptr;
     };
@@ -119,8 +119,8 @@ void *consumer(void *_q) {
 
 void *consumer_try(void *_q) {
     ThreadsafeQueue *q = static_cast<ThreadsafeQueue *>(_q);
-    void *data = nullptr;
     for (int i = 0; i < ELEMENTS_PER_THREAD; i++) {
+        void *data;
         REQUIRE(threadsafe_queue_try_pop(q, &data));
         REQUIRE(data == nullptr);
     }
