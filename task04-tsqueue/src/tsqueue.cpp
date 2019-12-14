890a91b149d8b01d76d91d0e3d7ccd2e57ae1e9d
achaikova <57727520+achaikova@users.noreply.github.com>
2019-12-14 19:10:33 +0300

    fix episode I

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,30 +1,29 @@
 #include "tsqueue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    queue_init(&(q->q));
-    pthread_mutex_init(&(q->m), NULL);
+    queue_init(&q->q);
+    pthread_mutex_init(&q->m, NULL);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_mutex_lock(&(q->m));
-    queue_destroy(&(q->q));
-    pthread_mutex_unlock(&(q->m));
+    pthread_mutex_destroy(&q->m);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    pthread_mutex_lock(&(q->m));
-    queue_push(&(q->q), data);
-    pthread_mutex_unlock(&(q->m));
+    pthread_mutex_lock(&q->m);
+    queue_push(&q->q, data);
+    pthread_mutex_unlock(&q->m);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    pthread_mutex_lock(&(q->m));
-    if (!queue_empty(&(q->q))) {
-        *data = queue_pop(&(q->q));
-        pthread_mutex_unlock(&(q->m));
+    pthread_mutex_lock(&q->m);
+    if (!queue_empty(&q->q)) {
+        *data = queue_pop(&q->q);
+        pthread_mutex_unlock(&q->m);
         return true;
     }
-    pthread_mutex_unlock(&(q->m));
+    pthread_mutex_unlock(&q->m);
     return false;
 }
 
--- task04-tsqueue/src/tsqueue_test.cpp
+++ task04-tsqueue/src/tsqueue_test.cpp
@@ -108,9 +108,10 @@ void *consumer(void *_q) {
 
 void *consumer_try(void *_q) {
     ThreadsafeQueue *q = static_cast<ThreadsafeQueue *>(_q);
+    void *data = nullptr;
     for (int i = 0; i < ELEMENTS_PER_THREAD; i++) {
-        void *data = nullptr;
         REQUIRE(threadsafe_queue_try_pop(q, &data));
+        REQUIRE(data == nullptr);
     }
     return nullptr;
 }
