ade454688c19e3f5fa41abc0cc73dabe43ede94b
Tanya <yakushina.tanya@gmail.com>
2019-12-15 02:08:55 +0300

    Add part 4: tictactoe

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -9,13 +9,46 @@ class TicTacToeUserHandler(UserHandler):
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
-        raise NotImplementedError
+        message = message.strip()
+        if message == 'start':
+            self.start_game()
+            return
+        if self.game is None:
+            self.send_message('Game is not started')
+            return
+        row, col, player = message.split()
+        self.make_turn(Player.X if player == 'X' else Player.O, row=int(row), col=int(col))
 
     def start_game(self) -> None:
-        raise NotImplementedError
+        self.game = TicTacToe()
+        self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
-        raise NotImplementedError
+        assert self.game is not None
+        if not self.game.can_make_turn(player, row=row, col=col):
+            self.send_message('Invalid turn')
+            return
+        self.game.make_turn(player, row=row, col=col)
+        self.send_field()
+        if self.game.is_finished():
+            if self.game.winner() == Player.X:
+                self.send_message('Game is finished, X wins')
+            elif self.game.winner() == Player.O:
+                self.send_message('Game is finished, O wins')
+            elif not self.game.winner():
+                self.send_message('Game is finished, draw')
+            self.game = None
 
     def send_field(self) -> None:
-        raise NotImplementedError
+        assert self.game
+        field = ''
+        for col in self.game.field:
+            for row in col:
+                if row is None:
+                    field += '.'
+                if row == Player.X:
+                    field += 'X'
+                if row == Player.O:
+                    field += 'O'
+            field += '\n'
+        self.send_message(field.strip('\n'))
