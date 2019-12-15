4cd0e72ec2bbe18b427a7f811b14a26076e3859a
Jimiliani <dikorolyov@mail.ru>
2019-12-15 18:09:29 +0300

    travis fixed

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -5,46 +5,54 @@ import re
 import argparse
 
 
-def filter(opt: Dict[str, Any], allowed: List[str], exception: bool = False) -> Dict[str, Any]:
-    formated_opt = {}
+def filterr(opt: Dict[str, Any], allowed: List[str], exception: bool = False) -> Dict[str, Any]:
+    formatted_opt = {}
     for one_opt in opt:
         if (one_opt in allowed) ^ exception:
-            formated_opt[one_opt] = opt[one_opt]
-    return formated_opt
+            formatted_opt[one_opt] = opt[one_opt]
+    return formatted_opt
 
 
 def init_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     output_format_group = parser.add_mutually_exclusive_group()
-    output_format_group.add_argument('-c', dest='do_count', action='store_true', help='Выдает только количество '
-                                                                                      'строк, содержащих образец.')
-    output_format_group.add_argument('-l', dest='do_only_files', action='store_true', help='Выдает только имена '
-                                                                                           'файлов, содержащих '
-                                                                                           'сопоставившиеся строки, '
-                                                                                           'по одному в строке. Если '
-                                                                                           'образец найден в '
-                                                                                           'нескольких строках файла, '
-                                                                                           'имя файла не повторяется.')
-    output_format_group.add_argument('-L', dest='do_only_not_files', action='store_true', help='Выдает только имена '
-                                                                                               'файлов, НЕ содержащих '
-                                                                                               'сопоставившиеся '
-                                                                                               'строки, по одному в '
-                                                                                               'строке. Если образец '
-                                                                                               'НЕ найден в '
-                                                                                               'нескольких строках '
-                                                                                               'файла, имя файла не '
-                                                                                               'повторяется.')
-    #                                                                Kto prochital tot zdoxnet))
-    parser.add_argument('-E', dest='regex', action='store_true', help='Ищет регулярное выражение')
-    parser.add_argument('-v', dest='do_invert', action='store_true', help='Выдает все строки, за исключением '
-                                                                          'содержащих образец.')
-    parser.add_argument('-i', dest='ignore_case', action='store_true', help='Игнорирует регистр символов при '
-                                                                            'сравнениях.')
-    parser.add_argument('-x', dest='do_whole_line', action='store_true', help='Считает сопоставившимися только '
-                                                                              'строки, все символы которых '
-                                                                              'использованы при сопоставлении с '
-                                                                              'фиксированной строкой или регулярным '
-                                                                              'выражением.')
+    output_format_group.add_argument('-c', dest='do_count', action='store_true',
+                                     help='Выдает только количество строк, содержащих образец.')
+    output_format_group.add_argument('-l',
+                                     dest='do_only_files',
+                                     action='store_true',
+                                     help='Выдает только файлов, содержащих '
+                                          'сопоставившиеся строки, по одному в строке.'
+                                          ' Если образец найден в '
+                                          'нескольких строках файла,'
+                                          ' имя файла не повторяется.')
+    output_format_group.add_argument('-L',
+                                     dest='do_only_not_files',
+                                     action='store_true',
+                                     help='Выдает только имена '
+                                          'файлов, НЕ содержащих '
+                                          'сопоставившиеся '
+                                          'строки, по одному в '
+                                          'строке. Если образец '
+                                          'НЕ найден в '
+                                          'нескольких строках '
+                                          'файла, имя файла не '
+                                          'повторяется.')
+    parser.add_argument('-E', dest='regex',
+                        action='store_true',
+                        help='Ищет регулярное выражение')
+    parser.add_argument('-v', dest='do_invert', action='store_true',
+                        help='Выдает все строки, за исключением содержащих образец.')
+    parser.add_argument('-i', dest='ignore_case',
+                        action='store_true',
+                        help='Игнорирует регистр символов при сравнениях.')
+    parser.add_argument('-x', dest='do_whole_line',
+                        action='store_true',
+                        help='Считает сопоставившимися только '
+                             'строки, все символы которых '
+                             'использованы при сопоставлении с '
+                             'фиксированной строкой или регулярным '
+                             'выражением.')
     parser.add_argument('needle', type=str, help='Строка которую необходимо найти')
     parser.add_argument('files', nargs='*', help='Список файлов, в которых нужно произвести поиск')
 
@@ -85,7 +93,8 @@ def finder_inline(opt: Dict[str, Any]) -> bool:
 def handler(opt: Dict[str, Any], final: bool) -> None:
     if final:
         if opt['delayed_output']:
-            if opt['do_count'] or opt['do_only_files'] and opt['count'] > 0 or opt['do_only_not_files'] and opt['count'] == 0:
+            if opt['do_count'] or opt['do_only_files'] and opt['count'] > 0 or \
+                    opt['do_only_not_files'] and opt['count'] == 0:
                 print(opt['format_out'].format_map(opt))
     else:
         opt['count'] += 1
@@ -95,12 +104,14 @@ def handler(opt: Dict[str, Any], final: bool) -> None:
 
 def searcher(opt: Dict[str, Any]) -> None:
     opt['count'] = 0
-    opt_for_formater = filter(opt, ['do_only_files', 'do_only_not_files', 'do_count', 'name_of_files', 'files'])
+    opt_for_formater = filterr(opt, ['do_only_files', 'do_only_not_files',
+                                     'do_count', 'name_of_files', 'files'])
     opt['format_out'] = format_builder(opt_for_formater)
-    opt_for_finder = filter(opt, ['regex', 'needle', 'do_invert', 'ignore_case', 'do_whole_line', 'regex_flags',
-                                  'do_whole_line', 'line'])
-    opt_for_handler = filter(opt, ['delayed_output', 'do_only_files', 'do_only_not_files',
-                                   'do_count', 'count', 'format_out', 'count'])
+    opt_for_finder = filterr(opt, ['regex', 'needle', 'do_invert',
+                                   'ignore_case', 'do_whole_line', 'regex_flags',
+                                   'do_whole_line', 'line'])
+    opt_for_handler = filterr(opt, ['delayed_output', 'do_only_files', 'do_only_not_files',
+                                    'do_count', 'count', 'format_out', 'count'])
     for line in opt['io']:
         line = line.rstrip('\n')
         opt_for_finder['line'] = line
