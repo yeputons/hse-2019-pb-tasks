69a87ea87bbecd0069333eca27b58ab2be46b89b
German <tarabonda@bk.ru>
2019-12-16 02:17:10 +0300

    Add test, fix User_Handler

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -5,7 +5,6 @@ from tictactoe import Player, TicTacToe
 
 class TicTacToeUserHandler(UserHandler):
     """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
-
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
@@ -17,12 +16,19 @@ class TicTacToeUserHandler(UserHandler):
         elif self.game is None:
             self.send_message('Game is not started')
         else:
-            player, row, col = message.split()
+            input_source = message.split()
+            if not len(input_source) == 3:
+                self.send_message('Invalid turn')
+                return
 
-            players = {'X': Player.X, 'O': Player.O}
+            if (not 0 <= int(input_source[1]) < 3) or (not 0 <= int(input_source[2]) < 3):
+                self.send_message('Invalid turn')
+                return
 
-            if self.game.can_make_turn(player=players[player], row=int(row), col=int(col)):
-                self.make_turn(player=players[player], row=int(row), col=int(col))
+            if self.game.can_make_turn(player=Player[input_source[0]],
+                                       row=int(input_source[1]), col=int(input_source[2])):
+                self.make_turn(player=Player[input_source[0]],
+                               row=int(input_source[1]), col=int(input_source[2]))
             else:
                 self.send_message('Invalid turn')
 
@@ -37,7 +43,6 @@ class TicTacToeUserHandler(UserHandler):
         self.game.make_turn(player=player, row=row, col=col)
         if self.game.is_finished():
             winner = self.game.winner()
-            assert winner
             if winner is None:
                 self.send_message('Game is finished, draw')
             else:
@@ -49,18 +54,8 @@ class TicTacToeUserHandler(UserHandler):
 
     def send_field(self) -> None:
         """Отправляет пользователю сообщение с текущим состоянием игры."""
-        game_field = []
         assert self.game
         field = self.game.field
-        assert field
-        for col in range(3):
-            message = ''
-            for row in range(3):
-                cell = field[row][col]
-                if cell is None:
-                    message += '.'
-                else:
-                    assert cell
-                    message += cell.name
-            game_field.append(message)
-        self.send_message('\n'.join(game_field))
+        message = '\n'.join([''.join([cell.name
+                                      if cell else '.' for cell in col]) for col in field])
+        self.send_message(message)
--- task03-bots/tictactoe_user_handler_test.py
+++ task03-bots/tictactoe_user_handler_test.py
@@ -26,7 +26,7 @@ def test_x_wins(mocker: pytest_mock.MockFixture):
 def test_draw(mocker: pytest_mock.MockFixture):
     send_message = mocker.stub(name='send_message_stub')
     bot = TicTacToeUserHandler(send_message)
-    bot.handle_message('MISHA GAY')
+    bot.handle_message('HMMM')
     bot.handle_message('X 1 1')
     bot.handle_message('start')
     bot.handle_message('X 0 1')
@@ -38,6 +38,7 @@ def test_draw(mocker: pytest_mock.MockFixture):
     bot.handle_message('X 0 2')
     bot.handle_message('O 1 2')
     bot.handle_message('X 2 1')
+    bot.handle_message('Kikoriki')
     assert send_message.call_args_list == [
         mocker.call('Game is not started'),
         mocker.call('Game is not started'),
@@ -50,7 +51,8 @@ def test_draw(mocker: pytest_mock.MockFixture):
         mocker.call('OXO\nXO.\n..X'),
         mocker.call('OXO\nXO.\nX.X'),
         mocker.call('OXO\nXO.\nXOX'),
-        mocker.call('Game is finished, draw')
+        mocker.call('Game is finished, draw'),
+        mocker.call('Game is not started')
     ]
 
 
@@ -82,4 +84,21 @@ def test_o_wins(mocker: pytest_mock.MockFixture):
         mocker.call('XXO\nXO.\n...'),
         mocker.call('Invalid turn'),
         mocker.call('Game is finished, O wins')
+    ]
+
+
+def test_invalid_turns(mocker: pytest_mock.MockFixture):
+    send_message = mocker.stub(name='send_message_stub')
+    bot = TicTacToeUserHandler(send_message)
+    bot.handle_message('Who')
+    bot.handle_message('start')
+    bot.handle_message('O 1 2')
+    bot.handle_message('X00')
+    bot.handle_message('X 1 3')
+    assert send_message.call_args_list == [
+        mocker.call('Game is not started'),
+        mocker.call('...\n...\n...'),
+        mocker.call('Invalid turn'),
+        mocker.call('Invalid turn'),
+        mocker.call('Invalid turn')
     ]
