a0753d9884cace41843f2b7ea0dd6b111d951cfa
Jimiliani <44744890+Jimiliani@users.noreply.github.com>
2019-12-15 19:41:52 +0300

    Update tsqueue.cpp

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,50 +1,46 @@
 #include "tsqueue.h"
-#include <assert.h>
-#include <iostream>
+#include <cassert>
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    assert(q);
     queue_init(&q->q);
-    int error = pthread_mutex_init(&q->mutex, NULL);
-    assert(error == 0);
-    error = pthread_cond_init(&q->cv, NULL);
-    assert(error == 0);
+    assert(pthread_mutex_init(&q->mutex, NULL) == 0);
+    assert(pthread_cond_init(&q->has_element, nullptr) == 0);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    assert(q);
-    queue_destroy(&q->q);
+    pthread_cond_destroy(&q->has_element);
     pthread_mutex_destroy(&q->mutex);
-    pthread_cond_destroy(&q->cv);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    assert(q);
     pthread_mutex_lock(&q->mutex);
+
     queue_push(&q->q, data);
-    pthread_cond_signal(&q->cv);
+
+    pthread_cond_signal(&q->has_element);
     pthread_mutex_unlock(&q->mutex);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    assert(q);
-    assert(data);
     pthread_mutex_lock(&q->mutex);
-    bool is_empty = queue_empty(&q->q);
-    if (!is_empty) {
+
+    bool has_element = !queue_empty(&q->q);
+    if (has_element)
         *data = queue_pop(&q->q);
-    }
+
     pthread_mutex_unlock(&q->mutex);
-    return !is_empty;
+    return has_element;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    void *result;
     pthread_mutex_lock(&q->mutex);
-    while (queue_empty(&q->q)) {
-        pthread_cond_wait(&q->cv, &q->mutex);
-    }
-    result = queue_pop(&q->q);
+
+    while (queue_empty(&q->q))
+        pthread_cond_wait(&q->has_element, &q->mutex);
+
+    void *data = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex);
-    return result;
+
+    return data;
 }
