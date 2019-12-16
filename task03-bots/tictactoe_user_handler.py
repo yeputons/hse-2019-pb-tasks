4315b4fe2d3880c62c4534c30ba120c4f64847dc
Ruslan Salkaev <rsalkaev@gmail.com>
2019-12-16 17:56:16 +0300

    Fix comments

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -11,30 +11,34 @@ class TicTacToeUserHandler(UserHandler):
         self.game: Optional[TicTacToe] = None
 
     def handle_message(self, message: str) -> None:
+        """Обрабатывает очередное сообщение от пользователя."""
         if message == 'start':
             self.start_game()
             return
         if not self.game:
             self.send_message('Game is not started')
             return
-        player_name, col, row = message.split(maxsplit=2)
+        player_name, col, row = message.split()
         self.make_turn(Player[player_name], row=int(row), col=int(col))
 
     def start_game(self) -> None:
+        """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
         self.game = TicTacToe()
         self.send_field()
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
+        """Обрабатывает ход игрока player в клетку (row, col)."""
         assert self.game
         if not self.game.can_make_turn(player=player, row=row, col=col):
             self.send_message('Invalid turn')
-        else:
-            self.game.make_turn(player=player, row=row, col=col)
-            self.send_field()
+            return
+        self.game.make_turn(player=player, row=row, col=col)
+        self.send_field()
         if self.game.is_finished():
             self.finish_game()
 
     def send_field(self) -> None:
+        """Отправляет пользователю сообщение с текущим состоянием игры."""
         assert self.game
         field: List[str] = []
         for row in self.game.field:
