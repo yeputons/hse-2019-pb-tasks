7043868930cb8fe7f447b85d5fda7291eccb2957
Glebanister <glabmar2001@gmail.com>
2019-12-16 17:40:13 +0300

    Change order of joining ping pong threads

--- task04-tsqueue/src/tsqueue_test.cpp
+++ task04-tsqueue/src/tsqueue_test.cpp
@@ -96,8 +96,8 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
     pthread_t t1, t2;
     REQUIRE(pthread_create(&t1, nullptr, pinger, qs) == 0);
     REQUIRE(pthread_create(&t2, nullptr, ponger, qs) == 0);
-    REQUIRE(pthread_join(t1, nullptr) == 0);
     REQUIRE(pthread_join(t2, nullptr) == 0);
+    REQUIRE(pthread_join(t1, nullptr) == 0);
 
     threadsafe_queue_destroy(&qs[1]);
     threadsafe_queue_destroy(&qs[0]);
