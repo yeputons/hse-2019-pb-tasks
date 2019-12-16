b6ec3fdb7bfe1d04d590522ef1f315b6ed8634f0
Artyom Son <s0sis0n@yandex.ru>
2019-12-16 03:07:29 +0300

    fixed

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -7,35 +7,35 @@ void threadsafe_queue_init(ThreadsafeQueue *q) {
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_cond_destroy(&q->cond_queue, nullptr);
-    pthread_mutex_destroy(&q->mutex_queue, nullptr);
-    queue_destroy(&q->mutex_queue);
+    pthread_cond_destroy(&q->cond_queue);
+    pthread_mutex_destroy(&q->mutex_queue);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    pthread_mutex_lock(&q->mutex_queue, nullptr);
+    pthread_mutex_lock(&q->mutex_queue);
     queue_push(&q->q, data);
-    pthread_cond_signal(&q->cond_queue, nullptr);
-    pthread_mutex_unlock(&q->mutex_queue, nullptr);
+    pthread_cond_signal(&q->cond_queue);
+    pthread_mutex_unlock(&q->mutex_queue);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    pthread_mutex_lock(&q->mutex_queue, nullptr);
-    flag = queue_empty(&q->q);
+    pthread_mutex_lock(&q->mutex_queue);
+    bool flag = queue_empty(&q->q);
     if (!flag){
         *data = queue_pop(&q->q);
     }
-    pthread_mutex_unlock(&q->mutex_queue, nullptr);
+    pthread_mutex_unlock(&q->mutex_queue);
     return flag;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    *data;
     pthread_mutex_lock(&q->mutex_queue);
     return nullptr;
     while (queue_empty(&q->q)){
         pthread_cond_wait(&q->cond_queue, &q->mutex_queue);
     }
+    void * data;
     data = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex_queue);
     return data;
