a356232259655b19ce100959bd2c672f07be86e1
Alex <luchinin.lescha@yandex.ru>
2019-12-16 21:49:23 +0400

    Fix clang-format

--- task04-tsqueue/src/tsqueue_test.cpp
+++ task04-tsqueue/src/tsqueue_test.cpp
@@ -51,57 +51,6 @@ TEST_SUITE("ThreadsafeQueue works like Queue in a single thread") {
     }
 }
 
-TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
-    ThreadsafeQueue qs[2];
-    threadsafe_queue_init(&qs[0]);
-    threadsafe_queue_init(&qs[1]);
-
-    // 0. Создаются два потока: `pinger` и `ponger`
-    //    (независимые от основного потока теста).
-    //    После этого `PING_PONGS` раз должно произойти следующее:
-    // 1. Поток `pinger` отправляет через `qs[0]` потоку `ponger`
-    //    указатель на локальную переменную типа `int`.
-    // 2. Поток `ponger` увеличивает полученную переменную на
-    //    единицу и отправляет результат обратно через `qs[1]`.
-    // 3. Поток `pinger` проверяет, что пришёл правильный адрес
-    //    и что локальная переменная была увеличена.
-    const int PING_PONGS = 100;
-
-    // Специальный синтаксис для объявления функции внутри функции.
-    // (в общем случае это лямбда-функции/замыкания, но нам это неважно).
-    auto pinger = [](void *_qs) -> void * {
-        ThreadsafeQueue *qs = static_cast<ThreadsafeQueue *>(_qs);
-        for (int i = 0; i < PING_PONGS; i++) {
-            int data = i * i;
-            int old_data = data;
-            threadsafe_queue_push(&qs[0], &data);
-            int *recieved_value = static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[1]));
-            CHECK(&data == recieved_value);
-            CHECK(old_data + 1 == data);
-        }
-        return nullptr;
-    };
-
-    auto ponger = [](void *_qs) -> void * {
-        ThreadsafeQueue *qs = static_cast<ThreadsafeQueue *>(_qs);
-        for (int i = 0; i < PING_PONGS; i++) {
-            int *data = static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[0]));
-            (*data)++;
-            threadsafe_queue_push(&qs[1], data);
-        }
-        return nullptr;
-    };
-
-    pthread_t t1, t2;
-    REQUIRE(pthread_create(&t1, nullptr, pinger, qs) == 0);
-    REQUIRE(pthread_create(&t2, nullptr, ponger, qs) == 0);
-    REQUIRE(pthread_join(t2, nullptr) == 0);
-    REQUIRE(pthread_join(t1, nullptr) == 0);
-
-    threadsafe_queue_destroy(&qs[1]);
-    threadsafe_queue_destroy(&qs[0]);
-}
-
 void *producer(void *_q) {
     ThreadsafeQueue *q = static_cast<ThreadsafeQueue *>(_q);
     for (int i = 0; i < ELEMENTS_PER_THREAD; i++) {
@@ -225,4 +174,57 @@ TEST_CASE("ThreadsafeQueue pushes and pops from multiple threads") {
     }
 
     threadsafe_queue_destroy(&q);
+}
+
+TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
+    ThreadsafeQueue qs[2];
+    threadsafe_queue_init(&qs[0]);
+    threadsafe_queue_init(&qs[1]);
+
+    // 0. Создаются два потока: `pinger` и `ponger`
+    //    (независимые от основного потока теста).
+    //    После этого `PING_PONGS` раз должно произойти следующее:
+    // 1. Поток `pinger` отправляет через `qs[0]` потоку `ponger`
+    //    указатель на локальную переменную типа `int`.
+    // 2. Поток `ponger` увеличивает полученную переменную на
+    //    единицу и отправляет результат обратно через `qs[1]`.
+    // 3. Поток `pinger` проверяет, что пришёл правильный адрес
+    //    и что локальная переменная была увеличена.
+    const int PING_PONGS = 100;
+
+    // Специальный синтаксис для объявления функции внутри функции.
+    // (в общем случае это лямбда-функции/замыкания, но нам это неважно).
+    auto pinger = [](void *_qs) -> void * {
+        ThreadsafeQueue *qs = static_cast<ThreadsafeQueue *>(_qs);
+        for (int i = 0; i < PING_PONGS; i++) {
+            int data = i * i;
+            int old_data = data;
+            threadsafe_queue_push(&qs[0], &data);
+            int *recieved_value =
+                static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[1]));
+            CHECK(&data == recieved_value);
+            CHECK(old_data + 1 == data);
+        }
+        return nullptr;
+    };
+
+    auto ponger = [](void *_qs) -> void * {
+        ThreadsafeQueue *qs = static_cast<ThreadsafeQueue *>(_qs);
+        for (int i = 0; i < PING_PONGS; i++) {
+            int *data =
+                static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[0]));
+            (*data)++;
+            threadsafe_queue_push(&qs[1], data);
+        }
+        return nullptr;
+    };
+
+    pthread_t t1, t2;
+    REQUIRE(pthread_create(&t1, nullptr, pinger, qs) == 0);
+    REQUIRE(pthread_create(&t2, nullptr, ponger, qs) == 0);
+    REQUIRE(pthread_join(t2, nullptr) == 0);
+    REQUIRE(pthread_join(t1, nullptr) == 0);
+
+    threadsafe_queue_destroy(&qs[1]);
+    threadsafe_queue_destroy(&qs[0]);
 }
