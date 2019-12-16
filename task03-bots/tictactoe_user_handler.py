91672fded188d0c683e2ce1dfec31aede42550b4
Ruslan Salkaev <rsalkaev@gmail.com>
2019-12-16 17:58:12 +0300

    Delete extra empty line

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -5,7 +5,6 @@ from tictactoe import Player, TicTacToe
 
 class TicTacToeUserHandler(UserHandler):
     """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
-
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
