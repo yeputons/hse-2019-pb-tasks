f5f0245d4dfd5f68827e80ebf891870e8901677f
Василий Янин <vasilii_yanin@mail.ru>
2019-12-16 22:58:48 +0300

    message

--- task03-bots/cli_single.py
+++ task03-bots/cli_single.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-def main() -> None:
-    raise NotImplementedError
+import sys
+import traceback
+from tictactoe_user_handler import TicTacToeUserHandler
 
+
+def send_message(message: str) -> None:
+    print(message)
+
+def main() -> None:
+    bot = TicTacToeUserHandler(send_message=send_message)
+    for line in sys.stdin:
+        line = line.rstrip('\n')
+        try:
+            bot.handle_message(message=line)
+        except Exception:  # pylint: disable=W0703
+            traceback.print_exc()
 
 if __name__ == '__main__':
     main()
