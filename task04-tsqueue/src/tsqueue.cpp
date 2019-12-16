e512474799b8aa30a765ac9031da256c2741ca5d
hwanglight <stray.polina.study@gmail.com>
2019-12-16 16:50:51 +0300

    Add threadsafe_queue_push()

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -18,15 +18,12 @@ void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&(q->work));
     queue_push(&(q->q), data);
     pthread_mutex_unlock(&(q->work));
-    static_cast<void>(q);  // Как-нибудь используем переменную.
-    static_cast<void>(data);  // Как-нибудь используем переменную.
+    //static_cast<void>(q);  // Как-нибудь используем переменную.
+    //static_cast<void>(data);  // Как-нибудь используем переменную.
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    // TODO
-    static_cast<void>(q);
-    static_cast<void>(data);
-    return false;
+
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
