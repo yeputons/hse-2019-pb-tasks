8c038aab53a09f81df68821ebc18749774aa2009
hwanglight <stray.polina.study@gmail.com>
2019-12-16 22:29:25 +0300

    Fixed style in tsqueue.cpp

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -3,40 +3,37 @@
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     queue_init(&(q->q));
     pthread_mutex_init(&(q->mutex_work), nullptr);
-    pthread_cond_init(&(q->cond_start_working), nullptr);
+    pthread_cond_init(&(q->cond_start_working_cause_queue_is_not_empty), nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_cond_destroy(&(q->cond_start_working));
-    pthread_mutex_destroy(&(q->mutex_work));
-    queue_destroy(&(q->q));
+    pthread_cond_destroy(&q->cond_start_working_cause_queue_is_not_empty);
+    pthread_mutex_destroy(&q->mutex_work);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    pthread_mutex_lock(&(q->mutex_work));
-    queue_push(&(q->q), data);
-    pthread_cond_signal(&q->cond_start_working);
-    pthread_mutex_unlock(&(q->mutex_work));
+    pthread_mutex_lock(&q->mutex_work);
+    queue_push(&q->q, data);
+    pthread_cond_signal(&q->cond_start_working_cause_queue_is_not_empty);
+    pthread_mutex_unlock(&q->mutex_work);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    bool success = false;
     pthread_mutex_lock(&q->mutex_work);
-    if (!queue_empty(&q->q)) {
+    bool success_poping = !queue_empty(&q->q);
+    if (success_poping)
         *data = queue_pop(&q->q);
-        success = true;
-    }
     pthread_mutex_unlock(&q->mutex_work);
-    return success;
+    return success_poping;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&q->mutex_work);
     while (queue_empty(&q->q)) {
-        pthread_cond_wait(&q->cond_start_working, &q->mutex_work);
+        pthread_cond_wait(&q->cond_start_working_cause_queue_is_not_empty, &q->mutex_work);
     }
     void *data = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex_work);
-
     return data;
 }
