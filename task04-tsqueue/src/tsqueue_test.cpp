b5c5866897095df80a0c3f5d170d46d2b9c1e489
vavilov mark <vavilovmark@gmail.com>
2019-12-16 20:14:46 +0300

    now it compiles

--- task04-tsqueue/src/tsqueue_test.cpp
+++ task04-tsqueue/src/tsqueue_test.cpp
@@ -114,8 +114,8 @@ void *consumer(void *_q) {
 
 void *consumer_try(void *_q) {
     ThreadsafeQueue *q = static_cast<ThreadsafeQueue *>(_q);
-    for (int i = 0, ; i < ELEMENTS_PER_THREAD; i++) {
-        void *data; 
+    for (int i = 0; i < ELEMENTS_PER_THREAD; i++) {
+        void *data;
         REQUIRE(threadsafe_queue_try_pop(q, &data) == true);
         REQUIRE(data == nullptr);
     }
