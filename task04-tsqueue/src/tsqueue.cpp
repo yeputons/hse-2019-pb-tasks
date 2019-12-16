990354e0a39e3f5c8ab11f3bb5a49ea6b4ea27b9
hwanglight <stray.polina.study@gmail.com>
2019-12-16 23:35:57 +0300

    Fixed mistakes

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -1,9 +1,9 @@
 #include "tsqueue.h"
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    queue_init(&(q->q));
-    pthread_mutex_init(&(q->mutex_work), nullptr);
-    pthread_cond_init(&(q->cond_start_cause_queue_is_not_empty), nullptr);
+    queue_init(&q->q);
+    pthread_mutex_init(&q->mutex_work, nullptr);
+    pthread_cond_init(&q->cond_start_cause_queue_is_not_empty, nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
@@ -21,11 +21,11 @@ void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
     pthread_mutex_lock(&q->mutex_work);
-    bool success_poping = !queue_empty(&q->q);
-    if (success_poping)
+    bool queue_is_not_empty = !queue_empty(&q->q);
+    if (queue_is_not_empty)
         *data = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex_work);
-    return success_poping;
+    return queue_is_not_empty;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
