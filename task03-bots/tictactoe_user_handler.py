b9abacf6fc89a788bc2c44d1dc69374d421c6837
Mikhaylova Anna <annamikhaylova6263@gmail.com>
2019-12-16 22:25:37 +0300

    Fix tictactoe bot

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -1,14 +1,14 @@
 from typing import Callable, Optional
 from bot import UserHandler
 from tictactoe import Player, TicTacToe
- 
- 
+
+
 class TicTacToeUserHandler(UserHandler):
     """Реализация логики бота для игры в крестики-нолики с одним пользователем."""
     def __init__(self, send_message: Callable[[str], None]) -> None:
         super(TicTacToeUserHandler, self).__init__(send_message)
         self.game: Optional[TicTacToe] = None
- 
+
     def handle_message(self, message: str) -> None:
         """Обрабатывает очередное сообщение от пользователя."""
         if message == 'start':
@@ -25,12 +25,12 @@ class TicTacToeUserHandler(UserHandler):
             self.make_turn(player=Player[player], row=int(row), col=int(col))
         except ValueError:
             self.send_message('Invalid turn')
- 
+
     def start_game(self) -> None:
         """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
         self.game = TicTacToe()
         self.send_field()
- 
+
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
         """Обрабатывает ход игрока player в клетку (row, col)."""
         assert self.game
@@ -47,7 +47,7 @@ class TicTacToeUserHandler(UserHandler):
                 self.send_message('Game is finished, O wins')
 
             self.game = None
- 
+
     def send_field(self) -> None:
         """Отправляет пользователю сообщение с текущим состоянием игры."""
         assert self.game
