bd839900d3f17727e99fb590d0030d18cf60c66d
Alexander Morozov <scanhex@gmail.com>
2019-12-16 12:23:58 +0300

    Apply style fixes

--- task04-tsqueue/src/tsqueue.cpp
+++ task04-tsqueue/src/tsqueue.cpp
@@ -2,37 +2,39 @@
 #include <cassert>
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    pthread_mutex_init(&q->mutex, nullptr);
-    pthread_cond_init(&q->cond, nullptr);
     queue_init(&q->q);
+    pthread_mutex_init(&q->mutex, nullptr);
+    pthread_cond_init(&q->cond_not_empty, nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    queue_destroy(&q->q);
+    pthread_cond_destroy(&q->cond_not_empty);
     pthread_mutex_destroy(&q->mutex);
-    pthread_cond_destroy(&q->cond);
+    queue_destroy(&q->q);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&q->mutex);
     queue_push(&q->q, data);
     pthread_mutex_unlock(&q->mutex);
-    pthread_cond_signal(&q->cond);
+    pthread_cond_signal(&q->cond_not_empty);
 }
 
 bool threadsafe_queue_try_pop(ThreadsafeQueue *q, void **data) {
-    bool res = false;
+    bool success = false;
     pthread_mutex_lock(&q->mutex);
-    if (!queue_empty(&q->q))
-        *data = queue_pop(&q->q), res = true;
+    if (!queue_empty(&q->q)) {
+        *data = queue_pop(&q->q);
+        success = true;
+    }
     pthread_mutex_unlock(&q->mutex);
-    return res;
+    return success;
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&q->mutex);
     while (queue_empty(&q->q))
-        pthread_cond_wait(&q->cond, &q->mutex);
+        pthread_cond_wait(&q->cond_not_empty, &q->mutex);
     void *res = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex);
     return res;
