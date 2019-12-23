60ee7b905724e3238d3b7f9597b78c72021fccf5
Василий Янин <vasilii_yanin@mail.ru>
2019-12-24 02:24:16 +0300

    message

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,73 @@
 #!/usr/bin/env python3
 from typing import List
+from typing import Tuple
 import sys
 import re
 import argparse
+
+
+def count(lines: List[str], regex: bool, pattern: str) -> int:
+    res = 0
+    for string in lines:
+        if regex:
+            if re.search(pattern, string):
+                res += 1
+        else:
+            if (pattern in string):
+                res += 1
+    return res
+
+
+def search_file(name: str, check: bool, regex: bool, lines: List[str], pattern: str):
+    for string in lines:
+        if regex:
+            if re.search(pattern, string):
+                if check:
+                    print(name + ':' + string)
+                else:
+                    print(string)
+        else:
+            if pattern in string:
+                if check:
+                    print(name + ':' + string)
+                else:
+                    print(string)
+
+
+def file_to_lines(file: str) -> List[str]:
+    if file == 'sys.stdin':
+        return [line.rstrip('\n') for line in sys.stdin.readlines()]
+    with open(file, 'r') as in_file:
+        return [line.rstrip('\n') for line in in_file.readlines()]
+
+
+def files_to_lines(files: List[str]) -> List[Tuple[str, List[str]]]:
+    return [(file, file_to_lines(file)) for file in files]
+
+
+def search_global(files: List[str], regex: bool, pattern: str, count_check: bool):
+    f = files_to_lines(files)
+    for file, lines in f:
+        if count_check:
+            if len(f) > 1:
+                print(file + ':' + str(count(lines, regex, pattern)))
+            else:
+                print(count(lines, regex, pattern))
+        else:
+            search_file(file, len(f) > 1, regex, lines, pattern)
 
 
 def main(args_str: List[str]):
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
+    parser.add_argument('-c', dest='count', action='store_true')
     parser.add_argument('-E', dest='regex', action='store_true')
     args = parser.parse_args(args_str)
-
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
-        line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
-
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+    if len(args.files) > 0:
+        search_global(args.files, args.regex, args.needle, args.count)
+    else:
+        search_global(['sys.stdin'], args.regex, args.needle, args.count)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -3,6 +3,90 @@ import io
 import grep
 
 
+def test_count(monkeypatch):
+    lines = ['needle', 'ndl', 'pref', 'needleneedle', 'suf', 'needle needle']
+    regex = 1
+    pattern = 'needle'
+    counter = grep.count(lines, regex, pattern)
+    assert counter == 3
+    pattern = 'n?dl?'
+    counter = grep.count(lines, regex, pattern)
+    assert counter == 4
+
+
+def test_search_file(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('the nendle\npref needle suf\nneedlen\n')
+    monkeypatch.chdir(tmp_path)
+    name = 'a.txt'
+    check = 0
+    regex = 0
+    lines = grep.file_to_lines(name)
+    pattern = 'needle'
+    grep.search_file(name, check, regex, lines, pattern)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'pref needle suf\nneedlen\n'
+    regex = 1
+    pattern = 'ne.dle'
+    grep.search_file(name, check, regex, lines, pattern)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'the nendle\npref needle suf\nneedlen\n'
+
+
+def test_file_to_lines(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('a\naaa\naaaaa\n')
+    monkeypatch.chdir(tmp_path)
+    file = 'a.txt'
+    lines = grep.file_to_lines(file)
+    assert lines == ['a', 'aaa', 'aaaaa']
+
+
+def test_files_to_lines(tmp_path, monkeypatch):
+    (tmp_path / 'a.txt').write_text('a\naaa\naaaaa\n')
+    (tmp_path / 'b.txt').write_text('bbbbb\nbbb\nb\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    lines = grep.files_to_lines(files)
+    assert lines == [('a.txt', ['a', 'aaa', 'aaaaa']), ('b.txt', ['bbbbb', 'bbb', 'b'])]
+
+
+def test_search_global(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('pref\nneedle\nsuf\n')
+    (tmp_path / 'b.txt').write_text('neddle\nb\nneedle needle\n')
+    monkeypatch.chdir(tmp_path)
+    files = ['a.txt', 'b.txt']
+    regex = 0
+    pattern = 'needle'
+    count_check = 0
+    grep.search_global(files, regex, pattern, count_check)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:needle\nb.txt:needle needle\n'
+
+    count_check = 1
+    grep.search_global(files, regex, pattern, count_check)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:1\n'
+
+    regex = 1
+    pattern = 'ne.dle'
+    count_check = 0
+    grep.search_global(files, regex, pattern, count_check)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:needle\nb.txt:neddle\nb.txt:needle needle\n'
+
+    regex = 1
+    pattern = 'ne.dle'
+    count_check = 1
+    grep.search_global(files, regex, pattern, count_check)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:2\n'
+
+
 def test_integrate_stdin_grep(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', io.StringIO(
         'pref needle?\nneedle? suf\nthe needl\npref needle? suf'))
