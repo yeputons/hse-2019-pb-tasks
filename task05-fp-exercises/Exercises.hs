46b9db32e6572b1f32796258777972447cf163c3
Egor Suvorov <egor.suvorov@gmail.com>
2020-01-18 00:24:40 +0300

    RunTests fixup-2

--- task05-fp-exercises/Exercises.hs
+++ task05-fp-exercises/Exercises.hs
@@ -1,9 +1,10 @@
 module Exercises where  -- Вспомогательная строчка, чтобы можно было использовать функции в других файлах.
+{- HLINT ignore "Use foldr" -}
+{- HLINT ignore "Unnecessary hiding" -}
 import Control.Arrow
 import Data.Char
 import Data.Text(isInfixOf, pack)
 import Prelude hiding (sum, concat, foldr, map)
-{- HLINT ignore "Use foldr" -}
 
 -- 1) Выделение функции высшего порядка.
 -- Функция sum' считает сумму чисел в списке при помощи функции sum''.
