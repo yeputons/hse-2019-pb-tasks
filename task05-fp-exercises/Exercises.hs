5235fdcc4b755764746693d580886be39c45ecb4
Ilya Onofriychuk <ilyaonof@yandex.ru>
2020-01-25 09:54:14 +0300

    Fix the second part of the task and implement grep

--- task05-fp-exercises/Exercises.hs
+++ task05-fp-exercises/Exercises.hs
@@ -150,7 +150,7 @@ fifthElement xs = case tryTail t of
                               Just a -> a
                               _      -> []
                         thirdElement ys = case tryTail ys of
-                                            Just a  -> secondElement
+                                            Just a  -> secondElement a
                                             _       -> Nothing
 
 -- Выделите общую логику в оператор ~~>.
@@ -243,7 +243,7 @@ type File = (String, [String])
 -- Здесь (\_ s -> s) --- это лямбда-функция, которая игнорирует первый
 -- параметр и возвращает второй.
 grep' :: (String -> [String] -> [String]) -> (String -> Bool) -> [File] -> [String]
-grep' format match files = undefined
+grep' format match files = concat' [format name (filter match lines) | (name, lines) <- files]
 
 -- Также вам предоставлена функция для проверки вхождения подстроки в строку.
 -- >>> isSubstringOf "a" "bac"
@@ -264,7 +264,7 @@ isSubstringOf n s = pack n `isInfixOf` pack s
 -- >>> grepSubstringNoFilename "c" [("a.txt", ["a", "a"]), ("b.txt", ["b", "bab", "c"]), ("c.txt", ["c", "ccccc"])]
 -- ["c", "c", "ccccc"]
 grepSubstringNoFilename :: String -> [File] -> [String]
-grepSubstringNoFilename needle files = undefined
+grepSubstringNoFilename needle = grep' (\_ s -> s) (isSubstringOf needle)
  
 -- Вариант, когда ищется точное совпадение и нужно ко всем подходящим строкам
 -- дописать имя файла через ":".
@@ -274,4 +274,4 @@ grepSubstringNoFilename needle files = undefined
 -- >>> grepExactMatchWithFilename "c" [("a.txt", ["a", "a"]), ("b.txt", ["b", "bab", "c"]), ("c.txt", ["c", "ccccc"])]
 -- ["b.txt:c", "c.txt:c"]
 grepExactMatchWithFilename :: String -> [File] -> [String]
-grepExactMatchWithFilename needle files = undefined
+grepExactMatchWithFilename needle = grep' (\name s -> map' ((name ++ ":") ++) s) (needle ==)
