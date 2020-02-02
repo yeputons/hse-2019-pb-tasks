19b9bfb1ad3e000e5cd88626328bceb73d15a855
Kirill Karnaukhov <bitikit@gmail.com>
2020-02-02 19:33:24 +0300

    complete first part

--- task06-fp-yat/Yat.hs
+++ task06-fp-yat/Yat.hs
@@ -47,8 +47,49 @@ showUnop Neg = "-"
 showUnop Not = "!"
 
 -- Верните текстовое представление программы (см. условие).
+
+-- scopeContains :: State -> Name -> Bool
+-- scopeContains []         _     = False
+-- scopeContains (sc:xscope) name | (fst sc) == name = True
+--                                | otherwise        = scopeContains xscope name 
+
+-- showExpression :: State -> Expression -> String
+-- showExpression _          (Number num)       = show num
+-- showExpression (sc:xscope) (Reference name)  | (fst sc) == name = show (snd sc)
+--                                              | otherwise       = showExpression xscope (Reference name)
+
+-- showExpression scope (Assign name expr) | scopeContains scope name = 
+
+dropLast :: Int -> [a] -> [a]
+dropLast n x = take ((length x) - n) x
+
+showArgs :: [String] -> String
+showArgs []    = ""
+showArgs args  = dropLast 2 . concat $ map (++ ", ") args
+
+addTabs :: String -> String
+addTabs []      = []
+addTabs (s:str) | s == '\n' = s:'\t':(addTabs str)
+                | otherwise = s:addTabs str
+
+showExpression :: Expression -> String
+showExpression (Number num)                      = show num
+showExpression (Reference name)                  = name
+showExpression (Assign name expr)                = concat ["let ", name, " = ", showExpression expr, " tel"]
+showExpression (BinaryOperation op left right)   = concat ["(", showExpression left, " ", showBinop op, " ", showExpression right, ")"]
+showExpression (UnaryOperation op expr)          = concat [showUnop op, showExpression expr]
+showExpression (FunctionCall name [])            = concat [name, "()"]
+showExpression (FunctionCall name args)          = concat [name, "(", showArgs $ map (showExpression) args, ")"]
+showExpression (Conditional expr ifTrue ifFalse) = concat ["if ", showExpression expr, " then ", showExpression ifTrue, " else ", showExpression ifFalse, " fi"]
+showExpression (Block [])                        = "{\n}"
+showExpression (Block exprs)                     = concat ["{\n\t", addTabs . dropLast 2 . concat $ (map ((++ ";\n") . showExpression) exprs), "\n}"]
+
+
+showFunction :: FunctionDefinition -> String
+showFunction (name, args, expr) = concat ["func ", name, "(", showArgs args, ") = ", showExpression expr]
+
 showProgram :: Program -> String
-showProgram = undefined
+showProgram (funcs, body) = concat [concat $ map ((++ "\n") . showFunction) funcs, showExpression body]  
 
 toBool :: Integer -> Bool
 toBool = (/=) 0
@@ -113,5 +154,53 @@ evalExpression = undefined
 -} -- Удалите эту строчку, если решаете бонусное задание.
 
 -- Реализуйте eval: запускает программу и возвращает её значение.
+
+evalBinop :: Binop -> Integer -> Integer -> Integer
+evalBinop Add a b = (+) a b 
+evalBinop Mul a b = (*) a b
+evalBinop Sub a b = (-) a b
+evalBinop Div a b = div a b
+evalBinop Mod a b = mod a b
+evalBinop Lt a b  = toInteger $ fromEnum $ (<) a b
+evalBinop Le a b  = toInteger $ fromEnum $ (<=) a b
+evalBinop Gt a b  = toInteger $ fromEnum $ (>) a b
+evalBinop Ge a b  = toInteger $ fromEnum $ (>=) a b
+evalBinop Eq a b  = toInteger $ fromEnum $ (==) a b
+evalBinop Ne a b  = toInteger $ fromEnum $ (/=) a b
+evalBinop And _ 0 = 0
+evalBinop And 0 _ = 0
+evalBinop And _ _ = 1
+evalBinop Or 0 0  = 0
+evalBinop Or _ _  = 1 
+
+evalUnop :: Unop -> Integer -> Integer
+evalUnop Neg a = -a
+evalUnop Not 0 = 1
+evalUnop Not _ = 0
+
+prepareScopeForFunction :: State -> [FunctionDefinition] -> FunctionDefinition -> [Expression] -> State
+prepareScopeForFunction scope funcs (_, [], _)       _     = scope
+prepareScopeForFunction scope funcs (fName, name:names, fExpr) (expr:exprs) = prepareScopeForFunction (name, snd value):scope funcs (fName, names, fExpr) exprs
+                                                                            where value = evalExpression scope funcs expr
+
+evalExpression :: State -> [FunctionDefinition] -> Expression -> (State, Integer)
+evalExpression scope funcs (Number num) = (scope, num)
+evalExpression (sc:scope) funcs (Reference name) | (fst sc) == name = (scope, snd sc)
+                                                 | otherwise        = evalExpression scope funcs (Reference name)
+
+evalExpression scope funcs (Assign name expr) = ((name, snd value):(fst value), snd value) 
+                                              where value = evalExpression scope funcs expr
+
+evalExpression scope funcs (BinaryOperation op left right) = (scope, evalBinop op (snd leftValue) (snd rightValue))
+                                                           where leftValue  = evalExpression scope funcs left
+                                                                 rightValue = evalExpression scope funcs right 
+
+evalExpression scope funcs (UnaryOperation op expr) = (scope, evalUnop op (snd $ value))
+                                                    where value = evalExpression scope funcs expr
+
+-- evalExpression scope ((fName, fArgs, fExpr):funcs) (FunctionCall name args) | fName /= name = evalExpression scope funcs (FunctionCall name args)
+--                                                                             | otherwise     = 
+--                                                                             where value = eval 
+
 eval :: Program -> Integer
-eval = undefined
+eval (funcs, body) = snd $ evalExpression [] funcs body
